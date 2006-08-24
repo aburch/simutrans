9b88e2334358180ad535236cbbe010dfa5163ef4
Christoph Mallon <christoph.mallon@gmx.de>
2006-08-24 09:50:09 +0000

    Just call abort() instead of doing some mumbo jumbo to get an exception
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@108 8aca7d54-2c30-db11-9de9-000461428c89

--- simdebug.cc
+++ simdebug.cc
@@ -2,9 +2,9 @@
 #define _SIM_DEBUG
 
 #include <assert.h>
+#include <stdlib.h>
 
 #include "simdebug.h"
-#include "simtools.h"
 #include "utils/log.h"
 
 
@@ -27,28 +27,18 @@ void init_logging(const char *logname, bool force_flush, bool log_debug)
 }
 
 
-// the next two functions are for helping with debugging
 #ifdef DEBUG
 
 #ifdef _MSC_VER
-// this make a pure virtual function call an division by zero error for MSVC
 int __cdecl _purecall()
 {
-	int b = simrand(12);
-	dbg->error("Pure virtual function call","-> DEBUGGER will be activated!");
-	b /= make_this_a_division_by_zero;
-	printf("%i",b);
+	abort();
 	return 0;	// to keep compiler happy
 }
-
 #else
-// this make a pure virtual function call an division by zero error for GCC
 extern "C" void __cxa_pure_virtual()
 {
-	int b = simrand(12);
-	dbg->error("Pure virtual function call","-> DEBUGGER will be activated!");
-	b /= make_this_a_division_by_zero;
-	printf("%i",b);
+	abort();
 }
 #endif
 
