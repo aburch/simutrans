e6831b0c4ea262b5ab38f98bbe2dd76d8f0a8427
Markus Pristovsek <prissi@physik.tu-berlin.de>
2006-10-21 21:28:33 +0000

    version 99.02.2
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@277 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/components/gui_flowtext.cc
+++ gui/components/gui_flowtext.cc
@@ -105,6 +105,9 @@ void gui_flowtext_t::set_text(const char *text)
 			else if(word[0]=='h' && word[1]=='1') {
 				att = endtag ? ATT_H1_END : ATT_H1_START;
 			}
+			else if(word[0]=='i') {
+				att = endtag ? ATT_IT_END : ATT_IT_START;
+			}
 			else if(word[0]=='e'  &&  word[1]=='m') {
 				att = endtag ? ATT_EM_END : ATT_EM_START;
 			}
@@ -195,100 +198,113 @@ void gui_flowtext_t::zeichnen(koord offset)
 
 
 
-koord gui_flowtext_t::output(koord offset, bool doit) const {
+koord gui_flowtext_t::output(koord offset, bool doit) const
+{
+	const int width = groesse.x;
 
-    const int width = groesse.x;
+	hyperlink_t * link = links;
 
-    hyperlink_t * link = links;
+	node_t *node = nodes;
+	int xpos = 0;
+	int ypos = 0;
+	int color = COL_BLACK;
+	int double_color = COL_BLACK;
+	bool double_it=false;
+	int max_width = width;
 
-    node_t *node = nodes;
-    int xpos = 0;
-    int ypos = 0;
-    int color = COL_BLACK;
-    int max_width = width;
+	while(node) {
+		if(node->att == ATT_NONE) {
+			int nxpos = xpos + proportional_string_width(node->text) + 4;
 
-    while(node) {
-	if(node->att == ATT_NONE) {
-	    int nxpos = xpos + proportional_string_width(node->text) + 4;
+			if(nxpos >= width) {
+				if(nxpos-xpos > max_width) {
+				    // word too long
+				    max_width = nxpos;
+				}
+				nxpos -= xpos;
+				xpos = 0;
+				ypos += LINESPACE;
+			}
 
-	    if(nxpos >= width) {
-		if(nxpos-xpos > max_width) {
-		    // word too long
-		    max_width = nxpos;
+			if(doit) {
+				if(double_it) {
+					display_proportional_clip(offset.x+xpos+1, offset.y+ypos+1, node->text, 0, double_color, true);
+				}
+				display_proportional_clip(offset.x+xpos, offset.y+ypos, node->text, 0, color, true);
+			}
+
+			xpos = nxpos;
 		}
-		nxpos -= xpos;
-		xpos = 0;
-		ypos += LINESPACE;
-	    }
+		else if(node->att == ATT_H1_START) {
+			color = COL_BLACK;
+			double_color = COL_WHITE;
+			double_it = true;
+		}
+		else if(node->att == ATT_H1_END) {
+			color = COL_BLACK;
+			double_it = false;
+			if(doit) {
+				display_fillbox_wh_clip(offset.x+1, offset.y+12+1, xpos-4, 1, COL_WHITE, true);
+				display_fillbox_wh_clip(offset.x, offset.y+12, xpos-4, 1, color, true);
+			}
+			xpos = 0;
+			ypos += LINESPACE;
+		}
+		else if(node->att == ATT_EM_START) {
+			color = COL_WHITE;
+		}
+		else if(node->att == ATT_EM_END) {
+			color = COL_BLACK;
+		}
+		else if(node->att == ATT_IT_START) {
+			color = COL_BLACK;
+			double_color = COL_YELLOW;
+			double_it = true;
+		}
+		else if(node->att == ATT_IT_END) {
+			double_it = false;
+		}
+		else if(node->att == ATT_STRONG_START) {
+			color = COL_RED;
+		}
+		else if(node->att == ATT_STRONG_END) {
+			color = COL_BLACK;
+		}
+		else if(node->att == ATT_A_START) {
+			color = COL_BLUE;
+			link->tl.x = xpos;
+			link->tl.y = ypos;
+		}
+		else if(node->att == ATT_A_END) {
+			link->br.x = xpos;
+			link->br.y = ypos+14;
 
-	    if(doit) {
-		display_proportional_clip(offset.x+xpos, offset.y+ypos, node->text, 0, color, true);
-	    }
+			if(link->br.x < link->tl.x) {
+				link->tl.x = 0;
+				link->tl.y = link->br.y-14;
+			}
 
-	    xpos = nxpos;
-	}
-	else if(node->att == ATT_H1_START) {
-	    color = COL_BLACK;
-	}
-	else if(node->att == ATT_H1_END) {
-	    color = COL_BLACK;
-	    if(doit) {
-		display_fillbox_wh_clip(offset.x, offset.y+12, xpos-4, 1, color, true);
-	    }
-	    xpos = 0;
-	    ypos += LINESPACE;
-	}
-	else if(node->att == ATT_EM_START) {
-	    color = COL_WHITE;
-	}
-	else if(node->att == ATT_EM_END) {
-	    color = COL_BLACK;
-	}
-	else if(node->att == ATT_STRONG_START) {
-	    color = COL_RED;
-	}
-	else if(node->att == ATT_STRONG_END) {
-	    color = COL_BLACK;
-	}
-	else if(node->att == ATT_A_START) {
-	    color = COL_BLUE;
-	    link->tl.x = xpos;
-	    link->tl.y = ypos;
-	}
-	else if(node->att == ATT_A_END) {
-	    link->br.x = xpos;
-	    link->br.y = ypos+14;
+			// links.insert(link);
 
-	    if(link->br.x < link->tl.x) {
-	      link->tl.x = 0;
-	      link->tl.y = link->br.y-14;
-	    }
-
-	    // links.insert(link);
-
-	    if(doit) {
-		display_fillbox_wh_clip(link->tl.x + offset.x,
+			if(doit) {
+				display_fillbox_wh_clip(link->tl.x + offset.x,
 					link->tl.y + offset.y + 10,
 					link->br.x-link->tl.x-4, 1,
 					color, true);
-	    }
+			}
 
-	    link = link->next;
+			link = link->next;
 
-	    color = COL_BLACK;
+			color = COL_BLACK;
+		}
+		else if(node->att == ATT_NEWLINE) {
+			xpos = 0;
+			ypos += LINESPACE;
+		}
+
+		node = node->next;
 	}
-	else if(node->att == ATT_NEWLINE) {
-	    xpos = 0;
-	    ypos += LINESPACE;
-	}
-
-	node = node->next;
-    }
-
-
-    // if(!doit) printf("%d\n", max_width);
-
-    return koord(max_width, ypos+LINESPACE);
+	return koord(max_width, ypos+LINESPACE);
 }
 
 
