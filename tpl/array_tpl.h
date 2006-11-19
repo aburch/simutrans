23087c0d3eeb73df6c740e34de2d09ce4d4fd1ea
Christoph Mallon <christoph.mallon@gmx.de>
2006-11-19 08:28:36 +0000

    Partially fix class array_tpl<>: - Make array_tpl<>(int) explicit - implicit conversion from int to array_tpl<> might lead to suprising results - Make the copy constructor unimplemented private: The automagically generated copy constructor would wreck havoc - array_tpl<>::resize() cannot be const - Make the index type explicit via a typedef
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@396 8aca7d54-2c30-db11-9de9-000461428c89

--- tpl/array_tpl.h
+++ tpl/array_tpl.h
@@ -1,15 +1,5 @@
-/*
- * array_tpl.h
- *
- * Copyright (c) 1997 - 2001 Hansjörg Malthaner
- *
- * This file is part of the Simutrans project and may not be used
- * in other projects without written permission of the author.
- */
-
-
-#ifndef tpl_array_tpl_h
-#define tpl_array_tpl_h
+#ifndef TPL_ARRAY_TPL_H
+#define TPL_ARRAY_TPL_H
 
 #include <typeinfo>
 #include "../simdebug.h"
@@ -19,65 +9,55 @@
  * A template class for bounds checked 1-dimesnional arrays.
  * This is kept as simple as possible. Does not use exceptions
  * for error handling.
- *
- * @author Hj. Malthaner
  */
-
-template <class T>
-class array_tpl
+template<class T> class array_tpl
 {
-private:
-	T* data;
-	uint16 size;
+	public:
+		typedef uint16 index;
 
-public:
+		explicit array_tpl(index s) : data(new T[s]), size(s) {}
 
-	array_tpl(unsigned int size) {
-		this->size = size;
-		data = new T[size];
-	}
-
-	array_tpl(unsigned int size, T * arr) {
-		this->size = size;
-		data = new T[size];
-		for(int i=0; i<size; i++) {
-			data[i] = arr[i];
+		array_tpl(index s, const T* arr) : data(new T[s]), size(s)
+		{
+			for (int i = 0; i < size; i++) data[i] = arr[i];
 		}
-	}
 
-	~array_tpl() {
-		delete [] data;
-	}
+		~array_tpl() { delete [] data; }
 
-	uint16 get_size() const {
-		return size;
-	}
+		index get_size() const { return size; }
 
-	void resize_size(uint16 resize) const {
-		if(size<resize) {
-			T *new_data = new T[size];
-			for( uint16 i=0;  i<size;  i++ ) {
-				new_data[i] = data[i];
+		void resize(index resize)
+		{
+			if (size < resize) {
+				T *new_data = new T[size];
+				for (index i = 0;  i < size;  i++ ) new_data[i] = data[i];
+				delete [] data;
+				data = new_data;
+				size = resize;
 			}
-			delete [] data;
-			data = new_data;
-			size = resize;
 		}
-	}
 
-	T& at(const uint16 i) {
-		if(i>=size) {
-			dbg->fatal("array_tpl<T>::at()", "index out of bounds: %d not in 0..%d, T=%s ", i, size-1, typeid(T).name());
+		T& at(index i) const
+		{
+			if (i >= size) {
+				dbg->fatal("array_tpl<%s>::at()", "index out of bounds: %d not in 0..%d", typeid(T).name(), i, size - 1);
+			}
+			return data[i];
 		}
-		return data[i];
-	}
 
-	const T& get(const uint16 i) {
-		if(i>=size) {
-			dbg->fatal("array_tpl<T>::get()", "index out of bounds: %d not in 0..%d, T=%s ", i, size-1, typeid(T).name());
+		const T& get(index i) const
+		{
+			if (i >= size) {
+				dbg->fatal("array_tpl<%s>::get()", "index out of bounds: %d not in 0..%d", typeid(T).name(), i, size - 1);
+			}
+			return data[i];
 		}
-		return data[i];
-	}
+
+	private:
+		array_tpl(const array_tpl&);
+
+		T* data;
+		index size;
 };
 
 #endif
