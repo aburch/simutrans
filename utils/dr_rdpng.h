6bde786746e5325a31aead3f9b71f9c513e1596a
Christoph Mallon <christoph.mallon@gmx.de>
2006-11-26 07:06:00 +0000

    Unify indentation
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@444 8aca7d54-2c30-db11-9de9-000461428c89

--- utils/dr_rdpng.h
+++ utils/dr_rdpng.h
@@ -1,9 +1,14 @@
+#ifndef DR_RDPNG_H
+#define DR_RDPNG_H
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int load_block(unsigned char **block, unsigned *width, unsigned *height, const char *fname,const int base_img_size);
+int load_block(unsigned char** block, unsigned* width, unsigned* height, const char* fname, int base_img_size);
 
 #ifdef __cplusplus
 }
+#endif
+
 #endif
