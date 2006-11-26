53384f37515d4b2cd2c67876dfc3728ee2674cf9
Christoph Mallon <christoph.mallon@gmx.de>
2006-11-26 16:18:03 +0000

    Overhaul vector_tpl<T>: - Make vector_tpl(uint) explicit to prevent suprises - Mark its data members private instead of protected - Remove an impossible case from resize(): if new_size > size then count cannot be larger than new_size - General cleanup NOTE: The semantics of remove() seems broken
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@448 8aca7d54-2c30-db11-9de9-000461428c89

--- tpl/vector_tpl.h
+++ tpl/vector_tpl.h
@@ -1,22 +1,5 @@
-/*
- * vector_tpl.h
- *
- * Copyright (c) 1997 - 2001 Hansjörg Malthaner
- *
- * This file is part of the Simutrans project and may not be used
- * in other projects without written permission of the author.
- */
-
-#ifndef tpl_vector_h
-#define tpl_vector_h
-
-/**
- * A template class for a simple vector type.
- * random access is allowed ...
- *
- * @date November 2000
- * @author Hj. Malthaner
- */
+#ifndef TPL_VECTOR_H
+#define TPL_VECTOR_H
 
 #include <stdlib.h>
 #include "../macros.h"
@@ -28,299 +11,168 @@ template<class T> class vector_tpl;
 template<class T> inline void swap(vector_tpl<T>& a, vector_tpl<T>& b);
 
 
-/**
- * A template class for a simple vector type.
- * @date November 2000
- * @author Hj. Malthaner
- *
- * extended with function store_at() for random acess storage by prissi
- * BEWARE: using this function will create default objects, depending of the type of the vector
- */
-
-template<class T>class vector_tpl
+/** A template class for a simple vector type */
+template<class T> class vector_tpl
 {
-protected:
+	public:
+		/** Construct a vector for cap elements */
+		explicit vector_tpl(uint32 cap) : data(cap > 0 ? new T[cap] : NULL), size(cap), count(0) {}
 
-	T * data;
+		~vector_tpl() { delete [] data; }
 
-	/**
-	 * Capacity.
-	 * @author Hj. Malthaner
-	 */
-	uint32 size;
+		/** sets the vector to empty */
+		void clear() { count = 0; }
 
+		/**
+		 * Resizes the maximum data that can be hold by this vector.
+		 * Existing entries are preserved, new_size must be big enough to hold them
+		 */
+		void resize(uint32 new_size)
+		{
+			if (new_size <= size) return; // do nothing
 
-	/**
-	 * Number of elements in vector.
-	 * @author Hj. Malthaner
-	 */
-	uint32 count;
-
-public:
-
-	/**
-	 * Construct a vector for size elements.
-	 * @param size The capacity of the new vector
-	 * @author Hj. Malthaner
-	 */
-	vector_tpl(uint32 size)
-	{
-		this->size  = size;
-		if(size>0) {
-			data = new T[size];
-		} else {
-			data = 0;
-		}
-		count = 0;
-	}
-
-
-	/**
-	 * Destructor.
-	 * @author Hj. Malthaner
-	 */
-	~vector_tpl()
-	{
-		if(data) {
+			T* new_data = new T[new_size];
+			for (uint32 i = 0; i < count; i++) new_data[i] = data[i];
 			delete [] data;
+			size = new_size;
+			data = new_data;
 		}
-	}
 
-	/**
-	 * sets the vector to empty
-	 * @author Hj. Malthaner
-	 */
-	void clear()
-	{
-		count = 0;
-	}
-
-	/**
-	 * Resizes the maximum data that can be hold by this vector.
-	 * Existing entries are preserved, new_size must be big enough
-	 * to hold them.
-	 *
-	 * @author prissi
-	 */
-	bool resize(uint32 new_size)
-	{
-		if(new_size<=size) {
-			return true;	// do nothing
-		}
-		//DBG_DEBUG("<vector_tpl>::resize()","old size %i, new size %i",size,new_size);
-		if(count > new_size) {
-			dbg->fatal("vector_tpl<T>::resize()", "cannot preserve elements.");
+		/**
+		 * Checks if element elem is contained in vector.
+		 * Uses the == operator for comparison.
+		 */
+		bool is_contained(T elem) const
+		{
+			for (uint32 i = 0; i < count; i++) {
+				if (data[i] == elem) return true;
+			}
 			return false;
 		}
-		T *new_data = new T[new_size];
 
-		if(size>0  &&  data) {
-			for(uint32 i = 0; i < count; i++) {
-				new_data[i] = data[i];
-			}
-			delete [] data;
-		}
-		size  = new_size;
-		data = new_data;
-		return true;
-	}
-
-	/**
-	 * Checks if element elem is contained in vector.
-	 * Uses the == operator for comparison.
-	 * @author Hj. Malthaner
-	 */
-	bool is_contained(T elem) const
-	{
-		for(uint32 i=0; i<count; i++) {
-			if(data[i] == elem) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-
-	/**
-	 * Appends the element at the end of the vector.
-	 * @author Hj. Malthaner
-	 */
-	bool append(T elem)
-	{
-		if(count >= size) {
-			dbg->fatal("vector_tpl<T>::append()","capacity %i exceeded.",size);
-			return false;
-		}
-		data[count ++] = elem;
-		return true;
-	}
-
-	/**
-	 * Appends the element at the end of the vector.
-	 * of out of spce, extend with this factor
-	 * @author prissi
-	 */
-	bool append(T elem,uint32 extend)
-	{
-		if(count>=size) {
-			if(!resize(count+extend)) {
-				dbg->fatal("vector_tpl<T>::append(,)","could not extend capacity %i.",size);
+		/** Appends the element at the end of the vector.  */
+		bool append(T elem)
+		{
+			if (count >= size) {
+				dbg->fatal("vector_tpl<T>::append()", "capacity %i exceeded.",size);
 				return false;
 			}
-		}
-		data[count ++] = elem;
-		return true;
-	}
-
-	/**
-	 * Checks if element is contained. Appends only new elements.
-	 *
-	 * @author Hj. Malthaner
-	 */
-	bool append_unique(T elem)
-	{
-		if(!is_contained(elem)) {
-			return append(elem);
-		}
-		return false;
-	}
-
-	/**
-	 * Checks if element is contained. Appends only new elements.
-	 * extend vector if nessesary
-	 * @author Hj. Malthaner
-	 */
-	bool append_unique(T elem,uint32 extend)
-	{
-		if(!is_contained(elem)) {
-			return append(elem,extend);
-		}
-		return false;
-	}
-
-
-	/**
-	* removes element, if contained
-	* @author prisse
-	*/
-	bool remove(T elem)
-	{
-		uint32 i,j;
-		for(i=j=0;  i<count;  i++,j++  ) {
-			if(data[i] == elem) {
-				// skip this one
-				j++;
-				count --;
-			}
-			// maybe we copy too often ...
-			if(j<size) {
-				data[i] = data[j];
-			}
-		}
-		return true;
-	}
-
-
-	/**
-	* insets data at a certain pos
-	* @author prissi
-	*/
-	bool insert_at(uint32 pos,T elem)
-	{
-		if(  pos<count  ) {
-			if(  count<size  ||  resize(count+1)) {
-				// ok, a valid position, make space
-				const long num_elements = (count-pos)*sizeof(T);
-				memmove( data+pos+1, data+pos, num_elements );
-				data[pos] = elem;
-				count ++;
-				return true;
-			}
-			else {
-				dbg->fatal("vector_tpl<T>::insert_at()","cannot insert at %i! Only %i elements.", pos, count);
-				return false;
-			}
-		}
-		else {
-			return append(elem,1);
-		}
-	}
-
-	/**
-	* put the data at a certain position
-	* use with care, may lead to unitialized entries
-	* @author prissi
-	*/
-	bool store_at(uint32 pos,T elem)
-	{
-		if(pos>=size) {
-			resize((pos&0xFFFFFFF7)+8);
-		}
-		// ok, a valid position, make space
-		data[pos] = elem;
-		if(pos>=count) {
-			count = pos+1;
-		}
-		return true;
-	}
-
-	/**
-	* removes element at position
-	* @author prissi
-	*/
-	bool remove_at(uint32 pos)
-	{
-		if(pos<size  &&  pos<count) {
-			uint32 i,j;
-			for(i=pos, j=pos+1;  j<count;  i++,j++  ) {
-				data[i] = data[j];
-			}
-			count --;
+			data[count++] = elem;
 			return true;
 		}
-		return false;
-	}
 
-
-	/**
-	 * Gets the element at position i
-	 * @author Hj. Malthaner
-	 */
-	const T& get(uint32 i) const
-	{
-		if(i>=count) {
-			dbg->fatal("vector_tpl<T>::get()","index out of bounds: %i not in 0..%d", i, count-1);
-			// will never go here
+		/**
+		 * Appends the element at the end of the vector.
+		 * if out of space, extend by this amount
+		 */
+		void append(T elem, uint32 extend)
+		{
+			if (count >= size) resize(count + extend);
+			data[count++] = elem;
 		}
-		return data[i];
-	}
 
-	/**
-	 * Accesses the element at position i
-	 * @author Hj. Malthaner
-	 */
-	T& at(uint32 i) const
-	{
-		if(i>=count) {
-			dbg->fatal("vector_tpl<T>::at()","index out of bounds: %i not in 0..%d\n", i, count-1);
-			// will never go here
+		/** Checks if element is contained. Appends only new elements. */
+		bool append_unique(T elem)
+		{
+			if (is_contained(elem)) return false;
+			return append(elem);
 		}
-		return data[i];
-	}
 
+		/**
+		 * Checks if element is contained. Appends only new elements.
+		 * extend vector if nessesary
+		 */
+		bool append_unique(T elem,uint32 extend)
+		{
+			if (is_contained(elem)) return false;
+			append(elem, extend);
+			return true;
+		}
 
-	/**
-	* Gets the number of elements in the vector.
-	* @author Hj. Malthaner
-	*/
-	uint32 get_count() const {return count;}
+		/** removes element, if contained */
+		bool remove(T elem)
+		{
+			uint32 i, j;
+			for (i = j = 0; i < count; i++, j++) {
+				if (data[i] == elem) {
+					// skip this one
+					j++;
+					count--;
+				}
+				// maybe we copy too often ...
+				if (j < size) {
+					data[i] = data[j];
+				}
+			}
+			return true;
+		}
 
+		/** insets data at a certain pos */
+		void insert_at(uint32 pos, T elem)
+		{
+			if (pos < count) {
+				if (count == size) resize(size + 1);
+				for (uint i = count; i > pos; i--) data[i] = data[i - 1];
+				data[pos] = elem;
+				count++;
+			} else {
+				append(elem, 1);
+			}
+		}
 
-	/**
-	* Gets the capacity.
-	* @author Hj. Malthaner
-	*/
-	uint32 get_size() const {return size;}
+		/**
+		 * put the data at a certain position
+ 		 * BEWARE: using this function will create default objects, depending on
+		 * the type of the vector
+		 */
+		void store_at(uint32 pos, T elem)
+		{
+			if (pos >= size) resize((pos & 0xFFFFFFF7) + 8);
+			data[pos] = elem;
+			if (pos >= count) count = pos + 1;
+		}
+
+		/** removes element at position */
+		bool remove_at(uint32 pos)
+		{
+			if (pos < count) {
+				for (uint i = pos; i < count; i++) data[i] = data[i + 1];
+				count--;
+				return true;
+			}
+			return false;
+		}
+
+		/** Gets the element at position i */
+		const T& get(uint32 i) const
+		{
+			if (i >= count) {
+				dbg->fatal("vector_tpl<T>::get()", "index out of bounds: %i not in 0..%d", i, count - 1);
+			}
+			return data[i];
+		}
+
+		/** Accesses the element at position i */
+		T& at(uint32 i) const
+		{
+			if (i >= count) {
+				dbg->fatal("vector_tpl<T>::at()", "index out of bounds: %i not in 0..%d\n", i, count - 1);
+			}
+			return data[i];
+		}
+
+		/** Get the number of elements in the vector */
+		uint32 get_count() const { return count; }
+
+		/** Get the capacity */
+		uint32 get_size() const { return size; }
+
+	private:
+		vector_tpl(const vector_tpl&);
+
+		T* data;
+		uint32 size;  ///< Capacity
+		uint32 count; ///< Number of elements in vector
 
 
 	friend void swap<>(vector_tpl<T>& a, vector_tpl<T>& b);
