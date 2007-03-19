63dc12c3e68aff7d9dfacdf4ebc16c96afa0ba86
Markus Pristovsek <prissi@physik.tu-berlin.de>
2007-03-19 21:37:59 +0000

    jump frame dialoge source
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@742 8aca7d54-2c30-db11-9de9-000461428c89

---
+++ gui/jump_frame.cc
@@ -0,0 +1,67 @@
+/*
+ * jump_frame.cc
+ *
+ * Copyright (c) 1997 - 2001 Hansjörg Malthaner
+ *
+ * This file is part of the Simutrans project and may not be used
+ * in other projects without written permission of the author.
+ *
+ * jumps to a position
+ */
+
+#include <string.h>
+
+#include "../simdebug.h"
+#include "../simworld.h"
+#include "jump_frame.h"
+#include "components/gui_button.h"
+#include "components/list_button.h"
+
+
+jump_frame_t::jump_frame_t(karte_t *welt) :
+	gui_frame_t("Jump to")
+{
+	this->welt = welt;
+
+	// Input box for new name
+	sprintf(buf, "%i,%i", welt->gib_ij_off().x, welt->gib_ij_off().y );
+	input.setze_text(buf, 62);
+	input.add_listener(this);
+	input.setze_pos(koord(10,4));
+	input.setze_groesse(koord(BUTTON_WIDTH, 14));
+	add_komponente(&input);
+
+	divider1.setze_pos(koord(10,24));
+	divider1.setze_groesse(koord(BUTTON_WIDTH,0));
+	add_komponente(&divider1);
+
+	jumpbutton.init( button_t::roundbox, "Jump to", koord(10, 28), koord( BUTTON_WIDTH,BUTTON_HEIGHT ) );
+	jumpbutton.add_listener(this);
+	add_komponente(&jumpbutton);
+
+	setze_opaque(true);
+	setze_fenstergroesse(koord(BUTTON_WIDTH+20, 62));
+}
+
+
+
+/**
+ * This method is called if an action is triggered
+ * @author V. Meyer
+ */
+bool jump_frame_t::action_triggered(gui_komponente_t *komp,value_t /* */)
+{
+	if(komp == &input || komp == &jumpbutton) {
+		// OK- Button or Enter-Key pressed
+		//---------------------------------------
+		koord my_pos;
+		sscanf(buf, "%hd,%hd", &my_pos.x, &my_pos.y);
+		if(welt->ist_in_kartengrenzen(my_pos)) {
+			welt->setze_ij_off(koord3d(my_pos,welt->min_hgt(my_pos)));
+		}
+	}
+	return true;
+}
+
+
+
---
+++ gui/jump_frame.h
@@ -0,0 +1,56 @@
+/*
+ * jump_frame.h
+ *
+ * Copyright (c) 1997 - 2001 Hansjörg Malthaner
+ *
+ * This file is part of the Simutrans project and may not be used
+ * in other projects without written permission of the author.
+ *
+ * Maintains the list of custon labels assigned to different palces on the map
+ */
+
+
+#ifndef gui_jumpframe_h
+#define gui_jumpframe_h
+
+
+#include "../tpl/slist_tpl.h"
+#include "components/action_listener.h"
+#include "gui_frame.h"
+#include "components/gui_textinput.h"
+#include "components/gui_divider.h"
+#include "components/gui_button.h"
+
+
+class karte_t;
+
+class jump_frame_t : public gui_frame_t, action_listener_t
+{
+	char buf[64];
+	gui_textinput_t input;
+	gui_divider_t divider1;
+	button_t jumpbutton;
+	karte_t *welt;
+
+public:
+	jump_frame_t(karte_t *welt);
+
+	/**
+	* Manche Fenster haben einen Hilfetext assoziiert.
+	* @return den Dateinamen für die Hilfe, oder NULL
+	* @author Hj. Malthaner
+	*/
+	const char * gib_hilfe_datei() const { return "jump_frame.txt"; }
+
+	/**
+	 * This method is called if an action is triggered
+	 * @author Hj. Malthaner
+	 *
+	 * Returns true, if action is done and no more
+	 * components should be triggered.
+	 * V.Meyer
+	 */
+	bool action_triggered(gui_komponente_t *komp, value_t extra);
+};
+
+#endif
