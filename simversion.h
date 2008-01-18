2fa5ac81e8bec1e767e08416f007184b2935b86e
Markus Pristovsek <prissi@physik.tu-berlin.de>
2008-01-18 20:22:26 +0000

    todo changed
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@1555 8aca7d54-2c30-db11-9de9-000461428c89

--- simversion.h
+++ simversion.h
@@ -3,8 +3,8 @@
 
 #define MAKEOBJ_VERSION "49"
 
-#define VERSION_NUMBER "99.17.1"
-#define WIDE_VERSION_NUMBER L"99.17.1"
+#define VERSION_NUMBER "99.18 nightly"
+#define WIDE_VERSION_NUMBER L"99.18 nightly"
 
 #define VERSION_DATE __DATE__
 
@@ -13,6 +13,6 @@
 #define SAVEGAME_VER_NR  "0.99.16"
 #define SAVEGAME_VERSION (SAVEGAME_PREFIX SAVEGAME_VER_NR)
 
-#define RES_VERSION_NUMBER  0, 99, 17, 1
+#define RES_VERSION_NUMBER  0, 99, 18, 0
 
 #endif
