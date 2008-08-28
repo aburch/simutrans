0691cf61edc529c483b4a4616a529ad1b2a4696e
Markus Pristovsek <prissi@physik.tu-berlin.de>
2008-08-28 20:35:37 +0000

    FIX: .. and crossings
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@1976 8aca7d54-2c30-db11-9de9-000461428c89

--- bauer/wegbauer.cc
+++ bauer/wegbauer.cc
@@ -1368,8 +1368,9 @@ wegbauer_t::intern_calc_straight_route(const koord3d start, const koord3d ziel)
 		if(bautyp&tunnel_flag) {
 			grund_t *bd_von = welt->lookup(koord3d(pos,start.z));
 			ok = (bd_von==NULL)  ||  bd_von->gib_typ()==grund_t::tunnelboden;
-			// check for halt crossing ...
-			if(ok  &&  bd_von  &&  bd_von->is_halt()) {
+			// check for halt or crossing ...
+			if(ok  &&  bd_von  &&  (bd_von->is_halt()  ||  bd_von->has_two_ways())) {
+				// then only single dir is ok ...
 				ribi_t::ribi haltribi = bd_von->gib_weg_ribi_unmasked( (waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)) );
 				haltribi = ribi_t::doppelt(haltribi);
 				ribi_t::ribi diffribi = ribi_t::doppelt( ribi_typ(diff) );
