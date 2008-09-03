3a1f9301f4b348b36457903f9ebecb889f70e8ff
Markus Pristovsek <prissi@physik.tu-berlin.de>
2008-09-03 15:16:57 +0000

    raloading diagonal hopefully fixed (but who knows)
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@1992 8aca7d54-2c30-db11-9de9-000461428c89

--- vehicle/simvehikel.cc
+++ vehicle/simvehikel.cc
@@ -1266,10 +1266,9 @@ DBG_MESSAGE("vehicle_t::rdwr()","bought at %i/%i.",(insta_zeit%12)+1,insta_zeit/
 		else {
 			file->rdwr_byte(steps, " ");
 			file->rdwr_byte(steps_next, "\n");
-			if(steps_next!=255   &&  file->is_loading()) {
+			if(steps_next==old_diagonal_length  &&  file->is_loading()) {
 				// reset diagonal length (convoi will be resetted anyway, if game diagonal is different)
 				steps_next = diagonal_length;
-				steps = (uint8)(((uint16)steps*1024u)/724u);
 			}
 		}
 		sint16 dummy16 = ((16*(sint16)hoff)/TILE_HEIGHT_STEP);
