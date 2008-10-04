55bbf38041b2eae40d356932369f83cebbe75f28
Markus Pristovsek <prissi@physik.tu-berlin.de>
2008-10-04 19:55:34 +0000

    some comments to image_reader.cc
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2056 8aca7d54-2c30-db11-9de9-000461428c89

--- besch/reader/image_reader.cc
+++ besch/reader/image_reader.cc
@@ -117,6 +117,7 @@ obj_besch_t *image_reader_t::read_node(FILE *fp, obj_node_info_t &node)
 		// get the adler hash (since we have zlib on board anyway ... )
 		bool do_register_image = true;
 		uint32 adler = adler32(0L, NULL, 0 );
+		// remember pic.len is sizeof(uint16)!
 		adler = adler32(adler, (const Bytef *)(besch->pic.data), besch->pic.len*2 );
 		static inthashtable_tpl<uint32, bild_besch_t *> images_adlers;
 		bild_besch_t *same = images_adlers.get(adler);
@@ -125,13 +126,16 @@ obj_besch_t *image_reader_t::read_node(FILE *fp, obj_node_info_t &node)
 			besch->pic.bild_nr = same->pic.bild_nr;
 			do_register_image = memcmp( besch->get_pic(), same->get_pic(), sizeof(bild_t)+(besch->pic.len*2) )!=0;
 		}
+		// unique image here
 		if(  do_register_image  ) {
 			if(!same) {
 				images_adlers.put(adler,besch);	// still with bild_nr == IMG_LEER!
 			}
+			// register image adds this image to the internal array maintained by simgraph??.cc
 			register_image( &(besch->pic) );
 		}
 		else {
+			// no need to load doubles ...
 			delete besch;
 			besch = same;
 		}
