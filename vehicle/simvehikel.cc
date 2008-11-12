52a654d9967b7d9d08675f1110e27345d7607ed7
Markus Pristovsek <prissi@physik.tu-berlin.de>
2008-11-12 15:48:10 +0000

    fix of broken savegames with vehciles in depots
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2114 8aca7d54-2c30-db11-9de9-000461428c89

--- vehicle/simvehikel.cc
+++ vehicle/simvehikel.cc
@@ -584,8 +584,7 @@ vehikel_t::rotate90()
 
 
 
-void
-vehikel_t::setze_convoi(convoi_t *c)
+void vehikel_t::setze_convoi(convoi_t *c)
 {
 	/* cnv can have three values:
 	 * NULL: not previously assigned
@@ -605,7 +604,13 @@ vehikel_t::setze_convoi(convoi_t *c)
 		}
 		// some convois were saved with broken coordinates
 		if(!welt->lookup(pos_prev)) {
-			pos_prev = welt->lookup_kartenboden(pos_prev.gib_2d())->gib_pos();
+			dbg->error("vehikel_t::setze_convoi()","pos_prev is illegal of convoi %i at %s", cnv->self.get_id(), gib_pos().gib_str() );
+			if(welt->lookup_kartenboden(pos_prev.gib_2d())) {
+				pos_prev = welt->lookup_kartenboden(pos_prev.gib_2d())->gib_pos();
+			}
+			else {
+				pos_prev = gib_pos();
+			}
 		}
 		if(pos_next!=koord3d::invalid  &&  !cnv->get_route()->empty()  &&  route_index<cnv->get_route()->gib_max_n()  &&  (welt->lookup(pos_next)==NULL  ||  welt->lookup(pos_next)->gib_weg(gib_waytype())==NULL)) {
 			pos_next = cnv->get_route()->position_bei(route_index+1u);
