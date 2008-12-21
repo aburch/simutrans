3d07d7f85ae414a7a6ce8b20ada9d4f8ae27be56
Markus Pristovsek <prissi@physik.tu-berlin.de>
2008-12-21 23:46:56 +0000

    distribution.sh updated to new location
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2177 8aca7d54-2c30-db11-9de9-000461428c89

--- distribute.sh
+++ distribute.sh
@@ -30,11 +30,11 @@ cd text
 # get the translations for basis
 # The first file is longer, but only because it contains SQL error messages
 # - discard it after complete download (although parsing it would give us the archive's name):
-wget --post-data "version=4&choice=all&submit=Export!" --delete-after http://translator.simutrans.com/script/wrap.php || {
+wget --post-data "version=0&choice=all&submit=Export!" --delete-after http://www.simutrans-germany.com/translator/script/wrap.php || {
   echo "Error: generating file language_pack-Base+texts.zip failed (wget returned $?)" >&2;
   exit 3;
 }
-wget -N http://translator.simutrans.com/tmp/language_pack-Base+texts.zip || {
+wget -N http://www.simutrans-germany.com/translator/data/tab/language_pack-Base+texts.zip || {
   echo "Error: download of file language_pack-Base+texts.zip failed (wget returned $?)" >&2
   rm -f "language_pack-Base+texts.zip"
   exit 4
