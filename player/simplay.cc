8f1ac82bfa999b7afdd2b6980b495bffa2b987ce
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-01-31 20:42:25 +0000

    ADD: no vehicle/cash sounds during fast forward
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2277 8aca7d54-2c30-db11-9de9-000461428c89

--- player/simplay.cc
+++ player/simplay.cc
@@ -423,7 +423,7 @@ void spieler_t::buche(const sint64 betrag, const koord pos, enum player_cost typ
 			add_message(pos, betrag);
 		}
 
-		if(!(labs((sint32)betrag)<=10000)) {
+		if(  !(labs((sint32)betrag)<=10000)  &&  !welt->is_fast_forward()  ) {
 			struct sound_info info;
 
 			info.index = SFX_CASH;
--- vehicle/simvehikel.cc
+++ vehicle/simvehikel.cc
@@ -806,7 +806,7 @@ void vehikel_t::remove_stale_freight()
 void
 vehikel_t::play_sound() const
 {
-	if(besch->get_sound() >= 0) {
+	if(  besch->get_sound() >= 0  &&  !welt->is_fast_forward()  ) {
 		struct sound_info info;
 		info.index = besch->get_sound();
 		info.volume = 255;
