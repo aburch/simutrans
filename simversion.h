74256124ba35b817de587bd882e9e9b9e0806a78
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-02-07 21:51:01 +0000

    savegame now 101.1
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2308 8aca7d54-2c30-db11-9de9-000461428c89

--- simversion.h
+++ simversion.h
@@ -11,9 +11,9 @@
 #define SAVEGAME_PREFIX  "Simutrans "
 #define XML_SAVEGAME_PREFIX  "<?xml version=\"1.0\"?>"
 
-#define SAVEGAME_VER_NR  "0.101.0"
+#define SAVEGAME_VER_NR  "0.101.1"
 #define SAVEGAME_VERSION (SAVEGAME_PREFIX SAVEGAME_VER_NR)
 
-#define RES_VERSION_NUMBER  0, 101, 0, 0
+#define RES_VERSION_NUMBER  0, 101, 1, 0
 
 #endif
