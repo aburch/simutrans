b7c5ca3fdb7a23d1ce0973fd488ce93279450951
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-02-12 15:21:45 +0000

    (z9999) station window patch
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2323 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/station_building_select.cc
+++ gui/station_building_select.cc
@@ -40,14 +40,20 @@ station_building_select_t::station_building_select_t(karte_t *welt, const haus_b
 	int layout = besch->get_all_layouts();
 	int row = (layout > 2) ? 1 : 0;
 	sint16 rw = get_base_tile_raster_width()/4;
-	int y_diff = (besch->get_b(0)==1) ? ((besch->get_h(0)==1) ? rw*4 : rw*5) : ((besch->get_h(0)==1) ? rw*5 : rw*6) ;
-	const koord img_offsets[4]={ koord(rw*2,0), koord(0,rw), koord(rw*4,rw), koord(rw*2,rw*2) };
-	const koord base_offsets[6]={ koord(0,0), koord(rw*8+10,0), koord(0,y_diff+30),  koord(rw*8+10,y_diff+30), koord(rw*8+10,0), koord(0,0) };
+	int width = (besch->get_b(0)==1) ? ((besch->get_h(0)==1) ? rw*4 : rw*6) : ((besch->get_h(0)==1) ? rw*6 : rw*8);
+	int x_diff = (width==rw*8) ? 0 : ((BUTTON_WIDTH>width) ? min((BUTTON_WIDTH-width)/2,rw*2) : rw*2);
+	width = max(BUTTON_WIDTH,width);
+	int height = (besch->get_b(0)==1) ? ((besch->get_h(0)==1) ? rw*4 : rw*5) : ((besch->get_h(0)==1) ? rw*5 : rw*6);
+	const koord img_offsets[4]={ koord(rw*2-x_diff,0), koord(-x_diff,rw), koord(rw*4-x_diff,rw), koord(rw*2-x_diff,rw*2) };
+	const koord base_offsets[6]={ koord(0,0), koord(width+10,0), koord(0,height+BUTTON_HEIGHT+10),  koord(width+10,height+BUTTON_HEIGHT+10), koord(width+10,0), koord(0,0) };
 
 	// image placeholder
 	for( sint16 i=0;  i<layout;  i++ ) {
 		for( sint16 j=0;  j<4;  j++ ) {
 			koord pos = img_offsets[j]+base_offsets[i+row*2];
+			if((height==rw*5)  &&  ((i&1  &&  besch->get_h(0)==1)  ||  (!i&1  &&  besch->get_b(0)==1))) {
+				pos.x = pos.x + x_diff;
+			}
 			img[i*4+j].set_pos( pos );
 			img[i*4+j].set_image(IMG_LEER);
 			img[i*4+j].set_pos( pos );
@@ -85,11 +91,11 @@ station_building_select_t::station_building_select_t(karte_t *welt, const haus_b
 
 	// button
 	for(int i=0; i<layout; i++) {
-		actionbutton[i].init( button_t::roundbox, translator::translate(label_text[i]), base_offsets[i+row*2]+koord(rw*4-BUTTON_WIDTH/2, y_diff), koord( BUTTON_WIDTH,BUTTON_HEIGHT ) );
+		actionbutton[i].init( button_t::roundbox, translator::translate(label_text[i]), base_offsets[i+row*2]+koord((width-BUTTON_WIDTH)/2, height), koord( BUTTON_WIDTH,BUTTON_HEIGHT ) );
 		actionbutton[i].add_listener(this);
 		add_komponente(&actionbutton[i]);
 	}
-	set_fenstergroesse(koord(rw*16+10, (y_diff+30)*(row+1)+10));
+	set_fenstergroesse(koord(width*2+10, (height+BUTTON_HEIGHT+10)*(row+1)+10));
 }
 
 
