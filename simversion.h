3d171305a9bd8588828564980e49de0faa1baeab
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-03-06 23:14:00 +0000

    Release 102.0 with correct help text and version number
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2369 8aca7d54-2c30-db11-9de9-000461428c89

--- simversion.h
+++ simversion.h
@@ -3,8 +3,8 @@
 
 #define MAKEOBJ_VERSION "49"
 
-#define VERSION_NUMBER "102 Nightly"
-#define WIDE_VERSION_NUMBER L"102 Nightly"
+#define VERSION_NUMBER "102.0"
+#define WIDE_VERSION_NUMBER L"102.0"
 
 #define VERSION_DATE __DATE__
 
