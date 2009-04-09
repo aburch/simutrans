3cc7e3cebdc80722715da7d284884328be65fbf1
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-04-09 20:55:45 +0000

    ADD: completly ignore message button at beginning of dialoge
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2415 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/message_option_t.h
+++ gui/message_option_t.h
@@ -1,6 +1,8 @@
 #ifndef message_option_h
 #define message_option_h
 
+#include "../simmesg.h"
+
 #include "gui_frame.h"
 #include "components/action_listener.h"
 #include "components/gui_button.h"
@@ -9,36 +11,35 @@
 
 class karte_t;
 
-#define MAX_MESSAGE_TYP (10)
-
 class message_option_t : public gui_frame_t, private action_listener_t
 {
 private:
-    gui_textarea_t text_label;
-    button_t buttons[3*MAX_MESSAGE_TYP];
-    gui_image_t legend;
-    sint32 ticker_msg, window_msg, auto_msg, ignore_msg;
+	gui_textarea_t text_label;
+	button_t buttons[4*(message_t::MAX_MESSAGE_TYPE-1)];
+	gui_image_t legend;
+	sint32 ticker_msg, window_msg, auto_msg, ignore_msg;
+
 	static karte_t *welt;
 
 public:
 	message_option_t(karte_t *welt);
 
-    /**
-     * Manche Fenster haben einen Hilfetext assoziiert.
-     * @return den Dateinamen für die Hilfe, oder NULL
-     * @author Hj. Malthaner
-     */
-    const char * get_hilfe_datei() const {return "mailbox.txt";}
+	/**
+	 * Manche Fenster haben einen Hilfetext assoziiert.
+	 * @return den Dateinamen für die Hilfe, oder NULL
+	 * @author Hj. Malthaner
+	 */
+	const char * get_hilfe_datei() const {return "mailbox.txt";}
 
-    /**
-     * This method is called if an action is triggered
-     * @author Hj. Malthaner
-     *
-     * Returns true, if action is done and no more
-     * components should be triggered.
-     * V.Meyer
-     */
-    bool action_triggered( gui_action_creator_t *komp, value_t extra);
+	/**
+	 * This method is called if an action is triggered
+	 * @author Hj. Malthaner
+	 *
+	 * Returns true, if action is done and no more
+	 * components should be triggered.
+	 * V.Meyer
+	 */
+	bool action_triggered( gui_action_creator_t *komp, value_t extra);
 };
 
 #endif
