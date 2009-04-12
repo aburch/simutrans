c0edd161e1fdc36ea6c2cc0bc3d849f88c11b11e
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-04-12 20:33:55 +0000

    Again 10 rows of buttons for messge options
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2416 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/message_option_t.cc
+++ gui/message_option_t.cc
@@ -38,7 +38,7 @@ message_option_t::message_option_t(karte_t *welt) :
 
 	welt->get_message()->get_message_flags( &ticker_msg, &window_msg, &auto_msg, &ignore_msg );
 
-	for(  int i=0;  i<message_t::MAX_MESSAGE_TYPE-1;  i++  ) {
+	for(  int i=0;  i<message_t::MAX_MESSAGE_TYPE;  i++  ) {
 		buttons[i*4].set_pos( koord(10,18+i*2*LINESPACE) );
 		buttons[i*4].set_typ(button_t::square_state);
 		buttons[i*4].pressed = ((ignore_msg>>i)&1)==0;
@@ -72,7 +72,7 @@ bool
 message_option_t::action_triggered( gui_action_creator_t *komp, value_t )
 {
 	((button_t*)komp)->pressed ^= 1;
-	for(  int i=0;  i<message_t::MAX_MESSAGE_TYPE-1;  i++  ) {
+	for(  int i=0;  i<message_t::MAX_MESSAGE_TYPE;  i++  ) {
 		if(&buttons[i*4+1]==komp) {
 			ignore_msg ^= (1<<i);
 		}
