fca5ebfe57953adf1cc2c00683563fadbe8183f0
James E. Petts <jamespetts@yahoo.com>
2009-04-30 20:33:40 +0100

    FIX: Missing files for automatic signal building from previous upload.

---
+++ gui/signal_spacing.h
@@ -0,0 +1,48 @@
+/*
+ * Dialogue to set the signal spacing.
+ * Used by wkz_roadsign_t
+ */
+
+#ifndef signal_spacing_h
+#define signal_spacing_h
+
+#include "gui_frame.h"
+#include "components/action_listener.h"
+
+class gui_numberinput_t;
+class button_t;
+class gui_label_t;
+class wkz_roadsign_t;
+
+class signal_spacing_frame_t : public gui_frame_t, private action_listener_t
+{
+	private:
+		wkz_roadsign_t* tool;
+		gui_numberinput_t signal_spacing_inp;
+		static uint8 signal_spacing;
+		gui_label_t signal_label;
+		button_t remove_button, replace_button;
+		static bool remove, replace;
+		button_t cancel_button, ok_button;
+	public:
+		signal_spacing_frame_t( wkz_roadsign_t * );
+
+	/**
+	 * Events werden hiermit an die GUI-Komponenten
+	 * gemeldet
+	 * @author Hj. Malthaner
+	 */
+	void infowin_event(const event_t *ev);
+
+	/**
+	 * This method is called if an action is triggered
+	 * @author Hj. Malthaner
+	 *
+	 * Returns true, if action is done and no more
+	 * components should be triggered.
+	 * V.Meyer
+	 */
+	bool action_triggered( gui_action_creator_t *komp, value_t extra);
+};
+
+#endif
