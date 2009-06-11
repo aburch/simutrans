7e9a01503685812a5ae58c6cc1fefe587f40c95b
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-06-11 21:02:45 +0000

    freelist will never return something smaller than 8 byte
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2510 8aca7d54-2c30-db11-9de9-000461428c89

--- dataobj/freelist.cc
+++ dataobj/freelist.cc
@@ -65,6 +65,7 @@ void *freelist_t::gimme_node(size_t size)
 
 	// all sizes should be dividable by 4
 	size = ((size+3)>>2)<<2;
+	size = min( 8, size );
 
 	// hold return value
 	nodelist_node_t *tmp;
@@ -148,6 +149,7 @@ void freelist_t::putback_node( size_t size, void *p )
 
 	// all sizes should be dividable by 4
 	size = ((size+3)>>2);
+	size = min( 2, size );
 
 	if(size>MAX_LIST_INDEX) {
 		switch(size) {
