aa26bce06493504cb6f59306ecdf6aa446cee3da
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-07-01 13:02:37 +0000

    connect everything as public player also underground
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2569 8aca7d54-2c30-db11-9de9-000461428c89

--- bauer/wegbauer.cc
+++ bauer/wegbauer.cc
@@ -1319,7 +1319,7 @@ wegbauer_t::intern_calc_straight_route(const koord3d start, const koord3d ziel)
 			bool must_be_straight = false;
 			grund_t *bd_von = welt->lookup(pos);
 			if(  bd_von  ) {
-				ok = bd_von->get_typ()==grund_t::tunnelboden  &&  bd_von->hat_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))  &&  spieler_t::check_owner(bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))->get_besitzer(),sp);
+				ok = bd_von->get_typ()==grund_t::tunnelboden  &&  bd_von->hat_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))  &&  check_owner(bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))->get_besitzer(),sp);
 				// if we have a slope, we must adjust height correspondingly
 				if(  bd_von->get_weg_hang()!=hang_t::flach  ) {
 					if(  ribi_typ(bd_von->get_weg_hang())==ribi_typ(diff)  ) {
@@ -1332,7 +1332,7 @@ wegbauer_t::intern_calc_straight_route(const koord3d start, const koord3d ziel)
 				// check for slope down ...
 				bd_von = welt->lookup(pos+koord3d(0,0,-Z_TILE_STEP));
 				if(  bd_von  &&  bd_von->get_weg_hang()!=hang_t::flach) {
-					ok = bd_von->get_typ() == grund_t::tunnelboden  &&  bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))  &&  spieler_t::check_owner(bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))->get_besitzer(),sp)  &&  ribi_typ(bd_von->get_weg_hang())==ribi_t::rueckwaerts(ribi_typ(diff));
+					ok = bd_von->get_typ() == grund_t::tunnelboden  &&  bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))  &&  check_owner(bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))->get_besitzer(),sp)  &&  ribi_typ(bd_von->get_weg_hang())==ribi_t::rueckwaerts(ribi_typ(diff));
 					if(  ok  ) {
 						route[route.get_count()-1].z -= 1;
 						pos.z -= Z_TILE_STEP;
@@ -1351,7 +1351,7 @@ wegbauer_t::intern_calc_straight_route(const koord3d start, const koord3d ziel)
 			if(  ok  &&  pos.get_2d()+diff==ziel.get_2d()  ) {
 				grund_t *bd_von = welt->lookup(koord3d(ziel.get_2d(),pos.z));
 				if(  bd_von  ) {
-					ok = bd_von->get_typ()==grund_t::tunnelboden  &&  bd_von->hat_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))  &&  spieler_t::check_owner(bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))->get_besitzer(),sp);
+					ok = bd_von->get_typ()==grund_t::tunnelboden  &&  bd_von->hat_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))  &&  check_owner(bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))->get_besitzer(),sp);
 					// slope present?
 					ok = ok && (bd_von->get_vmove(-diff)==pos.z);
 				}
@@ -1368,7 +1368,7 @@ wegbauer_t::intern_calc_straight_route(const koord3d start, const koord3d ziel)
 				ok &= ok  &&  (!gr->ist_wasser()  ||  min( welt->lookup_hgt(pos.get_2d()+diff), welt->get_grundwasser() ) > pos.z);
 				// check for slope down ...
 				bd_von = welt->lookup(pos+diff+koord3d(0,0,-Z_TILE_STEP));
-				ok &= bd_von==NULL  ||  (bd_von->get_typ()==grund_t::tunnelboden  &&  bd_von->hat_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))  &&  spieler_t::check_owner(bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))->get_besitzer(),sp));
+				ok &= bd_von==NULL  ||  (bd_von->get_typ()==grund_t::tunnelboden  &&  bd_von->hat_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))  &&  check_owner(bd_von->get_weg((waytype_t)(bautyp&(~wegbauer_t::tunnel_flag)))->get_besitzer(),sp));
 				if(ok &&  bd_von  &&  bd_von->get_weg_hang()!=hang_t::flach) {
 					ok = bd_von->get_typ() == grund_t::tunnelboden  &&  bd_von->get_weg_nr(0)->get_waytype() == besch->get_wtyp()  &&  ribi_typ(bd_von->get_weg_hang())==ribi_t::rueckwaerts(ribi_typ(diff));
 					if (ok) { // adjust height again
