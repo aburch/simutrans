3c51db2e88f60134fcb686fd6959778fe9572f30
Dwachs <dwachs@gmx.net>
2009-09-16 11:00:51 +0000

    FIX: public player cannot built transformers on factorie buildings
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2646 8aca7d54-2c30-db11-9de9-000461428c89

--- simwerkz.cc
+++ simwerkz.cc
@@ -1178,7 +1178,7 @@ const char *wkz_transformer_t::work( karte_t *welt, spieler_t *sp, koord3d k )
 {
 DBG_MESSAGE("wkz_senke()","called on %d,%d", k.x, k.y);
 	grund_t *gr=welt->lookup_kartenboden(k.get_2d());
-	if(gr  && gr->is_visible() && gr->get_grund_hang()==0  &&  !gr->ist_wasser()  &&  !gr->hat_wege()  &&  gr->kann_alle_obj_entfernen(sp)==NULL) {
+	if(gr  && gr->is_visible() && gr->get_grund_hang()==0  &&  !gr->ist_wasser()  &&  !gr->hat_wege()  &&  gr->kann_alle_obj_entfernen(sp)==NULL  &&  gr->find<gebaeude_t>()==NULL) {
 		fabrik_t *fab=leitung_t::suche_fab_4(k.get_2d());
 		if(fab==NULL) {
 			return "Transformer only next to factory!";
