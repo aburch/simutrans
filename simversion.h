3bd9fc703972277d602c0031a1878c5ba689a14d
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-10-15 20:20:07 +0000

    updated savegame version: new games are no longer compatible with 102.1
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2777 8aca7d54-2c30-db11-9de9-000461428c89

--- simversion.h
+++ simversion.h
@@ -3,15 +3,15 @@
 
 #define MAKEOBJ_VERSION "50"
 
-#define VERSION_NUMBER "102.1 Nightly"
-#define WIDE_VERSION_NUMBER L"102.1 Nightly"
+#define VERSION_NUMBER "102.2 Nightly"
+#define WIDE_VERSION_NUMBER L"102.2 Nightly"
 
 #define VERSION_DATE __DATE__
 
 #define SAVEGAME_PREFIX  "Simutrans "
 #define XML_SAVEGAME_PREFIX  "<?xml version=\"1.0\"?>"
 
-#define SAVEGAME_VER_NR  "0.102.1"
+#define SAVEGAME_VER_NR  "0.102.2"
 #define SAVEGAME_VERSION (SAVEGAME_PREFIX SAVEGAME_VER_NR)
 
 #define RES_VERSION_NUMBER  0, 102, 0, 0
