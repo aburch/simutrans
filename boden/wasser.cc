ed4db06f1387da8bafcee5b89a5f4238e542f241
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-11-04 20:41:29 +0000

    TOo much copied for water animation
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2845 8aca7d54-2c30-db11-9de9-000461428c89

--- boden/wasser.cc
+++ boden/wasser.cc
@@ -29,7 +29,7 @@ void wasser_t::prepare_for_refresh()
 {
 	if(!welt->is_fast_forward()  &&  umgebung_t::water_animation>0) {
 		int new_stage = (welt->get_zeit_ms() / umgebung_t::water_animation) % grund_besch_t::water_animation_stages;
-		wasser_t::change_stage |= (new_stage != stage);
+		wasser_t::change_stage = (new_stage != stage);
 		wasser_t::stage = new_stage;
 	}
 	else {
@@ -39,8 +39,7 @@ void wasser_t::prepare_for_refresh()
 
 
 
-bool
-wasser_t::zeige_info()
+bool wasser_t::zeige_info()
 {
 	if(get_halt().is_bound()) {
 		get_halt()->zeige_info();
