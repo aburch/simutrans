29fe5530f504b0f0d1d7b01ac0cb4409d33e76df
Markus Pristovsek <prissi@physik.tu-berlin.de>
2009-11-21 22:26:56 +0000

    sorting without assertions also for UTF8 line names
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@2887 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/schedule_list.cc
+++ gui/schedule_list.cc
@@ -68,31 +68,61 @@ static uint8 statistic_type[MAX_LINE_COST]={
 	STANDARD, STANDARD, MONEY, MONEY, MONEY, STANDARD, STANDARD
 };
 
+enum sort_modes_t { SORT_BY_NAME=0, SORT_BY_ID, SORT_BY_PROFIT, SORT_BY_TRANSPORTED, SORT_BY_CONVOIS, SORT_BY_DISTANCE, MAX_SORT_MODES };
+static const char *sort_text[MAX_SORT_MODES] = {
+    "cl_btn_sort_name",
+    "cl_btn_sort_id",
+    "Profit",
+	"Transported",
+	"Convoys",
+	"Distance"
+};
+static uint8 current_sort_mode = 0;
+
 #define LINE_NAME_COLUMN_WIDTH ((BUTTON_WIDTH*3)+11+11)
 #define SCL_HEIGHT (170)
 
 
 static bool compare_lines(line_scrollitem_t* a, line_scrollitem_t* b)
 {
+	switch(  current_sort_mode  ) {
+		case SORT_BY_NAME:	// default
+			break;
+		case SORT_BY_ID:
+			return (a->get_line().get_id(),b->get_line().get_id())<0;
+		case SORT_BY_PROFIT:
+			return (a->get_line()->get_finance_history(1,LINE_PROFIT) - b->get_line()->get_finance_history(1,LINE_PROFIT))<0;
+		case SORT_BY_TRANSPORTED:
+			return (a->get_line()->get_finance_history(1,LINE_TRANSPORTED_GOODS) - b->get_line()->get_finance_history(1,LINE_TRANSPORTED_GOODS))<0;
+		case SORT_BY_CONVOIS:
+			return (a->get_line()->get_finance_history(1,LINE_CONVOIS) - b->get_line()->get_finance_history(1,LINE_CONVOIS))<0;
+		case SORT_BY_DISTANCE:
+			// normalizing to the number of convoys to get the fastest ones ...
+			return (a->get_line()->get_finance_history(1,LINE_DISTANCE)/max(1,a->get_line()->get_finance_history(1,LINE_CONVOIS)) -
+			        b->get_line()->get_finance_history(1,LINE_DISTANCE)/max(1,b->get_line()->get_finance_history(1,LINE_CONVOIS)) )<0;
+	}
+	// default sorting ...
+
 	// first: try to sort by number
 	const char *atxt = a->get_text();
 	int aint = 0;
-	if(  isdigit(atxt[0])  ) {
+	// isdigit produces with UTF8 assertions ...
+	if(  atxt[0]>='0'  &&  atxt[0]<='9'  ) {
 		aint = atoi( atxt );
 	}
-	else if(  isdigit(atxt[1])  ) {
+	else if(  atxt[1]>='0'  &&  atxt[1]<='9'  ) {
 		aint = atoi( atxt+1 );
 	}
 	const char *btxt = b->get_text();
 	int bint = 0;
-	if(  isdigit(btxt[0])  ) {
+	if(  btxt[0]>='0'  &&  btxt[0]<='9'  ) {
 		bint = atoi( btxt );
 	}
-	else if(  isdigit(btxt[1])  ) {
+	else if(  btxt[1]>='0'  &&  btxt[1]<='9'  ) {
 		bint = atoi( btxt+1 );
 	}
 	if(  aint!=bint  ) {
-		return aint-bint<0;
+		return (aint-bint)<0;
 	}
 	// otherwise: sort by name
 	return strcmp(atxt, btxt)<0;
