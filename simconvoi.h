6645e9536c6d0c9574cf000e91f6bb91b3830849
Nathanael Nerode <neroden@gcc.gnu.org>
2010-04-25 20:58:09 -0400

    Partial merge from standard: simple comment improvement in simconvoi.h

--- simconvoi.h
+++ simconvoi.h
@@ -311,10 +311,10 @@ private:
 	koord record_pos;
 
 	// needed for speed control/calculation
-	sint32 akt_speed_soll;	// should go this
-	sint32 akt_speed;			// goes this at the moment
-	sint32 sp_soll;					// steps to go
-	sint32 previous_delta_v;		// Stores the previous delta_v value; otherwise these digits are lost during calculation and vehicle do not accelrate
+	sint32 akt_speed_soll;    // target speed
+	sint32 akt_speed;	        // current speed
+	sint32 sp_soll;           // steps to go
+	sint32 previous_delta_v;  // Stores the previous delta_v value; otherwise these digits are lost during calculation and vehicle do not accelrate
 
 	uint32 next_wolke;	// time to next smoke
 
