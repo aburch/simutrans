f5a863d6a6db06739e52e91048df0f3c04d28e02
James E. Petts <jamespetts@yahoo.com>
2010-05-01 23:16:52 +0100

    FIX: Cornering and braking on approaching did not reduce speed as it ought to do.

--- convoy.cc
+++ convoy.cc
@@ -89,7 +89,7 @@ void adverse_summary_t::add_vehicle(const vehikel_t &v)
 	// Or airplane circling for landing, or airplane height,
 	// Or cornering, or other odd cases
 	// These are carried in vehikel_t unlike other speed limits 
-	sint32 limit = v.get_speed_limit();
+	sint32 limit = speed_to_kmh(v.get_speed_limit());
 	if (max_speed > limit)
 	{
 		max_speed = limit;
