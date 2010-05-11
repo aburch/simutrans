799e381df777154fdbe07e5ae9f92f2186502092
James E. Petts <jamespetts@yahoo.com>
2010-05-11 22:31:58 +0100

    CHANGE: When a player builds a way on land that he/she already owns, the cost of buying land is deducted from the cost of building the way. CHANGE: City car speeds now shown in km/h rather than an indecipherable internal number.

--- bauer/wegbauer.cc
+++ bauer/wegbauer.cc
@@ -1841,7 +1841,15 @@ sint64 wegbauer_t::calc_costs()
 				}
 			}
 		}
+		const ding_t* thing = gr->obj_bei(0);
+		if(thing != NULL  &&  thing->get_besitzer() == sp) 
+		{
+			// We own this land. Ergo, building a way on it should be cheaper.
+			costs += welt->get_einstellungen()->cst_buy_land;
+		}
+			
 	}
+			
 	DBG_MESSAGE("wegbauer_t::calc_costs()","construction estimate: %f",costs/100.0);
 	return costs;
 }
@@ -1991,6 +1999,13 @@ void wegbauer_t::baue_strasse()
 		}
 		else {
 			// make new way
+			const ding_t* thing = gr->obj_bei(0);
+			if(thing != NULL  &&  thing->get_besitzer() == sp) 
+			{
+				// We own this land. Ergo, building a way on it should be cheaper.
+				cost -= welt->get_einstellungen()->cst_buy_land;
+				cost = cost < 0 ? 0 : cost;
+			}
 			strasse_t * str = new strasse_t(welt);
 
 			str->set_besch(besch);
@@ -2067,6 +2082,13 @@ void wegbauer_t::baue_schiene()
 				}
 			}
 			else {
+				const ding_t* thing = gr->obj_bei(0);
+				if(thing != NULL  &&  thing->get_besitzer() == sp) 
+				{
+					// We own this land. Ergo, building a way on it should be cheaper.
+					cost -= welt->get_einstellungen()->cst_buy_land;
+					cost = cost < 0 ? 0 : cost;
+				}
 				weg_t *sch=weg_t::alloc((waytype_t)besch->get_wtyp());
 				sch->set_besch(besch);
 				const wayobj_t* wayobj = gr->get_wayobj(sch->get_waytype());
