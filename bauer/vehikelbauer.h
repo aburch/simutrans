b521eefc4ad8cde4f448547d116f7e5852272070
Dwachs <dwachs@gmx.net>
2010-05-14 18:19:45 +0000

    FIX: replace missing vehicles with vehicles that do not need electrifiction
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@3283 8aca7d54-2c30-db11-9de9-000461428c89

--- bauer/vehikelbauer.h
+++ bauer/vehikelbauer.h
@@ -51,7 +51,7 @@ public:
 	/* for replacement during load time
 	 * prev_veh==NULL equals leading of convoi
 	 */
-	static const vehikel_besch_t *get_best_matching( waytype_t wt, const uint16 month_now, const uint32 target_weight, const uint32 target_power, const uint32 target_speed, const ware_besch_t * target_freight, bool include_electric, bool not_obsolete, const vehikel_besch_t *prev_veh, bool is_last );
+	static const vehikel_besch_t *get_best_matching( waytype_t wt, const uint16 month_now, const uint32 target_weight, const uint32 target_power, const uint32 target_speed, const ware_besch_t * target_freight, bool not_obsolete, const vehikel_besch_t *prev_veh, bool is_last );
 };
 
 #endif
