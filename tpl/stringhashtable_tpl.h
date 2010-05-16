cec50871aef77f38b4c93a4bbea2c485c83c0bcf
Christoph Mallon <christoph.mallon@gmx.de>
2010-05-16 18:05:21 +0000

    Remove the unused class stringhashtable2_tpl; only keep its hash function in commented out form in stringhash_t.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@3297 8aca7d54-2c30-db11-9de9-000461428c89

--- tpl/stringhashtable_tpl.h
+++ tpl/stringhashtable_tpl.h
@@ -16,42 +16,16 @@ public:
 	static uint32 hash(const char *key)
 	{
 		uint32 hash = 0;
+#if 1
 		for(  sint8 i=16;  i*key[0]!=0;  i--  ) {
 			hash += (uint8)(*key++);
 		}
-		return hash;
-	}
-
-	static const char *null()
-	{
-		return NULL;
-	}
-
-	static void dump(const char *key)
-	{
-		printf("%s", key);
-	}
-
-	static int comp(const char *key1, const char *key2)
-	{
-		return strcmp(key1, key2);
-	}
-};
-
-
-/*
- * Define the key characteristica for hashing "const char *".
- * Alternate implementation with whole string for hashcode.
- */
-class stringhash2_t {
-public:
-	static uint32 hash(const char *key)
-	{
-		uint32 code = 0;
-		while (*key) {
-			code = (code<<5) + code + *key++;
+#else
+		while (*key != '\0') {
+			hash = hash * 33 + *key++;
 		}
-		return code;
+#endif
+		return hash;
 	}
 
 	static const char *null()
@@ -89,27 +63,6 @@ public:
 
 	stringhashtable_iterator_tpl(const hashtable_tpl<const char *, value_t, stringhash_t> &hashtable) :
 		hashtable_iterator_tpl<const char *, value_t, stringhash_t>(hashtable) {}
-};
-
-
-/*
- * Ready to use class for hashing strings. Hashkey is calculated from whole
- * string.
- */
-template <class value_t>
-class stringhashtable2_tpl : public hashtable_tpl<const char *, value_t, stringhash2_t>
-{
-};
-
-
-template<class value_t>
-class stringhashtable2_iterator_tpl : public hashtable_iterator_tpl<const char *, value_t, stringhash2_t>
-{
-public:
-	stringhashtable2_iterator_tpl(const hashtable_tpl<const char *, value_t, stringhash2_t> *hashtable) :
-		hashtable_iterator_tpl<const char *, value_t, stringhash2_t>(hashtable) {}
-	stringhashtable2_iterator_tpl(const hashtable_tpl<const char *, value_t, stringhash2_t> &hashtable) :
-		hashtable_iterator_tpl<const char *, value_t, stringhash2_t>(hashtable) {}
 };
 
 #endif
