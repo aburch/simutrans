87dd4dbad04a6fc8b070886e0c5f37a9e3e60d5f
Christoph Mallon <christoph.mallon@gmx.de>
2010-05-16 10:25:28 +0000

    Improve readability, reduce code duplication, static, pass references.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@3293 8aca7d54-2c30-db11-9de9-000461428c89

--- bauer/fabrikbauer.cc
+++ bauer/fabrikbauer.cc
@@ -42,17 +42,20 @@ static sint32 fab_map_w=0;
 
 
 // marks factories with exclusion region in the position map
-void add_factory_to_fab_map( karte_t *welt, const fabrik_t *fab )
+static void add_factory_to_fab_map(karte_t const& welt, fabrik_t const& fab)
 {
-	sint16 start_y = max( 0, fab->get_pos().y-welt->get_einstellungen()->get_factory_spacing() );
-	const sint16 end_y = min( welt->get_groesse_y()-1, fab->get_pos().y+fab->get_besch()->get_haus()->get_h(fab->get_rotate())+welt->get_einstellungen()->get_factory_spacing() );
-	const sint16 start_x = max( 0, fab->get_pos().x-welt->get_einstellungen()->get_factory_spacing() );
-	const sint16 end_x = min( welt->get_groesse_x()-1, fab->get_pos().x+fab->get_besch()->get_haus()->get_b(fab->get_rotate())+welt->get_einstellungen()->get_factory_spacing() );
-	while(start_y<end_y) {
-		for(  sint16 x=start_x;  x<end_x;  x++  ) {
-			fab_map[fab_map_w*start_y+(x/8)] |= 1<<(x%8);
+	koord3d      const& pos     = fab.get_pos();
+	sint16       const  spacing = welt.get_einstellungen()->get_factory_spacing();
+	haus_besch_t const& hbesch  = *fab.get_besch()->get_haus();
+	sint16       const  rotate  = fab.get_rotate();
+	sint16       const  start_y = max(0, pos.y - spacing);
+	sint16       const  start_x = max(0, pos.x - spacing);
+	sint16       const  end_y   = min(welt.get_groesse_y() - 1, pos.y + hbesch.get_h(rotate) + spacing);
+	sint16       const  end_x   = min(welt.get_groesse_x() - 1, pos.x + hbesch.get_b(rotate) + spacing);
+	for (sint16 y = start_y; y < end_y; ++y) {
+		for (sint16 x = start_x; x < end_x; ++x) {
+			fab_map[fab_map_w * start_y + x / 8] |= 1 << (x % 8);
 		}
-		start_y ++;
 	}
 }
 
@@ -68,7 +71,7 @@ void init_fab_map( karte_t *welt )
 	}
 	slist_iterator_tpl <fabrik_t *> iter(welt->get_fab_list());
 	while(iter.next()) {
-		add_factory_to_fab_map( welt, iter.get_current() );
+		add_factory_to_fab_map(*welt, *iter.get_current());
 	}
 }
 
@@ -404,7 +407,7 @@ fabrik_t* fabrikbauer_t::baue_fabrik(karte_t* welt, koord3d* parent, const fabri
 	// now build factory
 	fab->baue(rotate);
 	welt->add_fab(fab);
-	add_factory_to_fab_map( welt, fab );
+	add_factory_to_fab_map(*welt, *fab);
 
 	// make all water station
 	if(info->get_platzierung() == fabrik_besch_t::Wasser) {
