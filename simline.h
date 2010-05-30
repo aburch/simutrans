525f2141c7bbfab1d21c0fed4dcd4750c8f47fa3
Christoph Mallon <christoph.mallon@gmx.de>
2010-05-30 17:40:46 +0000

    Remove unnecessary scope specifiers.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@3390 8aca7d54-2c30-db11-9de9-000461428c89

--- simline.h
+++ simline.h
@@ -217,7 +217,7 @@ class truckline_t : public simline_t
 	public:
 		truckline_t(karte_t* welt, spieler_t* sp) : simline_t(welt, sp)
 		{
-			type = simline_t::truckline;
+			type = truckline;
 			set_schedule(new autofahrplan_t());
 		}
 };
@@ -227,7 +227,7 @@ class trainline_t : public simline_t
 	public:
 		trainline_t(karte_t* welt, spieler_t* sp) : simline_t(welt, sp)
 		{
-			type = simline_t::trainline;
+			type = trainline;
 			set_schedule(new zugfahrplan_t());
 		}
 };
@@ -237,7 +237,7 @@ class shipline_t : public simline_t
 	public:
 		shipline_t(karte_t* welt, spieler_t* sp) : simline_t(welt, sp)
 		{
-			type = simline_t::shipline;
+			type = shipline;
 			set_schedule(new schifffahrplan_t());
 		}
 };
@@ -247,7 +247,7 @@ class airline_t : public simline_t
 	public:
 		airline_t(karte_t* welt, spieler_t* sp) : simline_t(welt, sp)
 		{
-			type = simline_t::airline;
+			type = airline;
 			set_schedule(new airfahrplan_t());
 		}
 };
@@ -257,7 +257,7 @@ class monorailline_t : public simline_t
 	public:
 		monorailline_t(karte_t* welt, spieler_t* sp) : simline_t(welt, sp)
 		{
-			type = simline_t::monorailline;
+			type = monorailline;
 			set_schedule(new monorailfahrplan_t());
 		}
 };
@@ -267,7 +267,7 @@ class tramline_t : public simline_t
 	public:
 		tramline_t(karte_t* welt, spieler_t* sp) : simline_t(welt, sp)
 		{
-			type = simline_t::tramline;
+			type = tramline;
 			set_schedule(new tramfahrplan_t());
 		}
 };
@@ -277,7 +277,7 @@ class narrowgaugeline_t : public simline_t
 	public:
 		narrowgaugeline_t(karte_t* welt, spieler_t* sp) : simline_t(welt, sp)
 		{
-			type = simline_t::narrowgaugeline;
+			type = narrowgaugeline;
 			set_schedule(new narrowgaugefahrplan_t());
 		}
 };
@@ -287,7 +287,7 @@ class maglevline_t : public simline_t
 	public:
 		maglevline_t(karte_t* welt, spieler_t* sp) : simline_t(welt, sp)
 		{
-			type = simline_t::maglevline;
+			type = maglevline;
 			set_schedule(new maglevfahrplan_t());
 		}
 };
