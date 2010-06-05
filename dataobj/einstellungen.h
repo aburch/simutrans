9642dfb9cac8e1ff7208584d712f34d431aebe50
Christoph Mallon <christoph.mallon@gmx.de>
2010-06-05 15:41:15 +0000

    Remove unnecessary typedefs.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@3421 8aca7d54-2c30-db11-9de9-000461428c89

--- dataobj/einstellungen.h
+++ dataobj/einstellungen.h
@@ -18,12 +18,12 @@ class tabfile_t;
 class weg_besch_t;
 
 
-typedef struct
+struct road_timeline_t
 {
 	char name[64];
 	uint16 intro;
 	uint16 retire;
-} road_timeline_t;
+};
 
 
 class einstellungen_t
@@ -130,11 +130,12 @@ private:
 
 	sint64 starting_money;
 
-	typedef struct {
+	struct yearmoney
+	{
 		sint16 year;
 		sint64 money;
 		bool interpol;
-	} yearmoney;
+	};
 
 	yearmoney startingmoneyperyear[10];
 
