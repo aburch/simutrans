b7e63583f9f1c028a1ffe79a4de0d07e73e70316
Christoph Mallon <christoph.mallon@gmx.de>
2010-06-07 12:44:45 +0000

    Remove pointless qualifier on return type.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@3446 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/components/ding_view_t.h
+++ gui/components/ding_view_t.h
@@ -10,7 +10,7 @@ class ding_view_t : public world_view_t
 	public:
 		ding_view_t(ding_t const* d, koord const size) : world_view_t(d->get_welt(), size), ding(d) {}
 
-		ding_t const* const get_ding() const { return ding; }
+		ding_t const* get_ding() const { return ding; }
 
 		void zeichnen(koord offset) { internal_draw(offset, ding); }
 
