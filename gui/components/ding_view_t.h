ad91179d6104e69f242a0ca4038cd3a18cb04b6d
Knightly <knightly@invalid>
2010-06-07 08:21:25 +0000

    FIX : Remove superfluous class scope qualifications
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@3445 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/components/ding_view_t.h
+++ gui/components/ding_view_t.h
@@ -8,14 +8,14 @@
 class ding_view_t : public world_view_t
 {
 	public:
-		ding_view_t::ding_view_t(ding_t const* d, koord const size) : world_view_t(d->get_welt(), size), ding(d) {}
+		ding_view_t(ding_t const* d, koord const size) : world_view_t(d->get_welt(), size), ding(d) {}
 
 		ding_t const* const get_ding() const { return ding; }
 
 		void zeichnen(koord offset) { internal_draw(offset, ding); }
 
 	protected:
-		koord3d ding_view_t::get_location() { return ding->get_pos(); }
+		koord3d get_location() { return ding->get_pos(); }
 
 	private:
 		ding_t const* ding; /**< The object to display */
