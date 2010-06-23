1f17b3d09dcf52688f8fae022e5b9fb858e0f950
Tommy <mesilliac@gmail.com>
2010-06-23 18:08:25 +1200

    Remove redundant comparison (unsigned >= 0).

--- besch/reader/factory_reader.cc
+++ besch/reader/factory_reader.cc
@@ -290,11 +290,9 @@ factory_reader_t::read_node(FILE *fp, obj_node_info_t &node)
 		besch->pax_level = decode_uint16(p);
 		if(experimental)
 		{
-			if(experimental_version >= 0)
-			{
-				besch->electricity_proportion = ((float)decode_uint16(p) / 100.0F); 
-				besch->inverse_electricity_proportion = 1 / besch->electricity_proportion;
-			}
+			besch->electricity_proportion = ((float)decode_uint16(p) / 100.0F); 
+			besch->inverse_electricity_proportion = 1 / besch->electricity_proportion;
+
 			if(experimental_version >= 1)
 			{
 				besch->upgrades = decode_uint8(p);
