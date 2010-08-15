747bd0c402222b139779e6a7056e384731b60400
Markus Pristovsek <prissi@physik.tu-berlin.de>
2010-08-15 20:15:37 +0000

    Add copy constructor to array2d_tpl
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@3664 8aca7d54-2c30-db11-9de9-000461428c89

--- tpl/array2d_tpl.h
+++ tpl/array2d_tpl.h
@@ -33,6 +33,13 @@ public:
 		data = new T[w*h];
 	}
 
+	array2d_tpl(const array2d_tpl <T> &other) {
+		w = other.w;
+		h = other.h;
+		data = new T[w*h];
+		memcpy(data, other.data, sizeof(T)*w*h);
+	}
+
 	~array2d_tpl() {
 		delete [] data;
 	}
@@ -78,16 +85,19 @@ public:
 		return data;
 	}
 
-
-	void copy_from(const array2d_tpl <T> &other) {
-		if(h == other.h  &&  w == other.w) {
-			memcpy(data, other.data, sizeof(T)*w*h);
+	array2d_tpl<T> & operator = (const array2d_tpl <T> &other) {
+		if(  h != other.h  &&  w != other.w  ) {
+			if(  h*w!=0  ) {
+				dbg->error("array2d_tpl<T>::=()","source has different size!");
+			}
 		}
-		else {
-			dbg->fatal("array2d_tpl<T>::copy_from()","source has different size!");
-		}
+		delete [] data;
+		w = other.w;
+		h = other.h;
+		data = new T[w*h];
+		memcpy(data, other.data, sizeof(T)*w*h);
 	}
-
 };
+
 
 #endif
