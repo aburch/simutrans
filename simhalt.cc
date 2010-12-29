4496af622dc70460d9ee59a80c23530295b69d63
Markus Pristovsek <prissi@physik.tu-berlin.de>
2010-12-29 23:25:08 +0000

    ADD: Annonce public stops
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4113 8aca7d54-2c30-db11-9de9-000461428c89

--- simhalt.cc
+++ simhalt.cc
@@ -1963,6 +1963,13 @@ void haltestelle_t::make_public_and_join( spieler_t *sp )
 		}
 	}
 
+	// tell the world of it ...
+	if(  sp->get_player_nr()!=1  &&  umgebung_t::networkmode  ) {
+		cbuffer_t buf(256);
+		buf.printf( translator::translate("%s at (%i,%i) now public stop."), get_name(), get_basis_pos().x, get_basis_pos().y );
+		welt->get_message()->add_message( buf, get_basis_pos(), message_t::ai, PLAYER_FLAG|sp->get_player_nr(), IMG_LEER );
+	}
+
 	recalc_station_type();
 }
 
