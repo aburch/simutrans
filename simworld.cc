87ecf1619514a9905983611191406ac8f61b132e
Knightly <knightly@invalid>
2011-01-06 10:42:18 +0000

    FIX: Avoid too many sync check commands
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4145 8aca7d54-2c30-db11-9de9-000461428c89

--- simworld.cc
+++ simworld.cc
@@ -5704,7 +5704,7 @@ bool karte_t::interactive(uint32 quit_month)
 					// some serverside tasks
 					if(  umgebung_t::networkmode  &&  umgebung_t::server  ) {
 						// broadcast sync info
-						if (  (network_frame_count==0  &&  (sint64)dr_time()-(sint64)next_step_time>fix_ratio_frame_time)
+						if (  (network_frame_count==0  &&  (sint64)dr_time()-(sint64)next_step_time>fix_ratio_frame_time*2)
 								||  (sync_steps % umgebung_t::server_sync_steps_between_checks)==0  ) {
 							nwc_check_t* nwc = new nwc_check_t(sync_steps + umgebung_t::server_frames_ahead, map_counter, last_randoms[sync_steps&63], sync_steps);
 							network_send_all(nwc, true);
