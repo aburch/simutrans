3fd8523d177ece1803eb34c77154d7f61175d03d
Dwachs <dwachs@gmx.net>
2011-02-12 10:11:51 +0000

    FIX: decouple powerbridge ends if removign the powerbridge
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4288 8aca7d54-2c30-db11-9de9-000461428c89

--- bauer/brueckenbauer.cc
+++ bauer/brueckenbauer.cc
@@ -683,6 +683,17 @@ const char *brueckenbauer_t::remove(karte_t *welt, spieler_t *sp, koord3d pos, w
 				br->entferne(sp);
 				delete br;
 			}
+			leitung_t *lt = gr->get_leitung();
+			if (lt) {
+				spieler_t *old_owner = lt->get_besitzer();
+				// first delete powerline to decouple from the bridge powernet
+				lt->entferne(old_owner);
+				delete lt;
+				// .. now create powerline to create new powernet
+				lt = new leitung_t(welt, gr->get_pos(), old_owner);
+				lt->laden_abschliessen();
+				gr->obj_add(lt);
+			}
 		}
 		else {
 			ribi_t::ribi ribi = gr->get_weg_ribi_unmasked(wegtyp);
