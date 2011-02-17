fcb73e1cb7eaab283a0a3ceb1a43a184defefe6c
Dwachs <dwachs@gmx.net>
2011-02-17 19:55:32 +0000

    FIX: no error message when creating line in linemanagement window
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4308 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/schedule_list.cc
+++ gui/schedule_list.cc
@@ -320,7 +320,8 @@ bool schedule_list_gui_t::action_triggered( gui_action_creator_t *komp, value_t 
 		// update line schedule via tool!
 		werkzeug_t *w = create_tool( WKZ_LINE_TOOL | SIMPLE_TOOL );
 		cbuffer_t buf(128);
-		buf.printf( "c,0,%i,%ld,0|,", (int)tabs_to_lineindex[tabs.get_active_tab_index()], 0 );
+		int type = tabs_to_lineindex[tabs.get_active_tab_index()];
+		buf.printf( "c,0,%i,0,0|%i|", type, type );
 		w->set_default_param(buf);
 		sp->get_welt()->set_werkzeug( w, sp );
 		// since init always returns false, it is save to delete immediately
