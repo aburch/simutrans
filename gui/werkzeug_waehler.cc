1bc4086111e2548e71df8c3367747cc321266ac8
Dwachs <dwachs@gmx.net>
2011-04-14 19:53:45 +0000

    FIX: empty toolbars can be closed by click and get a min-width
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4391 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/werkzeug_waehler.cc
+++ gui/werkzeug_waehler.cc
@@ -17,6 +17,7 @@
 #include "../dataobj/umgebung.h"
 #include "werkzeug_waehler.h"
 
+#define MIN_WIDTH (80)
 
 
 werkzeug_waehler_t::werkzeug_waehler_t(karte_t* welt, const char* titel, const char *helpfile, uint32 toolbar_id, koord icon, bool allow_break) :
@@ -29,7 +30,8 @@ werkzeug_waehler_t::werkzeug_waehler_t(karte_t* welt, const char* titel, const c
 	this->icon = icon;
 	this->tool_icon_disp_start = 0;
 	this->tool_icon_disp_end = 0;
-	set_fenstergroesse( icon );
+	has_prev_next= false;
+	set_fenstergroesse( koord(max(icon.x,MIN_WIDTH), TITLEBAR_HEIGHT) );
     dirty = true;
 }
 
@@ -67,7 +69,7 @@ DBG_DEBUG4("werkzeug_waehler_t::add_tool()","ww=%i, rows=%i",ww,rows);
 		tool_icon_height = min(tool_icon_height, umgebung_t::toolbar_max_height);
 	}
 	dirty = true;
-	gui_frame_t::set_fenstergroesse( koord( tool_icon_width*icon.x, min(tool_icon_height, ((tools.get_count()-1)/tool_icon_width)+1)*icon.y+16 ) );
+	gui_frame_t::set_fenstergroesse( koord( tool_icon_width*icon.x, min(tool_icon_height, ((tools.get_count()-1)/tool_icon_width)+1)*icon.y+TITLEBAR_HEIGHT ) );
 	tool_icon_disp_start = 0;
 	tool_icon_disp_end = min( tool_icon_disp_start+tool_icon_width*tool_icon_height, tools.get_count() );
 	has_prev_next = ((uint32)tool_icon_width*tool_icon_height < tools.get_count());
@@ -84,7 +86,7 @@ void werkzeug_waehler_t::reset_tools()
 		i--;
 		tools.remove_at(i);
 	}
-	gui_frame_t::set_fenstergroesse( koord( icon.x, TITLEBAR_HEIGHT ) );
+	gui_frame_t::set_fenstergroesse( koord(max(icon.x,MIN_WIDTH), TITLEBAR_HEIGHT) );
 	tool_icon_width = 0;
 	tool_icon_disp_start = 0;
 	tool_icon_disp_end = 0;
@@ -95,7 +97,8 @@ bool werkzeug_waehler_t::getroffen(int x, int y)
 {
 	int dx = x/icon.x;
 	int	dy = (y-16)/icon.y;
-	if(  x>=0 && dx<tool_icon_width  &&  y>=0  &&  (y<TITLEBAR_HEIGHT  ||  dy<tool_icon_height)  ) {
+	// either click in titlebar or on an icon
+	if(  x>=0   &&  y>=0  &&  (y<TITLEBAR_HEIGHT  ||  (dx<tool_icon_width  &&  dy<tool_icon_height) )  ) {
 		return y < TITLEBAR_HEIGHT || dx + tool_icon_width * dy + tool_icon_disp_start < (int)tools.get_count();
 	}
 	return false;
