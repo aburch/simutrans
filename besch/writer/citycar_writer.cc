3038e20a3f9959ca5acf37e2e83a99cc7d046c59
Christoph Mallon <christoph.mallon@gmx.de>
2011-05-19 13:34:39 +0000

    Remove unnecessary friend declaration for citycar_writer_t from stadtauto_besch_t.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4473 8aca7d54-2c30-db11-9de9-000461428c89

--- besch/writer/citycar_writer.cc
+++ besch/writer/citycar_writer.cc
@@ -1,6 +1,5 @@
 #include <string>
 #include "../../dataobj/tabfile.h"
-#include "../stadtauto_besch.h"
 #include "obj_node.h"
 #include "text_writer.h"
 #include "imagelist_writer.h"
@@ -9,27 +8,28 @@
 
 void citycar_writer_t::write_obj(FILE* fp, obj_node_t& parent, tabfileobj_t& obj)
 {
-	stadtauto_besch_t besch;
 	int i;
 
 	obj_node_t node(this, 10, &parent);
 
-	besch.gewichtung = obj.get_int("distributionweight", 1);
+	uint16 const gewichtung = obj.get_int("distributionweight", 1);
 
-	besch.intro_date  = obj.get_int("intro_year", DEFAULT_INTRO_DATE) * 12;
-	besch.intro_date += obj.get_int("intro_month", 1) - 1;
+	uint16 const intro_date =
+		obj.get_int("intro_year", DEFAULT_INTRO_DATE) * 12 +
+		obj.get_int("intro_month", 1) - 1;
 
-	besch.obsolete_date  = obj.get_int("retire_year", DEFAULT_RETIRE_DATE) * 12;
-	besch.obsolete_date += obj.get_int("retire_month", 1) - 1;
+	uint16 const obsolete_date =
+		obj.get_int("retire_year", DEFAULT_RETIRE_DATE) * 12 +
+		obj.get_int("retire_month", 1) - 1;
 
-	besch.geschw  = obj.get_int("speed", 80) * 16;
+	uint16 const geschw = obj.get_int("speed", 80) * 16;
 
 	// new version with intro and obsolete dates
-	node.write_uint16(fp, 0x8002,                    0); // version information
-	node.write_uint16(fp, (uint16) besch.gewichtung, 2);
-	node.write_uint16(fp, (uint16) besch.geschw,     4);
-	node.write_uint16(fp, besch.intro_date,          6);
-	node.write_uint16(fp, besch.obsolete_date,       8);
+	node.write_uint16(fp, 0x8002,        0); // version information
+	node.write_uint16(fp, gewichtung,    2);
+	node.write_uint16(fp, geschw,        4);
+	node.write_uint16(fp, intro_date,    6);
+	node.write_uint16(fp, obsolete_date, 8);
 
 	write_head(fp, node, obj);
 
