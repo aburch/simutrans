9f50c1ff7486cd9970df70438d3b4de6c507feaf
Christoph Mallon <christoph.mallon@gmx.de>
2011-05-19 13:34:07 +0000

    Remove unnecessary friend declaration for groundobj_writer_t from groundobj_besch_t.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4465 8aca7d54-2c30-db11-9de9-000461428c89

--- besch/writer/groundobj_writer.cc
+++ besch/writer/groundobj_writer.cc
@@ -1,6 +1,5 @@
 #include <string>
 #include "../../dataobj/tabfile.h"
-#include "../groundobj_besch.h"
 #include "obj_node.h"
 #include "text_writer.h"
 #include "imagelist2d_writer.h"
@@ -17,43 +16,43 @@ void groundobj_writer_t::write_obj(FILE* fp, obj_node_t& parent, tabfileobj_t& o
 
 	// Hajodoc: Preferred height of this tree type
 	// Hajoval: int (useful range: 0-14)
-	groundobj_besch_t besch;
+	climate_bits allowed_climates;
 	const char *climate_str = obj.get("climates");
 	if (climate_str) {
-		besch.allowed_climates = get_climate_bits(climate_str);
+		allowed_climates = get_climate_bits(climate_str);
 	} else {
 		printf("WARNING: without climates!\n");
-		besch.allowed_climates = all_but_arctic_climate;
+		allowed_climates = all_but_arctic_climate;
 	}
 	// seasons = 1: no seasons
 	// otherwise the year will be devided by the (number_of_seasons-1)
 	// The last image is alsway the snow image!
-	besch.number_of_seasons = obj.get_int("seasons", 1);
+	uint8 const number_of_seasons = obj.get_int("seasons", 1);
 
 	// distribution probabiltion for all of this set
-	besch.distribution_weight = obj.get_int("distributionweight", 3);
+	uint16 const distribution_weight = obj.get_int("distributionweight", 3);
 
 	// how much for removal
-	besch.cost_removal = obj.get_int("cost", 0);
+	sint32 const cost_removal = obj.get_int("cost", 0);
 
 	// !=0 for moving objects (sheeps, birds)
-	besch.speed = obj.get_int("speed", 0);
+	uint16 const speed = obj.get_int("speed", 0);
 
 	// 1 for to allow trees on this objects
-	besch.trees_on_top = obj.get_int("trees_on_top", 1)!=0;
+	bool const trees_on_top = obj.get_int("trees_on_top", 1) != 0;
 
 	// waytype for moving stuff; meaningful air for birds, water for fish, does not matter for everything else
-	const char* waytype = obj.get("waytype");
-	besch.waytype = (waytype==0  ||  waytype[0]==0) ? ignore_wt : (waytype_t)get_waytype(waytype);
+	char const* const waytype_txt = obj.get("waytype");
+	waytype_t   const waytype     = waytype_txt && waytype_txt[0] != '\0' ? get_waytype(waytype_txt) : ignore_wt;
 
 	// now for the images
 	slist_tpl<slist_tpl<string> > keys;
-	if(besch.speed==0) {
+	if (speed == 0) {
 		// fixed stuff
 		for (unsigned int phase = 0; 1; phase++) {
 			keys.append(slist_tpl<string>());
 
-			for (int seasons = 0; seasons < besch.number_of_seasons; seasons++) {
+			for (int seasons = 0; seasons < number_of_seasons; seasons++) {
 				char buf[40];
 
 				// Images of the tree
@@ -80,7 +79,7 @@ void groundobj_writer_t::write_obj(FILE* fp, obj_node_t& parent, tabfileobj_t& o
 		for (unsigned int dir = 0; dir<8; dir++) {
 			keys.append(slist_tpl<string>());
 
-			for (int seasons = 0; seasons < besch.number_of_seasons; seasons++) {
+			for (int seasons = 0; seasons < number_of_seasons; seasons++) {
 				char buf[40];
 
 				// Images of the tree
@@ -99,15 +98,14 @@ finish_images:
 	imagelist2d_writer_t::instance()->write_obj(fp, node, keys);
 
 	// Hajo: write version data
-	node.write_uint16(fp, 0x8001,                              0);
-
-	node.write_uint16(fp, (uint16) besch.allowed_climates,     2);
-	node.write_uint16(fp, (uint16) besch.distribution_weight,  4);
-	node.write_uint8 (fp, (uint8)  besch.number_of_seasons,    6);
-	node.write_uint8 (fp, (uint8)  besch.trees_on_top,         7);
-	node.write_uint16(fp, (uint16) besch.speed,                8);
-	node.write_uint16(fp, (uint16) besch.waytype,             10);
-	node.write_sint32(fp, (sint32) besch.cost_removal,        12);
+	node.write_uint16(fp, 0x8001,               0);
+	node.write_uint16(fp, allowed_climates,     2);
+	node.write_uint16(fp, distribution_weight,  4);
+	node.write_uint8 (fp, number_of_seasons,    6);
+	node.write_uint8 (fp, trees_on_top,         7);
+	node.write_uint16(fp, speed,                8);
+	node.write_uint16(fp, waytype,             10);
+	node.write_sint32(fp, cost_removal,        12);
 
 	node.write(fp);
 }
