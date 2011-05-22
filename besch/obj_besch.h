d1c3edd327952ab77d166a305afff0d1532aed45
Christoph Mallon <christoph.mallon@gmx.de>
2011-05-22 11:34:42 +0000

    Remove the unused method obj_besch_t::calc_checksum().
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4503 8aca7d54-2c30-db11-9de9-000461428c89

--- besch/obj_besch.h
+++ besch/obj_besch.h
@@ -10,7 +10,6 @@
 #include <cstddef>
 #include "../simtypes.h"
 
-class checksum_t;
 /*
  *  Autor:
  *      Volker Meyer
@@ -40,16 +39,6 @@ public:
 	void* operator new(size_t size, unsigned extra)
 	{
 		return ::operator new(size + extra);
-	}
-
-	/**
-	 * calculate checksum of the besch
-	 * called in register_obj
-	 * ATTENTION: xref's are NOT resolved yet
-	 * @author Dwachs
-	 */
-	void calc_checksum(checksum_t *) const
-	{
 	}
 
 	friend class obj_reader_t;
