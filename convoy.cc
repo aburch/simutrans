b93bcf7f0139bbc01e34d6ba3094cc0ebf65ca8d
James E. Petts <jamespetts@yahoo.com>
2011-05-27 13:32:58 +0100

    REVERT: Acceleration to standard parameters without 15% increase in tractive effort.

--- convoy.cc
+++ convoy.cc
@@ -268,11 +268,11 @@ void convoy_t::calc_move(long delta_t, uint16 simtime_factor_integer, const weig
 				// Below set speed: full acceleration
 				// If set speed is far below the convoy max speed as e.g. aircrafts on ground reduce force.
 				// If set speed is at most a 10th of convoy's maximum, we reduce force to its 10th.
-				d_f = d_get_force(d_v * 0.85) - d_Frs;
-				f = (get_force(v * fraction_t(85, 100)) - Frs).shorten();
+				//d_f = d_get_force(d_v * 0.85) - d_Frs;
+				//f = (get_force(v * fraction_t(85, 100)) - Frs).shorten();
 				//const double TEST_f_double_equivalent = (double)f.n / (double)f.d;
-//				d_f = d_get_force(d_v) - d_Frs;
-//				f = get_force(v.n / v.d) - Frs;
+				d_f = d_get_force(d_v) - d_Frs;
+				f = get_force(v.n / v.d) - Frs;
 				if (f > 1000000) // reducing force does not apply to 'weak' convoy's, thus we can save a lot of time skipping this code.
 				{
 					if (d_speed_ratio == 0) // speed_ratio is a constant within this function. So calculate it once only.
