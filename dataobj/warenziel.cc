e5b45da7e39dcd5f96f282a85c6fa5700abfca41
Dwachs <dwachs@gmx.net>
2011-06-24 18:30:06 +0000

    remove dead code in dataobj/warenziel.*
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4670 8aca7d54-2c30-db11-9de9-000461428c89

--- dataobj/warenziel.cc
+++ dataobj/warenziel.cc
@@ -5,39 +5,17 @@
  * (see licence.txt)
  */
 
-#include <stdio.h>
-
 #include "warenziel.h"
 #include "koord.h"
-
-#include "../simmem.h"
-#include "../besch/ware_besch.h"
-#include "../bauer/warenbauer.h"
-
-#include "../simtypes.h"
 #include "loadsave.h"
+#include "../macros.h"
 
-warenziel_t::warenziel_t(loadsave_t *file)
-{
-	rdwr(file);
-}
-
-
-void
-warenziel_t::rdwr(loadsave_t *file)
+void warenziel_t::rdwr(loadsave_t *file)
 {
 	// dummy ...
 	koord ziel;
 	ziel.rdwr(file);
 
-	if(file->is_saving()) {
-		const char *tn = warenbauer_t::get_info_catg_index(catg_index)->get_name();
-		file->rdwr_str(tn);
-	}
-	else {
-		char tn[256];
-		file->rdwr_str(tn, lengthof(tn));
-		halt = halthandle_t();
-		catg_index = warenbauer_t::get_info(tn)->get_catg_index();
-	}
+	char tn[256];
+	file->rdwr_str(tn, lengthof(tn));
 }
--- dataobj/warenziel.h
+++ dataobj/warenziel.h
@@ -8,46 +8,16 @@
 #ifndef dataobj_warenziel_h
 #define dataobj_warenziel_h
 
-#include "../halthandle_t.h"
-#include "../besch/ware_besch.h"
-
 class loadsave_t;
 
 /**
- * Diese Klasse wird zur Verwaltung von Zielen von
- * Haltestellen benutzt. Grundlegende Elemente sind
- * eine Koordinate und ein Zeitstempel.
- *
- * @author Hj. Malthaner
+ * class necessary to load pre0.99.13 savegames
  */
 
 class warenziel_t
 {
-private:
-	halthandle_t halt;
-	uint8 catg_index;
-
 public:
-	// don't use them, or fix them: Actually, these should check for stops
-	// but they are needed for list search ...
-	int operator==(const warenziel_t &wz) {
-		return (halt == wz.get_zielhalt()  &&  catg_index==wz.get_catg_index());
-	}
-	int operator!=(const warenziel_t &wz) {
-		return halt!=wz.get_zielhalt()  ||  catg_index!=wz.get_catg_index();
-	}
-
-	warenziel_t() { catg_index = 255; halt = halthandle_t();}
-
-	warenziel_t(halthandle_t &h, const ware_besch_t *b) { halt = h; catg_index = b->get_catg_index(); }
-
-	warenziel_t(loadsave_t *file);
-
-	void set_zielhalt(halthandle_t &h) { halt = h; }
-	const halthandle_t get_zielhalt() const { return halt; }
-
-	uint8 get_catg_index() const { return catg_index; }
-
+	warenziel_t() {}
 	void rdwr(loadsave_t *file);
 };
 
