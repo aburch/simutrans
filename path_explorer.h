4c74e420e391c7526669f0d7683d90b57ca2cb14
Knightly <knightly@invalid>
2011-07-26 05:25:21 +0800

    Reduce the size of transport matrix by half

--- path_explorer.h
+++ path_explorer.h
@@ -104,32 +104,15 @@ private:
 			uint16 aggregate_time;
 			halthandle_t next_transfer;
 
-			path_element_t() : aggregate_time(65535) { }
+			path_element_t() : aggregate_time(65535u) { }
 		};
 
 		// element used during path search only for storing best lines/convoys
 		struct transport_element_t
 		{
-			union
-			{
-				struct
-				{
-					uint16 first_line;
-					uint16 first_convoy;
-				};
-				uint32 first_transport;
-			};
-
-			union
-			{
-				struct
-				{
-					uint16 last_line;
-					uint16 last_convoy;
-				};
-				uint32 last_transport;
-			};
-
+			uint16 first_transport;
+			uint16 last_transport;
+			
 			transport_element_t() : first_transport(0), last_transport(0) { }
 		};
 
@@ -142,12 +125,12 @@ private:
 			// element used for storing indices of halts connected to a transfer, together with their common transport
 			struct connection_cluster_t
 			{
-				uint32 transport;
+				uint16 transport;
 				vector_tpl<uint16> connected_halts;
 
 				connection_cluster_t(const uint32 halt_vector_size) : connected_halts(halt_vector_size) { }
 
-				connection_cluster_t(const uint32 halt_vector_size, const uint32 transport_id, const uint16 halt_id) 
+				connection_cluster_t(const uint32 halt_vector_size, const uint16 transport_id, const uint16 halt_id) 
 					: transport(transport_id), connected_halts(halt_vector_size)
 				{
 					connected_halts.append(halt_id);
@@ -159,7 +142,7 @@ private:
 			vector_tpl<connection_cluster_t*> connection_clusters;
 			uint32 usage_level;			// number of connection clusters used
 			uint32 halt_vector_size;	// size of connected halt vector in connection cluster object
-			inthashtable_tpl<uint32, connection_cluster_t*> cluster_map;
+			inthashtable_tpl<uint16, connection_cluster_t*> cluster_map;
 
 		public:
 
@@ -194,7 +177,7 @@ private:
 				cluster_map.clear();
 			}
 
-			void register_connection(const uint32 transport_id, const uint16 halt_id)
+			void register_connection(const uint16 transport_id, const uint16 halt_id)
 			{
 				// check against each existing cluster
 				connection_cluster_t *const existing_cluster = cluster_map.get(transport_id);
@@ -242,7 +225,7 @@ private:
 				}
 				else
 				{
-					dbg->fatal("connection_t::operator[]()", "Index out of bounds: %i not in 0..%d", element_id, usage_level - 1);
+					dbg->fatal("connection_t::operator[]()", "Index out of bounds: %i not in 0..%i", element_id, (sint32)usage_level - 1);
 				}
 			}
 		};
@@ -264,6 +247,7 @@ private:
 
 		// set of variables for working path data
 		path_element_t **working_matrix;
+		uint16 *transport_index_map;
 		transport_element_t **transport_matrix;
 		uint16 *working_halt_index_map;
 		halthandle_t *working_halt_list;
