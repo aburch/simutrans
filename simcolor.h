a319faed741224fbb53b5664eced1de3ecb5c3fe
Markus Pristovsek <prissi@physik.tu-berlin.de>
2011-10-04 20:27:22 +0000

    slightly changed colors further
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4836 8aca7d54-2c30-db11-9de9-000461428c89

--- simcolor.h
+++ simcolor.h
@@ -85,10 +85,10 @@ typedef unsigned char COLOR_VAL;
 #define COL_OPS_PROFIT (87)
 #define COL_NEW_VEHICLES (79)
 #define COL_CONSTRUCTION (110)
-#define COL_PROFIT (122)
+#define COL_PROFIT (6)
 #define COL_TRANSPORTED (171)
 
-#define COL_CASH (6)
+#define COL_CASH (52)
 #define COL_VEHICLE_ASSETS (63)
 #define COL_MARGIN (175)
 #define COL_WEALTH (95)
