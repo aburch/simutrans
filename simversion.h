c33b8d5be17c348559c83d1cd42bdba0da8f15bc
Markus Pristovsek <prissi@physik.tu-berlin.de>
2011-10-29 22:53:39 +0000

    prepare for release
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4896 8aca7d54-2c30-db11-9de9-000461428c89

--- simversion.h
+++ simversion.h
@@ -3,16 +3,16 @@
 
 #define MAKEOBJ_VERSION "52"
 
-#define VERSION_NUMBER "Nightly 110.0.7"
-#define WIDE_VERSION_NUMBER L" Nightly 110.0.7"
+#define VERSION_NUMBER " 111.0"
+#define WIDE_VERSION_NUMBER L" 111.0"
 
 #define VERSION_DATE __DATE__
 
 #define SAVEGAME_PREFIX  "Simutrans "
 #define XML_SAVEGAME_PREFIX  "<?xml version=\"1.0\"?>"
 
-#define SAVEGAME_VER_NR  "0.110.7"
-#define SERVER_SAVEGAME_VER_NR  "0.110.7"
+#define SAVEGAME_VER_NR  "0.111.0"
+#define SERVER_SAVEGAME_VER_NR  "0.111.0"
 
 #define RES_VERSION_NUMBER  0, 110, 0, 7
 
