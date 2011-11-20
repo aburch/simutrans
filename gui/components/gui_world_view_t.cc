65961c51d115b1cc12dc048231d52402bfe95a1a
Dwachs <dwachs@gmx.net>
2011-11-20 18:04:18 +0000

    FIX non-critical typo
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@4953 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/components/gui_world_view_t.cc
+++ gui/components/gui_world_view_t.cc
@@ -56,7 +56,7 @@ void world_view_t::internal_draw(const koord offset, ding_t const* const ding)
 	koord         fine_here = koord(0, 0);
 	sint16        y_offset  = 0;
 	if(ding) { // offsets?
-		fine_here = koord(tile_raster_scale_x(-ding->get_xoff(), raster), tile_raster_scale_x(-ding->get_yoff() % (OBJECT_OFFSET_STEPS * 2), raster));
+		fine_here = koord(tile_raster_scale_x(-ding->get_xoff(), raster), tile_raster_scale_y(-ding->get_yoff() % (OBJECT_OFFSET_STEPS * 2), raster));
 		y_offset  = ding->get_yoff() / (OBJECT_OFFSET_STEPS * 2);
 		if(vehikel_basis_t const* const v = ding_cast<vehikel_basis_t>(ding)) {
 			int x = 0;
