4f01744bb81548638c889cc960f2890991501ada
James E. Petts <jamespetts@yahoo.com>
2011-12-21 17:10:00 +0000

    CODE: Remove testing code.

--- simconvoi.cc
+++ simconvoi.cc
@@ -4028,7 +4028,6 @@ sint64 convoi_t::calc_revenue(ware_t& ware)
 	const ware_besch_t* goods = ware.get_besch();
 	const sint64 starting_distance = ware.get_origin().is_bound() ? shortest_distance(ware.get_origin()->get_basis_pos(), fahr[0]->get_pos().get_2d()) - revenue_distance : 0;
 	const sint64 base_fare = goods->get_fare(revenue_distance, starting_distance);
-	const sint64 TEST_fare = goods->get_fare(revenue_distance);
 	const sint64 min_fare = base_fare / 10ll;
 	const uint16 speed_bonus_rating = calc_adjusted_speed_bonus(goods->get_speed_bonus(), distance);
 	const sint64 ref_speed = welt->get_average_speed( fahr[0]->get_besch()->get_waytype() );
