607b71dbeba2ab5dc8bc1387350e00b52028a802
Christoph Mallon <christoph.mallon@gmx.de>
2011-12-25 21:23:54 +0000

    Add a parameter for MSVC to the macro CXX11().
    This avoids code duplication and tests for the existence of _MSC_VER before checking its value.
    
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5055 8aca7d54-2c30-db11-9de9-000461428c89

--- simtypes.h
+++ simtypes.h
@@ -29,22 +29,23 @@
 #endif
 
 #define GCC_ATLEAST(major, minor) (defined __GNUC__ && (__GNUC__ > (major) || (__GNUC__ == (major) && __GNUC_MINOR__ >= (minor))))
-#define CXX11(major, minor)       (__cplusplus >= 201103L || (defined __GXX_EXPERIMENTAL_CXX0X__ && GCC_ATLEAST((major), (minor))))
 
-#if CXX11(4, 4)
+#define CXX11(gcc_major, gcc_minor, msc_ver) ( \
+	__cplusplus >= 201103L || \
+	(defined __GXX_EXPERIMENTAL_CXX0X__ && GCC_ATLEAST((gcc_major), (gcc_minor))) || \
+	(defined _MSC_VER && (msc_ver) != 0 && _MSC_VER >= (msc_ver)) \
+)
+
+#if CXX11(4, 4, 0)
 #	define DELETED = delete
 #else
 #	define DELETED
 #endif
 
-#if CXX11(4, 7)
-#	define OVERRIDE override
-#else
-#if _MSC_VER >= 1400
+#if CXX11(4, 7, 1400)
 #	define OVERRIDE override
 #else
 #	define OVERRIDE
-#endif
 #endif
 
 #ifdef __cplusplus
