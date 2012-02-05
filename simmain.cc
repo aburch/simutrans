3da45cb830de44896050ee4193c2daa244974871
Christoph Mallon <christoph.mallon@gmx.de>
2012-02-05 18:01:03 +0000

    Remove unnecessary special case regarding std::set_new_handler() for MSVC.
    According to the MSDN documentation std::set_new_handler() works just fine.
    
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5226 8aca7d54-2c30-db11-9de9-000461428c89

--- simmain.cc
+++ simmain.cc
@@ -7,10 +7,6 @@
 #include <string>
 #include <new>
 
-#ifdef _MSC_VER
-#include <new.h> // for _set_new_handler
-#endif
-
 #include "pathes.h"
 
 #include "simmain.h"
@@ -325,16 +321,9 @@ static void ask_language()
  * Dies wird in main mittels set_new_handler gesetzt und von der
  * Laufzeitumgebung im Falle des Speichermangels bei new() aufgerufen
  */
-#ifdef _MSC_VER
-int sim_new_handler(unsigned int)
-#else
 void sim_new_handler()
-#endif
 {
 	dbg->fatal("sim_new_handler()", "OUT OF MEMORY");
-#ifdef _MSC_VER
-	return 0;
-#endif
 }
 
 
@@ -365,11 +354,8 @@ int simu_main(int argc, char** argv)
 
 	uint32 quit_month = 0x7FFFFFFFu;
 
-#ifdef _MSC_VER
-	_set_new_handler(sim_new_handler);
-#else
 	std::set_new_handler(sim_new_handler);
-#endif
+
 	umgebung_t::init();
 
 	// you really want help with this?
