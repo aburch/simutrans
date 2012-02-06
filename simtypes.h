9484da55051aa1264179d657702462cde005e5be
Christoph Mallon <christoph.mallon@gmx.de>
2012-02-06 07:44:17 +0000

    Remove unnecessary special case for MSVC regarding sint64/uint64.
    According to the MSDN documentation MSVC knows (unsigned) long long.
    
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5231 8aca7d54-2c30-db11-9de9-000461428c89

--- simtypes.h
+++ simtypes.h
@@ -129,15 +129,13 @@ typedef unsigned int        uint32;
 typedef   signed long       sint32;
 typedef unsigned long       uint32;
 #endif
+typedef   signed long long  sint64;
+typedef unsigned long long  uint64;
 #ifdef _MSC_VER
-typedef   signed __int64	  sint64;
-typedef unsigned __int64    uint64;
 #	define GCC_PACKED
 #	define NORETURN __declspec(noreturn)
 #	pragma warning(disable: 4200 4311 4800 4996)
 #else
-typedef   signed long long  sint64;
-typedef unsigned long long  uint64;
 #	define GCC_PACKED __attribute__ ((__packed__))
 #	define NORETURN   __attribute__ ((noreturn))
 #endif
