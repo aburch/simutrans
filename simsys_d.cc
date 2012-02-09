1656697ed3e849d795fbcd00a6d973e719375fc6
Markus Pristovsek <prissi@physik.tu-berlin.de>
2012-02-09 20:53:01 +0000

    ADD: (swed) screenshots for allegro
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5245 8aca7d54-2c30-db11-9de9-000461428c89

--- simsys_d.cc
+++ simsys_d.cc
@@ -343,7 +343,9 @@ int dr_screenshot(const char *filename)
 		return 1;
 	}
 #endif
-	return 0;
+	PALETTE pal;
+	get_palette(pal);
+	return save_bitmap(filename, texture_map, pal) == 0 ? 1 : -1;
 }
 
 
