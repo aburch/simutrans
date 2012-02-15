251c1e4a5182403ce2ab65b3e2cdc7ef8ba36ef4
Richard Smith <richard@ex-parrot.com>
2012-02-15 14:11:08 +0000

    Fix memory owership in livery_scheme

--- dataobj/livery_scheme.cc
+++ dataobj/livery_scheme.cc
@@ -12,37 +12,25 @@ livery_scheme_t::livery_scheme_t(const char* n, const uint16 date)
 {
 	scheme_name = n;
 	retire_date = date;
-	liveries = new vector_tpl<livery_t>;
 }
 
-
-livery_scheme_t::~livery_scheme_t()
-{
-	/*ITERATE_ptr(liveries, i)
-	{
-		liveries->get_element(i).name.clear();
-	}
-	liveries.clear();*/
-	//delete liveries;
-	//liveries = NULL;
-}
 
 const char* livery_scheme_t::get_latest_available_livery(uint16 date, const vehikel_besch_t* besch) const
 {
-	if(liveries->empty())
+	if(liveries.empty())
 	{
 		// No liveries available at all
 		return NULL;
 	}
 	const char* livery = NULL;
 	uint16 latest_valid_intro_date = 0;
-	ITERATE_PTR(liveries, i)
+	ITERATE(liveries, i)
 	{
-		if(date >= liveries->get_element(i).intro_date && besch->check_livery(liveries->get_element(i).name.c_str()) && liveries->get_element(i).intro_date > latest_valid_intro_date)
+		if(date >= liveries.get_element(i).intro_date && besch->check_livery(liveries.get_element(i).name.c_str()) && liveries.get_element(i).intro_date > latest_valid_intro_date)
 		{
 			// This returns the most recent livery available for this vehicle that is not in the future.
-			latest_valid_intro_date = liveries->get_element(i).intro_date;
-			livery = liveries->get_element(i).name.c_str();
+			latest_valid_intro_date = liveries.get_element(i).intro_date;
+			livery = liveries.get_element(i).name.c_str();
 		}
 	}
 	return livery;
@@ -55,11 +43,11 @@ void livery_scheme_t::rdwr(loadsave_t *file)
 	uint16 count = 0;
 	if(file->is_saving())
 	{
-		count = liveries->get_count();
+		count = liveries.get_count();
 	}
 	else
 	{
-		liveries->clear();
+		liveries.clear();
 	}
 
 	file->rdwr_short(count);
@@ -71,8 +59,8 @@ void livery_scheme_t::rdwr(loadsave_t *file)
 	{
 		if(file->is_saving())
 		{
-			n = liveries->get_element(i).name;
-			date = liveries->get_element(i).intro_date;
+			n = liveries.get_element(i).name;
+			date = liveries.get_element(i).intro_date;
 		}
 		else
 		{
@@ -88,7 +76,7 @@ void livery_scheme_t::rdwr(loadsave_t *file)
 			livery_t liv;
 			liv.name = n;
 			liv.intro_date = date;
-			liveries->append(liv);
+			liveries.append(liv);
 		}	
 	}
 }
