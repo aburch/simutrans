a7790bc8372f387fbaf23b000f10f9a760faf4bc
Christoph Mallon <christoph.mallon@gmx.de>
2012-03-03 09:44:41 +0000

    Fix a bug: enables should be checked here.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5471 8aca7d54-2c30-db11-9de9-000461428c89

--- simhalt.cc
+++ simhalt.cc
@@ -2639,7 +2639,7 @@ void haltestelle_t::recalc_status()
 			long ware_sum = get_ware_summe(wtyp);
 			total_sum += ware_sum;
 			if(ware_sum>max_ware) {
-				status_bits |= (ware_sum>max_ware+32  ||  CROWDED) ? 2 : 1;
+				status_bits |= ware_sum > max_ware + 32 || enables & CROWDED ? 2 : 1;
 				overcrowded[wtyp->get_catg_index()/8] |= 1<<(wtyp->get_catg_index()%8);
 			}
 		}
