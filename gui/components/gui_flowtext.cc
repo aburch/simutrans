aa4bf90224dd6860f2a75b6952f26df323b265f6
Markus Pristovsek <prissi@physik.tu-berlin.de>
2012-03-14 14:27:52 +0000

    FIX: underlining of links
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5556 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/components/gui_flowtext.cc
+++ gui/components/gui_flowtext.cc
@@ -224,6 +224,9 @@ koord gui_flowtext_t::output(koord offset, bool doit, bool return_max_width)
 	int color        = COL_BLACK;
 	int double_color = COL_BLACK;
 	bool double_it   = false;
+	bool link_it     = false;	// true, if currently underlining for a link
+	int extra_pixel  = 0;		// extra pixel before next line
+	int last_link_x  = 0;		// at this position ye need to continue underline drawing
 	int max_width    = width;
 	int text_width   = width;
 	const int space_width = proportional_string_width(" ");
@@ -245,8 +248,17 @@ koord gui_flowtext_t::output(koord offset, bool doit, bool return_max_width)
 						max_width = nxpos;
 					}
 					nxpos -= xpos;
+					if(  xpos!=last_link_x  &&  link_it  ) {
+						if(  doit  ) {
+							// close the link
+							display_fillbox_wh_clip( offset.x + last_link_x, ypos + offset.y + LINESPACE-1, xpos-last_link_x, 1, color, false);
+						}
+						extra_pixel = 1;
+					}
 					xpos = 0;
-					ypos += LINESPACE;
+					last_link_x = 0;
+					ypos += LINESPACE+extra_pixel;
+					extra_pixel = 0;
 				}
 				if (nxpos >= text_width) {
 					text_width = nxpos;
@@ -255,17 +267,33 @@ koord gui_flowtext_t::output(koord offset, bool doit, bool return_max_width)
 				if (doit) {
 					if (double_it) {
 						display_proportional_clip(offset.x + xpos + 1, offset.y + ypos + 1, i.text.c_str(), 0, double_color, false);
+						extra_pixel |= 1;
 					}
-					display_proportional_clip(offset.x + xpos, offset.y + ypos, i.text.c_str(), 0, color, false);
+					KOORD_VAL width = display_proportional_clip(offset.x + xpos, offset.y + ypos, i.text.c_str(), 0, color, false);
+					if(  link_it  ) {
+						display_fillbox_wh_clip( offset.x + last_link_x, ypos + offset.y + LINESPACE-1, (xpos+width)-last_link_x, 1, color, false);
+						last_link_x = xpos+width;
+					}
 				}
-
+				if(  link_it  ) {
+					extra_pixel = 1;
+				}
 				xpos = nxpos;
 				break;
 			}
 
 			case ATT_NEWLINE:
 				xpos = 0;
-				ypos += LINESPACE;
+				if(  last_link_x<xpos  &&  link_it  ) {
+					if(  doit  ) {
+						// close the link
+						display_fillbox_wh_clip( offset.x + last_link_x, ypos + offset.y + LINESPACE-1, xpos-last_link_x, 1, color, false);
+					}
+					extra_pixel = 1;
+				}
+				ypos += LINESPACE+extra_pixel;
+				last_link_x = 0;
+				extra_pixel = 0;
 				break;
 
 			case ATT_A_START:
@@ -274,28 +302,16 @@ koord gui_flowtext_t::output(koord offset, bool doit, bool return_max_width)
 				if (link!=links.end()) {
 					link->tl.x = xpos;
 					link->tl.y = ypos;
+					last_link_x = xpos;
+					link_it = true;
 				}
 				break;
 
 			case ATT_A_END:
 				link->br.x = xpos;
 				link->br.y = ypos + LINESPACE;
-
-				if(doit) {
-					if(  link->tl.y+LINESPACE==link->br.y  ) {
-						display_fillbox_wh_clip(link->tl.x + offset.x, link->tl.y + offset.y + LINESPACE-1, link->br.x - link->tl.x, 1, color, false);
-					}
-					else {
-						// more than one line
-						display_fillbox_wh_clip (link->tl.x + offset.x, link->tl.y + offset.y + LINESPACE-1, get_groesse().x - link->tl.x, 1, color, false);
-						display_fillbox_wh_clip( offset.x, link->br.y + offset.y - 1, link->br.x, 1, color, false);
-						for(  KOORD_VAL y = link->tl.y+LINESPACE;  y<link->br.y-LINESPACE;  y+=LINESPACE  ) {
-							display_fillbox_wh_clip( offset.x, y + offset.y + LINESPACE-1, get_groesse().x, 1, color, false);
-						}
-					}
-				}
-
 				++link;
+				link_it = false;
 				color = COL_BLACK;
 				break;
 
@@ -313,7 +329,8 @@ koord gui_flowtext_t::output(koord offset, bool doit, bool return_max_width)
 					display_fillbox_wh_clip(offset.x,     offset.y + ypos + LINESPACE-1,     xpos, 1, color, false);
 				}
 				xpos = 0;
-				ypos += LINESPACE;
+				extra_pixel = 0;
+				ypos += LINESPACE+2;
 				break;
 
 			case ATT_EM_START:
