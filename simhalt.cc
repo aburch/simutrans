9155e80ccb18a86d634bfe4e51f6f27a87cabfc2
Markus Pristovsek <prissi@physik.tu-berlin.de>
2012-03-16 21:14:58 +0000

    move comment to its correct location
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5568 8aca7d54-2c30-db11-9de9-000461428c89

--- simhalt.cc
+++ simhalt.cc
@@ -1343,10 +1343,14 @@ int haltestelle_t::search_route( const halthandle_t *const start_halts, const ui
 
 		FOR(vector_tpl<connection_t>, const& current_conn, current_node.halt->connections[ware_catg_idx]) {
 
-			if(  !current_conn.halt.is_bound()  ) {
+			// halt may have been deleted or joined => test if still valid
+			if(  !current_conn.is_bound()  ) {
+				// removal seems better though ...
 				continue;
 			}
 
+			// since these are precalculated, they should be always pointing to a valid ground
+			// (if not, we were just under construction, and will be fine after 16 steps)
 			const uint16 reachable_halt_id = current_conn.halt.get_id();
 
 			const bool overcrowded_transfer = no_routing_over_overcrowding  &&  (current_halt_data.overcrowded  ||  current_conn.halt->is_overcrowded(ware_catg_idx) );
