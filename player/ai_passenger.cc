de7168401d2b87922e83002044d460f2c896151b
Markus Pristovsek <prissi@physik.tu-berlin.de>
2012-04-26 21:32:36 +0000

    FIX: (dlichtenberger) crash when using passenger AI and air transport
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5668 8aca7d54-2c30-db11-9de9-000461428c89

--- player/ai_passenger.cc
+++ player/ai_passenger.cc
@@ -1216,7 +1216,8 @@ DBG_MESSAGE("ai_passenger_t::do_passenger_ki()","using %s on %s",road_vehicle->g
 		// despite its name: try airplane
 		case NR_BAUE_AIRPORT_ROUTE:
 			// try airline (if we are wealthy enough) ...
-			if(  !air_transport  ||  finance_history_month[1][COST_CASH]<starting_money  ||  !create_air_transport_vehikel( start_stadt, end_stadt )) {
+			if(  !air_transport  ||  finance_history_month[1][COST_CASH] < starting_money  ||
+			     !end_stadt  ||  !create_air_transport_vehikel( start_stadt, end_stadt )  ) {
 				state = NR_BAUE_CLEAN_UP;
 			}
 			else {
