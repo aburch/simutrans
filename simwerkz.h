3d16d36d17645ea7bda8b8719b96d127fdb2b91a
Markus Pristovsek <prissi@physik.tu-berlin.de>
2012-05-29 21:51:20 +0000

    FIX: missed overide
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5752 8aca7d54-2c30-db11-9de9-000461428c89

--- simwerkz.h
+++ simwerkz.h
@@ -773,7 +773,7 @@ public:
 	wkz_rotate90_t() : werkzeug_t(WKZ_ROTATE90 | SIMPLE_TOOL) {}
 	image_id get_icon(spieler_t*) const OVERRIDE { return umgebung_t::networkmode ? IMG_LEER : icon; }
 	char const* get_tooltip(spieler_t const*) const OVERRIDE { return umgebung_t::networkmode ? translator::translate("deactivated in online mode") : translator::translate("Rotate map"); }
-	bool init( karte_t *welt, spieler_t * );
+	bool init( karte_t *welt, spieler_t * ) OVERRIDE;
 	bool is_init_network_save() const OVERRIDE { return !umgebung_t::networkmode; }
 	bool is_work_network_save() const OVERRIDE { return !umgebung_t::networkmode; }
 };
