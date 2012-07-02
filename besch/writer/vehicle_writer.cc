a833d2c602f06af2fd5fa37c1a31f0e313eafe91
Markus Pristovsek <prissi@physik.tu-berlin.de>
2012-07-02 21:45:23 +0000

    Right size for vehicle record
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5799 8aca7d54-2c30-db11-9de9-000461428c89

--- besch/writer/vehicle_writer.cc
+++ besch/writer/vehicle_writer.cc
@@ -56,7 +56,7 @@ void vehicle_writer_t::write_obj(FILE* fp, obj_node_t& parent, tabfileobj_t& obj
 	int i;
 	uint8  uv8;
 
-	int total_len = 37;
+	int total_len = 39;
 
 	// prissi: must be done here, since it may affect the len of the header!
 	string sound_str = ltrim( obj.get("sound") );
