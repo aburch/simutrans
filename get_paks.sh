53e186d3789ce7b7f1c967613766048da849f564
Markus Pristovsek <prissi@physik.tu-berlin.de>
2012-08-02 22:10:16 +0000

    further improving pak set selector sript under linux
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5852 8aca7d54-2c30-db11-9de9-000461428c89

--- get_paks.sh
+++ get_paks.sh
@@ -52,57 +52,122 @@ paksets=( \
 )
 
 # these sets are not in a simutrans folder in the zip
-malformedpaksets=( \
+nosimutranspaksets=( \
 "http://downloads.sourceforge.net/project/simutrans/pak64.contrast/pak64.Contrast_910.zip" \
 "http://hd.simutrans.com/release/PakHD_v04B_100-0.zip" \
 "http://downloads.sourceforge.net/project/simutrans/pak128.britain/pak128.Britain%20for%20111-0/pak128.Britain.1.11-111-2.zip" \
 "http://downloads.sourceforge.net/project/simutrans/pak128.japan/for%20Simutrans%20110.0.1/pak128.japan-110.0.1-version16-08-2011.zip" \
 "http://downloads.sourceforge.net/project/ironsimu/pak48.Excentrique/v018/pak48-excentrique_v018.zip" \
 )
-# not a zip
-#"http://simutrans.bilkinfo.de/pak64.ho-scale-latest.tar.gz" \
 
+tgzpaksets=( \
+ "http://simutrans.bilkinfo.de/pak64.ho-scale-latest.tar.gz" \
+)
+
+
+choices=()
+installpak=()
+
+echo "-- Choose at least one of these paks --"
+
+let setcount=0
+let choicecount=0
+let "maxcount = ${#paksets[*]}"
+while [ $setcount -lt $maxcount ]; do
+      installpak[choicecount]=0
+      urlname=${paksets[$setcount]}
+      zipname="${urlname##http*\/}"
+      choicename="${zipname%.zip}"
+      choicename="${choicename/simupak/pak}"
+      choices[choicecount]=$choicename
+      let "setcount += 1"
+      let "choicecount += 1"
+      echo "${choicecount}) ${choicename}"
+done
+
+let setcount=0
+let "maxcount = ${#nosimutranspaksets[*]}"
+while [ $setcount -lt $maxcount ]; do
+      installpak[choicecount]=0
+      urlname=${nosimutranspaksets[$setcount]}
+      zipname="${urlname##http*\/}"
+      choicename="${zipname%.zip}"
+      choicename="${choicename/simupak/pak}"
+      choices[choicecount]=$choicename
+      let "setcount += 1"
+      let "choicecount += 1"
+      echo "${choicecount}) ${choicename}"
+done
+
+while true; do
+  read -p "Which paks to install? (enter number or (i) to install or (x) to exit)" pak
+#exit?
+  if [[ $pak = [xX] ]]; then
+    exit
+  fi
+# test if installation now
+  if [[ $pak = [iI] ]]; then
+    echo "You will install now"
+    let setcount=0
+    while [ $setcount -lt $choicecount ]; do
+      if [ ${installpak[$setcount]} -gt 0 ]; then
+        let "displaycount=$setcount+1"
+        echo "${displaycount}) ${choices[$setcount]}"
+      fi
+      let "setcount += 1"
+    done
+    read -p "Is this correct? (y/n)" yn
+    if [[ $yn = [yY] ]]; then
+      break
+    fi
+    # edit again
+    echo "-- Choose again one of these paks --"
+    let setcount=0
+    while [ $setcount -lt $choicecount ]; do
+      echo "${choices[$setcount]}"
+      let "setcount += 1"
+    done
+    let "pak=0"
+  fi
+# otherwise it shoudl be a number
+  let "setcount=pak-1"
+  if [ $setcount -lt $choicecount ]; then
+    if [ $setcount -ge 0 ]; then
+      status=${installpak[$setcount]}
+      if [ $status -lt 1 ]; then
+        echo "adding ${choices[$setcount]}"
+        installpak[$setcount]=1
+      else
+        echo "not installing ${choices[$setcount]}"
+        installpak[$setcount]=0
+      fi
+    fi
+  fi
+done
+
+# first the regular pak sets
 pushd ..
 let setcount=0
 let "maxcount = ${#paksets[*]}"
 while [ $setcount -lt $maxcount ]; do
-  while true; do
-      urlname=${paksets[$setcount]}
-      zipname="${urlname##http*\/}"
-      let "setcount += 1"
-      echo $zipname
-      read -p "Do you wish to install $zipname ? (y/n/x)" yn
-      case $yn in
-          [Yy]* ) DownloadInstallZip $urlname $zipname; break;;
-          [Nn]* ) break;;
-          [Xx]* ) exit;;
-          * ) if [ $setcount -lt 2 ]; then
-            DownloadInstallZip $urlname $zipname;
-           fi
-           break;;
-      esac
-  done
+  if [ ${installpak[$setcount]} -gt 0 ]; then
+    urlname=${paksets[$setcount]}
+    zipname="${urlname##http*\/}"
+    DownloadInstallZip $urlname $zipname
+  fi
+  let "setcount += 1"
 done
 
 #for the wrong paksets, just change to simutrans folder
 popd
-let setcount=0
-let "maxcount = ${#malformedpaksets[*]}"
-while [ $setcount -lt $maxcount ]; do
-  while true; do
-      urlname=${malformedpaksets[$setcount]}
-      zipname="${urlname##http*\/}"
-      let "setcount += 1"
-      read -p "Do you wish to install $zipname ? (y/n/x)" yn
-      case $yn in
-          [Yy]* ) DownloadInstallZip $urlname $zipname; break;;
-          [Nn]* ) break;;
-          [Xx]* ) exit;;
-          * ) if [ $setcount -lt 2 ]; then
-            DownloadInstallZip $urlname $zipname;
-           fi
-           break;;
-      esac
-  done
- done
+while [ $setcount -lt $choicecount ]; do
+  if [ ${installpak[$setcount]} -gt 0 ]; then
+    let "correctcount=$setcount-$maxcount]"
+    echo $correctcount
+    urlname=${nosimutranspaksets[$correctcount]}
+    zipname="${urlname##http*\/}"
+    DownloadInstallZip $urlname $zipname
+  fi
+  let "setcount += 1"
+done
 exit
