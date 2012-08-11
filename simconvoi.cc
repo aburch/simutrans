16c3e467007bcdb8bab489bd18308238cc732c2b
James E. Petts <jamespetts@yahoo.com>
2012-08-11 18:05:41 +0100

    FIX: Steam locomotives with double tenders and Garretts with powered front portions were not treated correctly on reversing.

--- simconvoi.cc
+++ simconvoi.cc
@@ -2810,6 +2810,11 @@ convoi_t::reverse_order(bool rev)
 				// Check for double-headed tender locomotives
 				a += fahr[a]->get_besch()->get_nachfolger_count();
 			}
+			if(anz_vehikel > 1 && fahr[1]->get_besch()->get_leistung() == 0 && fahr[1]->get_besch()->get_nachfolger_count() == 1 && fahr[1]->get_besch()->get_nachfolger(0)->get_leistung() == 0 && fahr[1]->get_besch()->get_nachfolger(0)->get_preis() == 0)
+			{
+				// Multiple tenders or Garretts with powered front units.
+				a ++;
+			}
 		}
 
 		//Check whether this is a Garrett type vehicle
