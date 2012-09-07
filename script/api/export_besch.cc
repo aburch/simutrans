37b87d5c897435ba41af4c7a56215e5f1ba4e816
Dwachs <dwachs@gmx.net>
2012-09-07 11:18:16 +0000

    no code execution in assert()
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@5905 8aca7d54-2c30-db11-9de9-000461428c89

--- script/api/export_besch.cc
+++ script/api/export_besch.cc
@@ -46,7 +46,8 @@ void begin_besch_class(HSQUIRRELVM vm, const char* name, const void* (*func)(con
 	sq_pushstring(vm, name, -1);
 	// get extend_get class
 	sq_pushstring(vm, "extend_get", -1);
-	assert( SQ_SUCCEEDED(sq_get(vm, -3)) );
+	SQInteger res = sq_get(vm, -3);
+	assert( SQ_SUCCEEDED(res) );
 	// create class
 	sq_newclass(vm, true);
 	// store method to retrieve besch in typetag pointer
