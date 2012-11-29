79f232ec4f7c4a2bf6683192009b31435a632b40
Markus Pristovsek <prissi@physik.tu-berlin.de>
2012-11-29 15:31:34 +0000

    since no soon relase: step back game version number
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@6108 8aca7d54-2c30-db11-9de9-000461428c89

--- simversion.h
+++ simversion.h
@@ -17,8 +17,8 @@
 
 // Beware: SAVEGAME minor is often ahead of version minor when there were patches.
 // ==> These have no direct connection at all!
-#define SIM_SAVE_MINOR      1
-#define SIM_SERVER_MINOR    1
+#define SIM_SAVE_MINOR      0
+#define SIM_SERVER_MINOR    0
 
 #define MAKEOBJ_VERSION "55"
 
