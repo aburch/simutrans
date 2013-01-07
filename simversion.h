3223d201a61c853450d03638f9a5d36f1827c2d6
Dwachs <dwachs@gmx.net>
2013-01-07 19:40:13 +0000

    Release of makeobj 55.1 for simutrans 112.1
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@6246 8aca7d54-2c30-db11-9de9-000461428c89

--- simversion.h
+++ simversion.h
@@ -12,15 +12,15 @@
 
 #define SIM_VERSION_MAJOR 112
 #define SIM_VERSION_MINOR   1
-#define SIM_VERSION_PATCH   1
-#define SIM_VERSION_BUILD SIM_BUILD_NIGHTLY
+#define SIM_VERSION_PATCH   0
+#define SIM_VERSION_BUILD SIM_BUILD_RELEASE
 
 // Beware: SAVEGAME minor is often ahead of version minor when there were patches.
 // ==> These have no direct connection at all!
 #define SIM_SAVE_MINOR      2
 #define SIM_SERVER_MINOR    2
 
-#define MAKEOBJ_VERSION "55"
+#define MAKEOBJ_VERSION "55.1"
 
 #ifndef QUOTEME
 #	define QUOTEME_(x) #x
