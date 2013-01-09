03c6cfe81a24831e2c7cf5bc6ea0aa42597a6ea6
Markohs <jose.marcos.lopez@gmail.com>
2013-01-09 08:47:02 +0000

    Translate from german and add documentation to a file.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@6254 8aca7d54-2c30-db11-9de9-000461428c89

--- besch/spezial_obj_tpl.h
+++ besch/spezial_obj_tpl.h
@@ -1,6 +1,5 @@
 /*
- * Hilfstemplates, um Beschreibungen, die das Programm direkt benötigt, zu
- * initialisieren.
+ * Routines to manage special object descriptors (beschs) lists used in the program.
  */
 
 #ifndef __SPEZIAL_OBJ_TPL_H
@@ -11,10 +10,9 @@
 #include "../simdebug.h"
 
 
-/*
- * Beschreibung eines erforderlichen Objekts. Für die nachfolgenden Funktionen
- * wird eine Liste dieser Beschreibungen angelegt, wobei die Liste mit einem
- * "{NULL, NULL}" Eintrag terminiert wird.
+/**
+ * Descriptors (beschs) of required objects. The following functions manage
+ * the list. The list is "{NULL, NULL}" terminated.
  */
 template<class besch_t> struct spezial_obj_tpl {
 	const besch_t** besch;
@@ -22,9 +20,11 @@ template<class besch_t> struct spezial_obj_tpl {
 };
 
 
-/*
- * Ein Objektzeiger wird anhand der übergebenen Liste gesetzt, falls der Name
- * des Objektes einem der in der Liste erwähnten Objekte gehört.
+/**
+ * An object pointer is set on the passed list, if the name of the
+ * object belongs to one of the objects mentioned in the list.
+ * @param so List to operate over.
+ * @parem besch Descriptor to add.
  */
 template<class besch_t> bool register_besch(spezial_obj_tpl<besch_t>* so, const besch_t* besch)
 {
@@ -41,9 +41,9 @@ template<class besch_t> bool register_besch(spezial_obj_tpl<besch_t>* so, const 
 }
 
 
-/*
- * Überprüft die übergebene Liste, ob alle Objekte ungleich NULL, sprich
- * geladen sind.
+/**
+ * Verifies the passed list for all objects to be not NULL, ie are loaded.
+ * @param so List to check.
  */
 template<class besch_t> bool alles_geladen(spezial_obj_tpl<besch_t>* so)
 {
@@ -56,7 +56,11 @@ template<class besch_t> bool alles_geladen(spezial_obj_tpl<besch_t>* so)
 	return true;
 }
 
-
+/**
+ * Shows debug messages showing which descriptors lack definition.
+ * @param so List to check.
+ * @param count Number of elements to check.
+ */
 template<class besch_t> void warne_ungeladene(spezial_obj_tpl<besch_t>* so, int count)
 {
 	for (; count-- && so->name; ++so) {
