96365026c34f2e90c8a86cef8bf69d6d4592af54
Dwachs <dwachs@gmx.net>
2013-01-16 19:31:56 +0000

    FIX: navigation with buttons in tab panel
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@6280 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/components/gui_tab_panel.cc
+++ gui/components/gui_tab_panel.cc
@@ -71,7 +71,7 @@ bool gui_tab_panel_t::action_triggered(gui_action_creator_t *komp, value_t)
 
 bool gui_tab_panel_t::infowin_event(const event_t *ev)
 {
-	if(  required_groesse.x>groesse.x  &&  ev->ev_class!=EVENT_KEYBOARD  &&  ev->ev_code==MOUSE_LEFTBUTTON  ) {
+	if(  (required_groesse.x>groesse.x  ||  offset_tab > 0)  &&  ev->ev_class!=EVENT_KEYBOARD  &&  ev->ev_code==MOUSE_LEFTBUTTON  ) {
 		// buttons pressed
 		if(  left.getroffen(ev->cx, ev->cy)  ) {
 			event_t ev2 = *ev;
