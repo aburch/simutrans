fb1c216478199adfcc970e82723e553ccce4ed91
Markus Pristovsek <prissi@physik.tu-berlin.de>
2013-03-23 22:19:11 +0000

    ADD: show used player colros with an X
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@6411 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/kennfarbe.cc
+++ gui/kennfarbe.cc
@@ -8,6 +8,7 @@
 #include "../simdebug.h"
 #include "../simevent.h"
 #include "../simimg.h"
+#include "../simworld.h"
 #include "../besch/skin_besch.h"
 #include "../simskin.h"
 #include "../dataobj/translator.h"
@@ -40,9 +41,18 @@ farbengui_t::farbengui_t(spieler_t *sp) :
 	c1.set_pos( koord(D_MARGIN_LEFT,y) );
 	add_komponente( &c1 );
 	y += LINESPACE+D_V_SPACE;
+	// now get all colors (but the one of the current player)
+	uint32 used_colors1 = 0;
+	uint32 used_colors2 = 0;
+	for(  int i=0;  i<MAX_PLAYER_COUNT;  i++  ) {
+		if(  i!=sp->get_player_nr()  &&  sp->get_welt()->get_spieler(i)  ) {
+			used_colors1 |= 1 << (sp->get_welt()->get_spieler(i)->get_player_color1() / 8);
+			used_colors2 |= 1 << (sp->get_welt()->get_spieler(i)->get_player_color2() / 8);
+		}
+	}
 	// color buttons
 	for(unsigned i=0;  i<28;  i++) {
-		player_color_1[i].init( button_t::box_state, "", koord( D_MARGIN_LEFT+(i%14)*(D_BUTTON_HEIGHT+D_H_SPACE), y+(i/14)*(D_BUTTON_HEIGHT+D_H_SPACE) ), koord(D_BUTTON_HEIGHT,D_BUTTON_HEIGHT) );
+		player_color_1[i].init( button_t::box_state, (used_colors1 & (1<<i) ? "X" : ""), koord( D_MARGIN_LEFT+(i%14)*(D_BUTTON_HEIGHT+D_H_SPACE), y+(i/14)*(D_BUTTON_HEIGHT+D_H_SPACE) ), koord(D_BUTTON_HEIGHT,D_BUTTON_HEIGHT) );
 		player_color_1[i].background = i*8+4;
 		player_color_1[i].add_listener(this);
 		add_komponente( player_color_1+i );
@@ -56,7 +66,7 @@ farbengui_t::farbengui_t(spieler_t *sp) :
 	y += LINESPACE+D_V_SPACE;
 	// second color buttons
 	for(unsigned i=0;  i<28;  i++) {
-		player_color_2[i].init( button_t::box_state, "", koord( D_MARGIN_LEFT+(i%14)*(D_BUTTON_HEIGHT+D_H_SPACE), y+(i/14)*(D_BUTTON_HEIGHT+D_H_SPACE) ), koord(D_BUTTON_HEIGHT,D_BUTTON_HEIGHT) );
+		player_color_2[i].init( button_t::box_state, (used_colors2 & (1<<i) ? "X" : ""), koord( D_MARGIN_LEFT+(i%14)*(D_BUTTON_HEIGHT+D_H_SPACE), y+(i/14)*(D_BUTTON_HEIGHT+D_H_SPACE) ), koord(D_BUTTON_HEIGHT,D_BUTTON_HEIGHT) );
 		player_color_2[i].background = i*8+4;
 		player_color_2[i].add_listener(this);
 		add_komponente( player_color_2+i );
