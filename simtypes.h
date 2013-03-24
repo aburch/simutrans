ca514d403ef2a95d047e710ff46e2f9434aebeef
Dwachs <dwachs@gmx.net>
2013-03-24 13:32:36 +0000

    (MCollett) compile with clang
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@6413 8aca7d54-2c30-db11-9de9-000461428c89

--- simtypes.h
+++ simtypes.h
@@ -17,6 +17,10 @@
 #	define ALLOCA(type, name, count) type* name = static_cast<type*>(alloca(sizeof(type) * (count)))
 #
 # define inline _inline
+#
+#elif defined __clang__
+#	include <alloca.h>
+#	define ALLOCA(type, name, count) type* name = static_cast<type*>(alloca(sizeof(type) * (count)))
 #else
 #	define ALLOCA(type, name, count) type name[count]
 #endif
