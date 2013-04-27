7cf54f3167de34f771ffda399567910aaec98fc0
Dwachs <dwachs@gmx.net>
2013-04-27 12:01:54 +0000

    FIX: remove some calls to welt->set_dirty, reset dirty flag for toolbars
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@6480 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/thing_info.cc
+++ gui/thing_info.cc
@@ -49,8 +49,6 @@ ding_infowin_t::ding_infowin_t(const ding_t* ding) :
  */
 void ding_infowin_t::zeichnen(koord pos, koord gr)
 {
-	set_dirty();
-
 	buf.clear();
 	info(buf);
 	textarea.recalc_size();
