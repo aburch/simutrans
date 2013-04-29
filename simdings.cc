289b8a315e74782546e737831dd931206784512e
Markus Pristovsek <prissi@physik.tu-berlin.de>
2013-04-29 20:56:44 +0000

    Even less redraws
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@6486 8aca7d54-2c30-db11-9de9-000461428c89

--- simdings.cc
+++ simdings.cc
@@ -323,18 +323,22 @@ void ding_t::mark_image_dirty(image_id bild,sint16 yoff) const
 			// vehicles need finer steps to appear smoother
 			v->get_screen_offset( xpos, ypos, get_tile_raster_width() );
 		}
-		// too close to border => set dirty to be sure (smoke, skyscrapes, birds, or the like)
-		KOORD_VAL dummy, ybild;
-		display_get_image_offset( bild, &dummy, &ybild, &dummy, &dummy );
-		const sint16 distance_to_border = 3 - (yoff+get_yoff()+ybild)/(rasterweite/4);
-		if(  pos.x <= distance_to_border  ||  pos.y <= distance_to_border  ) {
-			welt->set_background_dirty();
-		}
 		// better not try to twist your brain to follow the retransformation ...
 		const koord diff = get_pos().get_2d()-welt->get_world_position()-welt->get_view_ij_offset();
-		const sint16 x = (diff.x-diff.y)*(rasterweite/2) + tile_raster_scale_x(get_xoff(), rasterweite) + xpos;
-		const sint16 y = (diff.x+diff.y)*(rasterweite/4) + tile_raster_scale_y( yoff+get_yoff()-get_pos().z*TILE_HEIGHT_STEP, rasterweite) + ((display_get_width()/rasterweite)&1)*(rasterweite/4) + ypos;
+		const sint16 x = (diff.x-diff.y)*(rasterweite/2) + tile_raster_scale_x(get_xoff(), rasterweite) + xpos + welt->get_x_off();
+		const sint16 y = (diff.x+diff.y)*(rasterweite/4) + tile_raster_scale_y( yoff+get_yoff()-get_pos().z*TILE_HEIGHT_STEP, rasterweite) + ((display_get_width()/rasterweite)&1)*(rasterweite/4) + ypos + welt->get_y_off();
 		// mark the region after the image as dirty
-		display_mark_img_dirty( bild, x+welt->get_x_off(), y+welt->get_y_off() );
+		display_mark_img_dirty( bild, x, y );
+
+		// too close to border => set dirty to be sure (smoke, skyscrapes, birds, or the like)
+		KOORD_VAL xbild, ybild, wbild, hbild;
+		display_get_image_offset( bild, &xbild, &ybild, &wbild, &hbild );
+		const sint16 distance_to_border = 3 - (yoff+get_yoff()+ybild)/(rasterweite/4);
+		if(  pos.x <= distance_to_border  ||  pos.y <= distance_to_border  ) {
+			// but only if the image is actually visible ...
+			if(  x+xbild+wbild >= 0  &&  xpos <= display_get_width()  &&  y+ybild+hbild >= 0  &&  ypos+ybild < display_get_height()  ) {
+				welt->set_background_dirty();
+			}
+		}
 	}
 }
