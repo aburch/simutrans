b286302f6a2d2fa3bec52a5341c96fb00c916a2d
Nathanael Nerode <ncn_floss10@fastmail.fm>
2013-06-01 13:29:30 -0400

    Correct the save game version for the accounting merge (it was messed up by merge from experimental)

--- simversion.h
+++ simversion.h
@@ -11,14 +11,14 @@
 #define SIM_BUILD_RELEASE           2
 
 #define SIM_VERSION_MAJOR 112
-#define SIM_VERSION_MINOR   3.
+#define SIM_VERSION_MINOR   3
 #define SIM_VERSION_PATCH   0
 #define SIM_VERSION_BUILD SIM_BUILD_NIGHTLY
 
 // Beware: SAVEGAME minor is often ahead of version minor when there were patches.
 // ==> These have no direct connection at all!
-#define SIM_SAVE_MINOR      3
-#define SIM_SERVER_MINOR    3
+#define SIM_SAVE_MINOR      5
+#define SIM_SERVER_MINOR    5
 
 #define EX_VERSION_MAJOR	11
 #define EX_VERSION_MINOR	9007
