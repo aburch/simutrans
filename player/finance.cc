ccb494a8f345241cff063705723c36d6123da9af
Nathanael Nerode <ncn_floss10@fastmail.fm>
2013-06-03 23:06:05 -0400

    Fix mindless typo in last commit of finance.cc

--- player/finance.cc
+++ player/finance.cc
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-#include "../utils/float32e8.h"
+#include "../utils/float32e8_t.h"
 #include "../simworld.h"
 #include "../besch/haus_besch.h"
 #include "../dataobj/loadsave.h"
