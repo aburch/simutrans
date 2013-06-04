fbaa8a77b56d96f81b1d5992b4d06ec3aa4bab46
Nathanael Nerode <ncn_floss10@fastmail.fm>
2013-06-04 12:00:00 -0400

    Const correctness for new method in simdepot.h

--- simdepot.h
+++ simdepot.h
@@ -59,7 +59,7 @@ public:
 	 *   - 0 if we don't want to filter by traction type
 	 *   - a bitmask of possible traction types; we need only match one
 	 */
-	inline bool is_suitable_for( const vehikel_t * test_vehicle, const uint8 traction_types = 0) {
+	inline bool is_suitable_for( const vehikel_t * test_vehicle, const uint8 traction_types = 0) const {
 		assert(test_vehicle != NULL);
 
 		// Owner must be the same
