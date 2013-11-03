8948a7599912bc735920b10a2ec103bc472fd526
Markus Pristovsek <prissi@physik.tu-berlin.de>
2013-11-03 21:41:57 +0000

    (z9999+) Display correct maintenance
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@6886 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/convoi_detail_t.cc
+++ gui/convoi_detail_t.cc
@@ -302,7 +302,7 @@ void gui_vehicleinfo_t::zeichnen(koord offset)
 				display_proportional_clip( pos.x+w+offset.x+len, pos.y+offset.y+total_height+extra_y, number, ALIGN_LEFT, price>0?MONEY_PLUS:MONEY_MINUS, true );
 				extra_y += LINESPACE;
 
-				if(  int cost = v->get_besch()->get_maintenance()  ) {
+				if(  int cost = cnv->get_welt()->scale_with_month_length(v->get_besch()->get_maintenance())  ) {
 					KOORD_VAL len = display_proportional_clip( pos.x+w+offset.x, pos.y+offset.y+total_height+extra_y, translator::translate("Maintenance"), ALIGN_LEFT, COL_BLACK, true );
 					len += display_proportional_clip( pos.x+w+offset.x+len, pos.y+offset.y+total_height+extra_y, ": ", ALIGN_LEFT, COL_BLACK, true );
 					money_to_string( number, cost/(100.0) );
@@ -337,7 +337,7 @@ void gui_vehicleinfo_t::zeichnen(koord offset)
 				display_proportional_clip( pos.x+w+offset.x+len, pos.y+offset.y+total_height+extra_y, number, ALIGN_LEFT, cost>=0?MONEY_PLUS:MONEY_MINUS, true );
 				extra_y += LINESPACE;
 				// Fixed costs
-				if(  int cost = v->get_besch()->get_maintenance()  ) {
+				if(  int cost = cnv->get_welt()->scale_with_month_length(v->get_besch()->get_maintenance())  ) {
 					KOORD_VAL len = display_proportional_clip( pos.x+w+offset.x, pos.y+offset.y+total_height+extra_y, translator::translate("Maintenance"), ALIGN_LEFT, COL_BLACK, true );
 					len += display_proportional_clip( pos.x+w+offset.x+len, pos.y+offset.y+total_height+extra_y, ": ", ALIGN_LEFT, COL_BLACK, true );
 					money_to_string( number, cost/(100.0) );
--- gui/depot_frame.cc
+++ gui/depot_frame.cc
@@ -1357,8 +1357,8 @@ void depot_frame_t::zeichnen(koord pos, koord groesse)
 				txt_convoi_value.printf("%s %8s", translator::translate("Restwert:"), buf );
 
 				txt_convoi_cost.clear();
-				if(  sint32 fix_cost = cnv->get_fix_cost()  ) {
-					money_to_string(  buf, cnv->get_purchase_cost() / 100.0, false );
+				if(  sint32 fix_cost = depot->get_welt()->scale_with_month_length(cnv->get_fix_cost())  ) {
+					money_to_string(  buf, fix_cost / 100.0, false );
 					txt_convoi_cost.printf( translator::translate("Cost: %8s (%.2f$/km %.f$/m)\n"), buf, (double)cnv->get_running_cost()/100.0, (double)fix_cost/100.0 );
 				}
 				else {
@@ -1549,7 +1549,7 @@ void depot_frame_t::draw_vehicle_info_text(koord pos)
 			buf.append( "\n" );
 		}
 
-		if(  sint32 fix_cost = veh_type->get_maintenance()  ) {
+		if(  sint32 fix_cost = depot->get_welt()->scale_with_month_length(veh_type->get_maintenance())  ) {
 			char tmp[128];
 			money_to_string( tmp, veh_type->get_preis() / 100.0, false );
 			buf.printf( translator::translate("Cost: %8s (%.2f$/km %.f$/m)\n"), tmp, veh_type->get_betriebskosten()/100.0, fix_cost/100.0 );
