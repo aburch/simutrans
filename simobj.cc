7f52503867e2de6219b6511a88877b158a44ce53
Dwachs <dwachs@gmx.net>
2014-01-29 07:37:07 +0000

    FIX: correctly mark image dirty - take offsets into account
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7045 8aca7d54-2c30-db11-9de9-000461428c89

--- simobj.cc
+++ simobj.cc
@@ -372,7 +372,7 @@ void obj_t::mark_image_dirty(image_id bild, sint16 yoff) const
 		}
 
 		viewport_t *vp = welt->get_viewport();
-		scr_coord scr_pos = vp->get_screen_coord(get_pos(),koord(xpos,ypos+yoff));
+		scr_coord scr_pos = vp->get_screen_coord(get_pos(), koord(xpos + get_xoff(), ypos + get_yoff() + yoff));
 
 		// mark the region after the image as dirty
 		display_mark_img_dirty( bild, scr_pos.x, scr_pos.y );
