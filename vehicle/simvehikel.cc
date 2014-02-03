d675371450cbd648160ec9ff1bcd7db854ecb603
James E. Petts <jamespetts@yahoo.com>
2014-02-03 21:22:50 +0000

    CHANGE: Lakes are now treated differently from the sea as regards way constraints in that lakes are assumed to have all possible permissive constraints, whereas the sea is (as was previously the case) assumed to have no constraints.

--- vehicle/simvehikel.cc
+++ vehicle/simvehikel.cc
@@ -4428,10 +4428,18 @@ grund_t* schiff_t::betrete_feld()
 
 bool schiff_t::ist_befahrbar(const grund_t *bd) const
 {
-	if(  bd->ist_wasser()  ) 
+	if(bd->ist_wasser()) 
 	{
-		// If there are permissive constraints, this vehicle cannot use the open water.
-		return besch->get_way_constraints().get_permissive() == 0;
+		// If there are permissive constraints, this vehicle cannot
+		// traverse open seas, but it may use lakes. 
+		if(bd->get_hoehe() > welt->get_grundwasser())
+		{
+			return true;
+		}
+		else
+		{	
+			return besch->get_way_constraints().get_permissive() == 0;
+		}
 	}
 	// channel can have more stuff to check
 	const weg_t *w = bd->get_weg(water_wt);
