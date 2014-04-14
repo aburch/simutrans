eb7331f0b8356f64d152326c716cfddd1e962de4
Markus Pristovsek <prissi@physik.tu-berlin.de>
2014-04-14 12:26:06 +0000

    Allow river or fences below low bridges
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7140 8aca7d54-2c30-db11-9de9-000461428c89

--- bauer/brueckenbauer.cc
+++ bauer/brueckenbauer.cc
@@ -284,9 +284,14 @@ koord3d brueckenbauer_t::finde_ende(spieler_t *sp, koord3d pos, const koord zv, 
 			return koord3d::invalid;
 		}
 
-		if(  welt->get_settings().get_way_height_clearance() == 2  &&  gr->get_hoehe() == max_height-1  &&  gr->get_weg_nr(0)  ) {
-			error_msg = "Not enough clearance.";
-			return koord3d::invalid;
+		if(  welt->get_settings().get_way_height_clearance() == 2  &&  gr->get_hoehe() == max_height-1  )
+			if(  weg_t *w = gr->get_weg_nr(0)  ) {
+				// now check if this is not a fence or a river (max_speed == 0)
+				if(  w->get_max_speed() > 0  ) {
+					error_msg = "Not enough clearance.";
+					return koord3d::invalid;
+				}
+			}
 		}
 
 		// check for height
