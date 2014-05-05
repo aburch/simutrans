cbfbf8a774089e4d0a3ca0bfe5dc6f97ba36e428
Christoph Mallon <christoph.mallon@gmx.de>
2014-05-05 07:01:01 +0000

    cleanup: Use proper if cascade.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7210 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/banner.cc
+++ gui/banner.cc
@@ -43,13 +43,11 @@
 #define L_DIALOG_WIDTH (D_MARGIN_LEFT + 3*D_BUTTON_WIDTH + 2*D_H_SPACE + D_MARGIN_RIGHT)
 
 #ifdef _OPTIMIZED
-	#define L_DEBUG_TEXT " (optimized)"
+#	define L_DEBUG_TEXT " (optimized)"
+#elif defined DEBUG
+#	define L_DEBUG_TEXT " (debug)"
 #else
-	#ifdef DEBUG
-		#define L_DEBUG_TEXT " (debug)"
-	#else
-		#define L_DEBUG_TEXT
-	#endif
+#	define L_DEBUG_TEXT
 #endif
 
 // colors
