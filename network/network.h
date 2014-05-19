d412997046c93cb18b6dcc9a5c9e07930c100690
Markus Pristovsek <prissi@physik.tu-berlin.de>
2014-05-19 21:59:01 +0000

    Mingw seems does not know about _WIN32_WINNT_WINXP correctly, so we default to IP4 if requested for Win2k compile (unbreak nightly)
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7219 8aca7d54-2c30-db11-9de9-000461428c89

--- network/network.h
+++ network/network.h
@@ -12,7 +12,10 @@
 #	ifndef IPV6_V6ONLY
 #		define IPV6_V6ONLY (27)
 #	endif
-
+#	if WINVER<=0x0500
+		// compiling for Windows 2000
+#		define USE_IP4_ONLY
+#	endif
 #	define GET_LAST_ERROR() WSAGetLastError()
 #	include <errno.h>
 #	undef  EWOULDBLOCK
