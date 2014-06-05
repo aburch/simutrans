c184d2f306add23db83d52ec165e00a0e8130517
TurfIt <turfittt@gmail.com>
2014-06-05 20:08:43 +0000

    FIX: depot allowing selection of line with wrong type
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7248 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/depot_frame.cc
+++ gui/depot_frame.cc
@@ -886,6 +886,9 @@ void depot_frame_t::update_data()
 		if(  !last_selected_line.is_bound()  ) {
 			// try last general line
 			last_selected_line = schedule_list_gui_t::selected_line[ depot->get_besitzer()->get_player_nr() ][ 0 ];
+			if(  last_selected_line.is_bound()  &&  last_selected_line->get_linetype() != depot->get_line_type()  ) {
+				last_selected_line = linehandle_t();
+			}
 		}
 	}
 	if(  last_selected_line.is_bound()  ) {
