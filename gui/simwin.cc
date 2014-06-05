b6847d7a70609d6a4d8c6749c802497400b72146
TurfIt <turfittt@gmail.com>
2014-06-05 19:33:32 +0000

    FIX: proper background for non-skinned main toolbar
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7247 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/simwin.cc
+++ gui/simwin.cc
@@ -1484,7 +1484,7 @@ void win_display_flush(double konto)
 		}
 	}
 	else {
-		display_fillbox_wh_rgb( 0, 0, disp_width, menu_height, MN_GREY2, false );
+		display_fillbox_wh( 0, 0, disp_width, menu_height, MN_GREY2, false );
 	}
 	// .. extra logic to enable tooltips
 	tooltip_element = menu_height > get_maus_y() ? main_menu : NULL;
