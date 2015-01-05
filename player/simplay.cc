ad57e0e1b675ff23b82045a2817c17a22ec031a9
James E. Petts <jamespetts@yahoo.com>
2015-01-05 13:58:21 +0000

    FIX: Do not auto-mothball roads and canals from a liquidated player.

--- player/simplay.cc
+++ player/simplay.cc
@@ -693,7 +693,7 @@ void spieler_t::ai_bankrupt()
 								else {
 									weg_t *way = (weg_t *)obj;
 									const weg_besch_t* mothballed_type = wegbauer_t::way_search_mothballed(way->get_waytype(), (weg_t::system_type)way->get_besch()->get_styp());
-									if(mothballed_type)
+									if(mothballed_type && way->get_waytype() != road_wt && way->get_waytype() != water_wt)
 									{
 										way->set_besch(mothballed_type);
 									}
