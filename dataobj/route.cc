66e760e56fcb89f423db59b791133a3b65b0678c
Dwachs <dwachs@gmx.net>
2015-01-08 16:21:23 +0000

    FIX: best node is determined by f-value, which is the heuristic
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7461 8aca7d54-2c30-db11-9de9-000461428c89

--- dataobj/route.cc
+++ dataobj/route.cc
@@ -375,7 +375,7 @@ bool route_t::intern_calc_route(karte_t *welt, const koord3d ziel, const koord3d
 			break;
 		}
 
-		uint32 topnode_g = !queue.empty() ? queue.front()->g : max_cost;
+		uint32 topnode_f = !queue.empty() ? queue.front()->f : max_cost;
 
 		const ribi_t::ribi way_ribi =  fahr->get_ribi(gr);
 		// testing all four possible directions
@@ -456,10 +456,9 @@ bool route_t::intern_calc_route(karte_t *welt, const koord3d ziel, const koord3d
 					}
 				}
 
-
-				if(  new_g <= topnode_g  ) {
+				if(  new_f <= topnode_f  ) {
 					// do not put in queue if the new node is the best one
-					topnode_g = new_g;
+					topnode_f = new_f;
 					if(  new_top  ) {
 						queue.insert(new_top);
 					}
