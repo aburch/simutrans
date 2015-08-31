090034051541bfdb7765a20278773fdc5b763f86
James E. Petts <jamespetts@yahoo.com>
2015-09-01 00:12:20 +0100

    FIX: The maintenance cost for signals/signs incorrectly showed as negative in the tooltip

--- simtool.cc
+++ simtool.cc
@@ -5200,7 +5200,7 @@ char const* tool_build_roadsign_t::get_tooltip(player_t const*) const
 		char tip[256];
 		sprintf(tip, "%s, %s %i%s", translator::translate(besch->get_name()), translator::translate("Max. speed:"), speed_to_kmh(besch->get_max_speed()), translator::translate("km/h"));
 
-		return tooltip_with_price_maintenance(welt, tip, -besch->get_preis(), -besch->get_maintenance());
+		return tooltip_with_price_maintenance(welt, tip, -besch->get_preis(), besch->get_maintenance());
 	}
 	return NULL;
 }
