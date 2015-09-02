dd28ccfbebdd27c9c7609ccfc5e19a5869b4a711
Markus Pristovsek <prissi@physik.tu-berlin.de>
2015-09-02 11:49:06 +0000

    Use timeGetTime for windows Posix builds too
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7567 8aca7d54-2c30-db11-9de9-000461428c89

--- simsys_posix.cc
+++ simsys_posix.cc
@@ -11,60 +11,6 @@
 #ifndef _MSC_VER
 #include <unistd.h>
 #include <sys/time.h>
-#else
-#include <WinSock2.h> // For timeval.
-
-// Simple system time adapter. Timezone not supported as never used.
-int gettimeofday(struct timeval *tv, struct timezone *tz)
-{
-	// File time object used for standard time reference.
-	FILETIME time;
-
-	// Converter used to convert time into comparable units.
-	ULARGE_INTEGER converter;
-
-	// The comparable time for output.
-	ULONGLONG gtodtime = 0;
-
-	// Start of epoch (January 1, 1970), defined by opengroup and after FILETIME epoch (January 1, 1601).
-	// Should be constant if allowed.
-	SYSTEMTIME EPOCH;
-	EPOCH.wDay = 1;
-	EPOCH.wDayOfWeek = 4;
-	EPOCH.wHour = 0;
-	EPOCH.wMilliseconds = 0;
-	EPOCH.wMinute = 0;
-	EPOCH.wMonth = 1;
-	EPOCH.wSecond = 0;
-	EPOCH.wYear = 1970;
-
-	// Convert epoch to file time then to comparable units.
-	SystemTimeToFileTime(&EPOCH, &time);
-	converter.LowPart = time.dwLowDateTime;
-	converter.HighPart = time.dwHighDateTime;
-	gtodtime-= converter.QuadPart; // Underflow does not mater as overflow corrects.
-
-	// Get the current time. The accuracy is OS dependant with newer OS versions being more accurate.
-#if WINVER >= _WIN32_WINNT_WIN8
-	GetSystemTimePreciseAsFileTime(&time) // Microsecond accurate, required accuracy.
-#else
-	GetSystemTimeAsFileTime(&time); // Sub-optimal accuracy but has to do.
-#endif
-	// Convert current time to comparable units.
-	converter.LowPart = time.dwLowDateTime;
-	converter.HighPart = time.dwHighDateTime;
-	gtodtime+= converter.QuadPart;
-
-	// Drop precision from "100-nanosecond intervals" to 1us intervals.
-	gtodtime/= 10;
-
-	// Fill timeval structure to meet specification.
-	tv->tv_usec = (long)(gtodtime % 1000000lu);
-	tv->tv_sec = (long)(gtodtime / 1000000lu);
-
-	// Always "success" as Windows does not give any errors doing this.
-	return 0;
-}
 #endif
 
 #include <signal.h>
@@ -179,6 +125,7 @@ static timeval first;
 
 uint32 dr_time()
 {
+#ifndef _MSC_VER
 	timeval second;
 	gettimeofday(&second,NULL);
 	if (first.tv_usec > second.tv_usec) {
@@ -188,6 +135,9 @@ uint32 dr_time()
 	}
 
 	return (second.tv_sec - first.tv_sec)*1000ul + (second.tv_usec - first.tv_usec)/1000ul;
+#else
+	return timeGetTime();
+#endif
 }
 
 void dr_sleep(uint32 msec)
@@ -230,6 +180,8 @@ static void posix_sigterm(int)
 int main(int argc, char **argv)
  {
 	signal( SIGTERM, posix_sigterm );
+#ifndef _MSC_VER
  	gettimeofday(&first,NULL);
+#endif
  	return sysmain(argc, argv);
  }
