36fa1fd7207f24ce1c0e6aca7957ed3794f05392
James E. Petts <jamespetts@yahoo.com>
2015-09-06 14:47:32 +0100

    FIX: The correct (main) choose aspect is shown when a choose signal is reserved in the ordinary way without finding an alternative platform

--- vehicle/simvehicle.cc
+++ vehicle/simvehicle.cc
@@ -4743,9 +4743,10 @@ sint32 rail_vehicle_t::block_reserver(route_t *route, uint16 start_index, uint16
 		{
 			if(counter -- || pre_signals.empty() || (reached_end_of_loop && (early_platform_index == INVALID_INDEX || last_stop_signal_index < early_platform_index)))
 			{
+				const bool use_no_choose_aspect = signal->get_besch()->is_choose_sign(); // TODO: Allow this to set a choose aspect selectively 
 				if(signal->get_besch()->get_working_method() == absolute_block || signal->get_besch()->get_working_method() == token_block || signal->get_besch()->get_working_method() == cab_signalling)
 				{
-					signal->set_state(roadsign_t::clear);
+					signal->set_state(use_no_choose_aspect ? roadsign_t::clear_no_choose : roadsign_t::clear);
 				}
 				if(signal->get_besch()->get_working_method() == track_circuit_block)
 				{
@@ -4764,66 +4765,66 @@ sint32 rail_vehicle_t::block_reserver(route_t *route, uint16 start_index, uint16
 						default:
 							if(next_signal_index == INVALID_INDEX || signal->get_pos() != route->position_bei(next_signal_index))
 							{
-								signal->set_state(roadsign_t::clear);
+								signal->set_state(use_no_choose_aspect ? roadsign_t::clear_no_choose : roadsign_t::clear);
 							}
 							break;
 						case 3:
 							if(counter + add_value >= 1)
 							{
-								signal->set_state(roadsign_t::clear);
+								signal->set_state(use_no_choose_aspect ? roadsign_t::clear_no_choose : roadsign_t::clear);
 							}
 							else if(next_signal_index == INVALID_INDEX || signal->get_pos() != route->position_bei(next_signal_index))
 							{
 								if(signal->get_state() == roadsign_t::danger)
 								{
-									signal->set_state(roadsign_t::caution);
+									signal->set_state(use_no_choose_aspect ? roadsign_t::caution_no_choose : roadsign_t::caution);
 								}
 							}
 							break;
 						case 4:
 							if(counter + add_value >= 2)
 							{
-								signal->set_state(roadsign_t::clear);
+								signal->set_state(use_no_choose_aspect ? roadsign_t::clear_no_choose : roadsign_t::clear);
 							}
 							else if(counter + add_value >= 1)
 							{
-								if(signal->get_state() == roadsign_t::danger || signal->get_state() == roadsign_t::caution)
+								if(signal->get_state() == roadsign_t::danger || signal->get_state() == roadsign_t::caution || signal->get_state() == roadsign_t::caution_no_choose)
 								{
-									signal->set_state(roadsign_t::preliminary_caution);
+									signal->set_state(use_no_choose_aspect ? roadsign_t::preliminary_caution_no_choose : roadsign_t::preliminary_caution);
 								}	
 							}
 							else if(next_signal_index == INVALID_INDEX || signal->get_pos() != route->position_bei(next_signal_index))
 							{
 								if(signal->get_state() == roadsign_t::danger)
 								{
-									signal->set_state(roadsign_t::caution);
+									signal->set_state(use_no_choose_aspect ? roadsign_t::caution_no_choose : roadsign_t::caution);
 								}
 							}
 							break;
 						case 5:
 							if(counter + add_value >= 3)
 							{
-								signal->set_state(roadsign_t::clear);
+								signal->set_state(use_no_choose_aspect ? roadsign_t::clear_no_choose : roadsign_t::clear);
 							}
 							else if(counter + add_value >= 2)
 							{
-								if(signal->get_state() == roadsign_t::danger || signal->get_state() == roadsign_t::caution || signal->get_state() == roadsign_t::preliminary_caution)
+								if(signal->get_state() == roadsign_t::danger || signal->get_state() == roadsign_t::caution || signal->get_state() == roadsign_t::preliminary_caution || signal->get_state() == roadsign_t::caution_no_choose || signal->get_state() == roadsign_t::preliminary_caution_no_choose)
 								{
-									signal->set_state(roadsign_t::advance_caution);
+									signal->set_state(use_no_choose_aspect ? roadsign_t::advance_caution_no_choose : roadsign_t::advance_caution);
 								}
 							}
 							else if(counter + add_value >= 1)
 							{
-								if(signal->get_state() == roadsign_t::danger || signal->get_state() == roadsign_t::caution)
+								if(signal->get_state() == roadsign_t::danger || signal->get_state() == roadsign_t::caution || signal->get_state() == roadsign_t::caution_no_choose)
 								{
-									signal->set_state(roadsign_t::preliminary_caution);
+									signal->set_state(use_no_choose_aspect ? roadsign_t::preliminary_caution_no_choose : roadsign_t::preliminary_caution);
 								}
 							}
 							else if(next_signal_index == INVALID_INDEX || signal->get_pos() != route->position_bei(next_signal_index))
 							{
 								if(signal->get_state() == roadsign_t::danger)
 								{
-									signal->set_state(roadsign_t::caution);
+									signal->set_state(use_no_choose_aspect ? roadsign_t::caution_no_choose : roadsign_t::caution);
 								}
 							}
 							break;
