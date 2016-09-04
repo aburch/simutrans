84e76a38374adadbdb60deb53f25f3930caf4b57
James E. Petts <jamespetts@yahoo.com>
2016-09-04 11:33:50 +0100

    CODE: Remove unused magic number (Dwachs, from Standard)

--- gui/simwin.h
+++ gui/simwin.h
@@ -103,8 +103,7 @@ enum magic_numbers {
 	magic_halt_info=magic_convoi_detail+65536,
 	magic_halt_detail=magic_halt_info+65536,
 	magic_replace=magic_halt_detail+65536,
-	magic_halt_departure=magic_replace+65536,
-	magic_toolbar=magic_halt_departure+65536,
+	magic_toolbar=magic_replace+65536,
 	magic_info_pointer=magic_toolbar+256,
 	magic_max = magic_info_pointer+843
 };
