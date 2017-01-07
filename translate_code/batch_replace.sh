d0832c5089bea250c71154868fd38daefdb2cfd7
Dwachs <dwachs@gmx.net>
2017-01-07 16:23:01 +0000

    only change files that contain the pattern
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7989 8aca7d54-2c30-db11-9de9-000461428c89

--- translate_code/batch_replace.sh
+++ translate_code/batch_replace.sh
@@ -3,6 +3,19 @@ while IFS=, read from to
 do
 	echo "Replace $from by $to"
 	REPLACE="s/$from/$to/g"
-	find ../ -name "*.h"  -exec sed -i "${REPLACE}" {} \;
-	find ../ -name "*.cc" -exec sed -i "${REPLACE}" {} \;
+
+	find ../ -name "*.cc" -or -name "*.h" | while read fname; do
+
+		sed "${REPLACE}" $fname > tmp.sed
+		cmp -s tmp.sed $fname
+		res=$?
+
+		if [ $res -eq 1 ]
+		then
+			echo "Replace in $fname"
+			mv tmp.sed $fname
+		else
+			rm tmp.sed
+		fi
+	done
 done < $1
