2fff30523f42abed76640d756213fc5416e07bb0
James E. Petts <jamespetts@yahoo.com>
2017-01-12 21:21:20 +0000

    CODE: Use SO_REUSEADDR to bind server sockets (Dwachs, from Standard) FIX: MSVC compile error and const-ness changes (Dr. Supergood, from Standard)

--- network/network.cc
+++ network/network.cc
@@ -434,13 +434,19 @@ bool network_init_server( int port )
 			/* Disable IPv4-mapped IPv6 addresses for this IPv6 listen socket
 			   This ensures that we are using separate sockets for dual-stack, one for v4, one for v6 */
 			if (  walk->ai_family == AF_INET6  ) {
-				int on = 1;
+				int const on = 1;
 				if (  setsockopt(server_socket, IPPROTO_IPV6, IPV6_V6ONLY, (char*)&on, sizeof(on)) != 0  ) {
 					dbg->warning( "network_init_server()", "Call to setsockopt() failed for: \"%s\", error was: \"%s\"", ip.c_str(), strerror(GET_LAST_ERROR()) );
 					network_close_socket( server_socket );
 					server_socket = INVALID_SOCKET;
 					continue;
 				}
+			}
+
+			// Enable reusing of local addresses
+			int const enable = 1;
+			if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char *>(&enable), sizeof(enable)) != 0) {
+				dbg->warning("network_init_server()", "Call to setsockopt(SO_REUSEADDR) failed for: \"%s\", error was: \"%s\"", ip.c_str(), strerror(GET_LAST_ERROR()));
 			}
 
 			if (  bind( server_socket, walk->ai_addr, walk->ai_addrlen )  ) {
