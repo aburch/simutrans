715a7e7a8d0a3ce17fc44ad2899abe32d339eb47
James E. Petts <jamespetts@yahoo.com>
2017-01-13 22:49:03 +0000

    CODE: Fully update the networking code from the latest version of Standard, fixing a Windows specific compile error in that version.

--- network/network.h
+++ network/network.h
@@ -1,52 +1,37 @@
 #ifndef sim_network_h
 #define sim_network_h
 
+#if defined(_WIN32) && !defined(__CYGWIN__)
+#define USE_WINSOCK 1
+#else
+#define USE_WINSOCK 0
+#endif
+
 // windows headers
-#ifdef _WIN32
+#if USE_WINSOCK
 // must be include before all simutrans stuff!
 
-#define NOMINMAX 1
-
-// So that windows.h won't include all kinds of things
-#define WIN32_LEAN_AND_MEAN
-
-// first: we must find out version number
-#ifndef WINVER
-#	define _WINSOCKAPI_
-#	include <windows.h>
-#	undef _WINSOCKAPI_
-#endif
-
-// then we know which winsock version to use
-#if WINVER < 0x0500
-#	include <winsock.h>
-#	define USE_IP4_ONLY
-#	define socklen_t int
-#else
-#	include <windows.h>
 #	include <winsock2.h>
+#	include <windows.h>
 #	include <ws2tcpip.h>
-#endif
 #	ifndef IPV6_V6ONLY
 #		define IPV6_V6ONLY (27)
 #	endif
-
-#if WINVER<=0x0500
- // compiling for Windows 2000
-# define USE_IP4_ONLY
-# endif
-
+#	if WINVER<=0x0500
+// compiling for Windows 2000
+#		define USE_IP4_ONLY
+#	endif
 #	define GET_LAST_ERROR() WSAGetLastError()
 #	include <errno.h>
 #	undef  EWOULDBLOCK
 #	define EWOULDBLOCK WSAEWOULDBLOCK
 #else
-	// beos specific headers
+// beos specific headers
 #	ifdef  __BEOS__
 #		include <net/netdb.h>
 #		include <net/sockets.h>
 
-	// non-beos / non-windows
+// non-beos / non-windows
 #	else
 #		include <sys/types.h>
 #		include <sys/socket.h>
@@ -63,30 +48,9 @@
 // all non-windows
 #	include <fcntl.h>
 #	include <errno.h>
-	// to keep compatibility to MS windows
-	typedef int SOCKET;
+// to keep compatibility to MS windows
+typedef int SOCKET;
 #	define INVALID_SOCKET -1
-#	define GET_LAST_ERROR() (errno)
-#endif
-
-#if 0
-#ifdef WIN32
-// must be include before all simutrans stuff!
-#include <WinSock.h>
-#undef min
-#undef max
-#else
-#include <sys/types.h>
-#include <sys/socket.h>
-#ifdef __BEOS__
-#include <net/socket.h>
-#define PF_INET AF_INET
-#define socklen_t int
-#else
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#endif
-#endif
 #	define GET_LAST_ERROR() (errno)
 #endif
 
@@ -98,56 +62,56 @@ class network_command_t;
 class gameinfo_t;
 class karte_t;
 
-void network_close_socket( SOCKET sock );
+void network_close_socket(SOCKET sock);
 
-void network_set_socket_nodelay( SOCKET sock );
+void network_set_socket_nodelay(SOCKET sock);
 
 // open a socket or give a decent error message
 SOCKET network_open_address(char const* cp, char const*& err);
 
 // if sucessful, starts a server on this port
-bool network_init_server( int port );
+bool network_init_server(int port);
 
 /**
- * returns pointer to commmand or NULL
- */
+* returns pointer to commmand or NULL
+*/
 network_command_t* network_get_received_command();
 
 /**
- * do appropriate action for network games:
- * - server: accept connection to a new client
- * - all: receive commands and puts them to the received_command_queue
- *
- * @param timeout in milliseconds
- * @return pointer to first received commmand
- * more commands can be obtained by call to network_get_received_command
- */
+* do appropriate action for network games:
+* - server: accept connection to a new client
+* - all: receive commands and puts them to the received_command_queue
+*
+* @param timeout in milliseconds
+* @return pointer to first received commmand
+* more commands can be obtained by call to network_get_received_command
+*/
 network_command_t* network_check_activity(karte_t *welt, int timeout);
 
 /**
- * send data to dest:
- * if timeout_ms is positive:
- *    try to send all data, return true if all data are sent otherwise false
- * if timeout_ms is not positive:
- *    try to send as much as possible but return after one send attempt
- *    return true if connection is still open and sending can be continued later
- *
- * @param buf the data
- * @param count length of buffer and number of bytes to be sent
- * @param sent number of bytes sent
- * @param timeout_ms time-out in milli-seconds
- */
-bool network_send_data( SOCKET dest, const char *buf, const uint16 size, uint16 &count, const int timeout_ms );
+* send data to dest:
+* if timeout_ms is positive:
+*    try to send all data, return true if all data are sent otherwise false
+* if timeout_ms is not positive:
+*    try to send as much as possible but return after one send attempt
+*    return true if connection is still open and sending can be continued later
+*
+* @param buf the data
+* @param count length of buffer and number of bytes to be sent
+* @param sent number of bytes sent
+* @param timeout_ms time-out in milli-seconds
+*/
+bool network_send_data(SOCKET dest, const char *buf, const uint16 size, uint16 &count, const int timeout_ms);
 
 /**
- * receive data from sender
- * @param dest the destination buffer
- * @param len length of destination buffer and number of bytes to be received
- * @param received number of received bytes is returned here
- * @param timeout_ms time-out in milli-seconds
- * @return true if connection is still valid, false if an error occurs and connection needs to be closed
- */
-bool network_receive_data( SOCKET sender, void *dest, const uint16 len, uint16 &received, const int timeout_ms );
+* receive data from sender
+* @param dest the destination buffer
+* @param len length of destination buffer and number of bytes to be received
+* @param received number of received bytes is returned here
+* @param timeout_ms time-out in milli-seconds
+* @return true if connection is still valid, false if an error occurs and connection needs to be closed
+*/
+bool network_receive_data(SOCKET sender, void *dest, const uint16 len, uint16 &received, const int timeout_ms);
 
 void network_process_send_queues(int timeout);
 
@@ -156,11 +120,11 @@ bool network_check_server_connection();
 
 // send data to all clients (even us)
 // nwc is invalid after the call
-void network_send_all(network_command_t* nwc, bool exclude_us );
+void network_send_all(network_command_t* nwc, bool exclude_us);
 
 // send data to server only
 // nwc is invalid after the call
-void network_send_server(network_command_t* nwc );
+void network_send_server(network_command_t* nwc);
 
 void network_reset_server();
 
