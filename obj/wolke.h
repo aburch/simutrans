69db2b155521ed18eea1eb265d66a31b6ab6a89f
Dwachs <dwachs@gmx.net>
2017-02-17 20:41:21 +0000

    CODE change wolke_t::cloud_nr to uint8
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8095 8aca7d54-2c30-db11-9de9-000461428c89

--- obj/wolke.h
+++ obj/wolke.h
@@ -19,7 +19,7 @@ private:
 
 	uint16 insta_zeit;	// clouds vanish when insta_zeit>2500 => maximum 5 images ...
 	sint8 base_y_off;
-	sint8 cloud_nr;
+	uint8 cloud_nr;
 
 public:
 	static bool register_desc(const skin_desc_t *desc);
