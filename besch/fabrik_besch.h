f95c28a2fa7117fed646b6e86dd4fc8757aae0da
James E. Petts <jamespetts@yahoo.com>
2017-03-18 20:10:07 +0000

    FIX: Crash in some cases

--- besch/fabrik_besch.h
+++ besch/fabrik_besch.h
@@ -149,7 +149,7 @@ public:
 	goods_desc_t const* get_ware() const { return get_child<goods_desc_t>(0); }
 	int get_capacity() const { return capacity; } 
 	int get_supplier_count() const { return supplier_count; } 
-	int get_consumption() const { return consumption; }
+	int get_consumption() const { return this ? consumption : 1; }
 	void calc_checksum(checksum_t *chk) const;
 };
 
