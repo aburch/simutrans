0eca95c59fc45b82b9072922e081935350381827
James E. Petts <jamespetts@yahoo.com>
2017-03-18 19:34:30 +0000

    FIX: Prevent excessive generation of pedestrians in some cases

--- vehicle/simpeople.cc
+++ vehicle/simpeople.cc
@@ -176,8 +176,9 @@ void pedestrian_t::generate_pedestrians_at(const koord3d k, uint32 count, uint32
 			return;
 		}
 
+		count = min(count, 128);
 		
-		for (int i = 0; i < count; i++)
+		for (uint32 i = 0; i < count; i++)
 		{
 			pedestrian_t* ped = new pedestrian_t(gr, time_to_live);
 			ped->calc_height(gr);
