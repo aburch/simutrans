648459c020eb1a4b608aa8358fc3d45dcb34f75b
André Zanghelini <an_dz@invalid>
2017-04-02 20:08:00 +0000

    Add decoration waytype
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8172 8aca7d54-2c30-db11-9de9-000461428c89

--- descriptor/writer/get_waytype.cc
+++ descriptor/writer/get_waytype.cc
@@ -35,6 +35,8 @@ waytype_t get_waytype(const char* waytype)
 		uv8 = tram_wt;
 	} else if (!STRICMP(waytype, "power")) {
 		uv8 = powerline_wt;
+	} else if (!STRICMP(waytype, "decoration")) {
+		uv8 = any_wt;
 	} else {
 		dbg->fatal("get_waytype()","invalid waytype \"%s\"\n", waytype);
 		exit(1);
