aburch/simutrans

commit 8481018986181825b1224a978b71a8102197f47a

FIX crash with way building tools after loading, correctly cleanup tool before loading savegame
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8182 8aca7d54-2c30-db11-9de9-000461428c89

simworld.cc
@@ -5092,7 +5092,7 @@ void karte_t::load(loadsave_t *file)
 	intr_disable();
 	dbg->message("karte_t::laden()", "Prepare for loading" );
 	for(  uint i=0;  i<MAX_PLAYER_COUNT;  i++  ) {
-		selected_tool[i] = tool_t::general_tool[TOOL_QUERY];
+		set_tool(tool_t::general_tool[TOOL_QUERY], get_player(i));
 	}
 	destroy_all_win(true);
 

commit 59412d62f27c4096b85ae5d068081d617254af74

FIX: Crash with loading (due to scenario being NULL) introduced by r8182 (Dwachs, from Standard)

simworld.cc
@@ -8190,8 +8190,10 @@ void karte_t::load(loadsave_t *file)
 
 	intr_disable();
 	dbg->message("karte_t::load()", "Prepare for loading" );
-	for(  uint i=0;  i<MAX_PLAYER_COUNT;  i++  ) {
-		selected_tool[i] = tool_t::general_tool[TOOL_QUERY];
+	for (uint8 sp_nr = 0; sp_nr < MAX_PLAYER_COUNT; sp_nr++) {
+		if (two_click_tool_t* tool = dynamic_cast<two_click_tool_t*>(selected_tool[sp_nr])) {
+			tool->cleanup();
+		}
 	}
 	destroy_all_win(true);
 