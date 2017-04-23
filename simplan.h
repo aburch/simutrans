1601403fae322166d7e52c5c7b14a47a69b47743
Dwachs <dwachs@gmx.net>
2017-04-23 19:27:45 +0000

    remove unused method declaration
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8216 8aca7d54-2c30-db11-9de9-000461428c89

--- simplan.h
+++ simplan.h
@@ -241,8 +241,6 @@ public:
 
 	void display_obj(const sint16 xpos, const sint16 ypos, const sint16 raster_tile_width, const bool is_global, const sint8 hmin, const sint8 hmax  CLIP_NUM_DEF) const;
 
-	void display_tileoverlay(sint16 xpos, sint16 ypos, const sint8 hmin, const sint8 hmax) const;
-
 	void display_overlay(sint16 xpos, sint16 ypos) const;
 
 	static void toggle_horizontal_clip(CLIP_NUM_DEF0);
