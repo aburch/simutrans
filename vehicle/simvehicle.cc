54dbcfd2584c96269215751cc2e4e12262be8614
James E. Petts <jamespetts@yahoo.com>
2017-06-16 23:58:00 +0100

    VERSION: Increment the revision to 21 to accommodate the incrementing of the revision on the master branch to 20.

--- vehicle/simvehicle.cc
+++ vehicle/simvehicle.cc
@@ -2316,7 +2316,7 @@ void vehicle_t::rdwr_from_convoi(loadsave_t *file)
 
 	obj_t::rdwr(file);
 
-	if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 20)
+	if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 21)
 	{
 		// We need to save this in case "desc" cannot be found on re-loading because
 		// of some pakset changes.
@@ -2361,7 +2361,7 @@ void vehicle_t::rdwr_from_convoi(loadsave_t *file)
 		}
 	}
 
-	if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 20)
+	if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 21)
 	{
 		file->rdwr_bool(create_dummy_ware);
 	}
@@ -2414,7 +2414,7 @@ DBG_MESSAGE("vehicle_t::rdwr_from_convoi()","bought at %i/%i.",(purchase_time%12
 		file->rdwr_long(speed_limit);
 		file->rdwr_enum(direction);
 		file->rdwr_enum(previous_direction);
-		if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 20)
+		if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 21)
 		{
 			for (uint8 i = 0; i < number_of_classes; i++)
 			{
@@ -2523,7 +2523,7 @@ DBG_MESSAGE("vehicle_t::rdwr_from_convoi()","bought at %i/%i.",(purchase_time%12
 	}
 	else // Loading
 	{
-		if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 20)
+		if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 21)
 		{
 			for (uint8 i = 0; i < number_of_classes; i++)
 			{
@@ -2697,7 +2697,7 @@ DBG_MESSAGE("vehicle_t::rdwr_from_convoi()","bought at %i/%i.",(purchase_time%12
 		current_livery = "default";
 	}
 
-	if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 20)
+	if (file->get_extended_version() >= 13 || file->get_extended_revision() >= 21)
 	{
 		for (uint8 i = 0; i < number_of_classes; i++)
 		{
