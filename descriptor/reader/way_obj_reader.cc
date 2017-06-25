9f8985d9c372de4efa63a16fbee2fd93fc5ca8fe
teamhimeh <thleaderh@gmail.com>
2017-06-25 18:05:03 +0900

    road_wtすなはちovertake_wtはおかしい

--- descriptor/reader/way_obj_reader.cc
+++ descriptor/reader/way_obj_reader.cc
@@ -67,9 +67,6 @@ obj_desc_t * way_obj_reader_t::read_node(FILE *fp, obj_node_info_t &node)
 	else {
 		dbg->fatal("way_obj_reader_t::read_node()","Invalid version %d", version);
 	}
-	if ((waytype_t)desc->wtyp == road_wt && (waytype_t)desc->own_wtyp == road_wt) {
-		desc->wtyp = overtake_wt;
-	}
 	DBG_DEBUG("way_obj_reader_t::read_node()",
 	     "version=%d price=%d maintenance=%d topspeed=%d wtype=%d own_wtype=%d overtaking_info=%d intro=%i/%i, retire=%i/%i",
 	     version,
