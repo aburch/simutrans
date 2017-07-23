d245d12803de80a3620d6a920a1a9d86f7605746
James E. Petts <jamespetts@yahoo.com>
2017-07-23 16:07:17 +0100

    CODE: Remove deprecated GUI code

--- gui/halt_info.cc
+++ gui/halt_info.cc
@@ -363,12 +363,6 @@ void halt_info_t::draw(scr_coord pos, scr_size size)
 		// information about the passengers happiness
 		info_buf.clear();
 		halt->info(info_buf);
-#ifdef DEBUG
-		// This system currently only gives the number of
-		// packets, not the actual number of passengers (etc.). 
-		// Useful only for debugging therefore.
-		//info_buf.printf(transferring);
-#endif
 		display_multiline_text(pos.x + D_MARGIN_LEFT, top, info_buf, SYSCOL_TEXT);
 	}
 }
