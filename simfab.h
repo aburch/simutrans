e825c5d97a1e6f81a120671ab92b271e45a6f8dc
James E. Petts <jamespetts@yahoo.com>
2017-07-30 11:48:46 +0100

    FIX: Loss of precision in industry production figures

--- simfab.h
+++ simfab.h
@@ -692,8 +692,8 @@ public:
 	sint32 get_base_production() const { return prodbase; }
 	void set_base_production(sint32 p);
 
-	// TODO: Consider refctoring so as to avoid calling this method every step (although preliminary tests indicate that this does not seem to cause much slow-down compared to the previous method).
-	sint32 get_current_production() const { return (sint32)welt->calc_adjusted_monthly_figure(((sint64)prodbase * (sint64)get_prodfactor()) >> 8ll); }
+	// This is done this way rather than reusing get_prodfactor() because the latter causes a lack of precision (everything being rounded to the nearest 16). 
+	sint32 get_current_production() const { return (sint32)(welt->calc_adjusted_monthly_figure(((sint64)prodbase * (sint64)(DEFAULT_PRODUCTION_FACTOR + prodfactor_electric + prodfactor_pax + prodfactor_mail)))) >> 8l; }
 
 	/* prissi: returns the status of the current factory, as well as output */
 	enum { bad, medium, good, inactive, nothing };
