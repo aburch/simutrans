7483d887fac635df863cd0d511845dff0e6533ef
Dwachs <dwachs@gmx.net>
2017-08-11 15:23:47 +0000

    FIX unitialized variable
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8287 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/components/gui_combobox.cc
+++ gui/components/gui_combobox.cc
@@ -46,6 +46,7 @@ gui_combobox_t::gui_combobox_t() :
 	closed_size = get_size();
 	max_size = scr_size(0,10*LINESPACE);
 	set_highlight_color(color_idx_to_rgb(COL_SOFT_BLUE));
+	opened_at_bottom = false;
 }
 
 
