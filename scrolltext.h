d420f0fbc555273722c6ba46f03f019e71b29f34
James E. Petts <jamespetts@yahoo.com>
2017-08-20 11:51:36 +0100

    CODE: Update the credits

--- scrolltext.h
+++ scrolltext.h
@@ -261,6 +261,7 @@
 " Rémi Denis-Courmont","French",
 " Ferencz Szekely","Hungary & Romanian",
 " Alex Revo","Russian",
+" Grivlad", "Russian (Extended)",
 " Roman Schwörer","French & German",
 " Yannis Kaskamanid", "Greek",
 " Igor Eliezer", "Portuguese",
