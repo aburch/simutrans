41009178831a841512aecc191640af139b313d97
VictorErik <violinvictor@hotmail.com>
2017-09-19 23:21:52 +0200

    Small fixes

--- simhalt.cc
+++ simhalt.cc
@@ -3339,16 +3339,16 @@ void haltestelle_t::get_freight_info(cbuffer_t & buf)
 						}
 					}
 				}
-				//else
+				else
 				{
 					freight_list_sorter_t::sort_freight(*warray, buf, (freight_list_sorter_t::sort_mode_t)sortierung, NULL, "waiting");
 				}
 			}
 		}
 
-		buf.append("\n");
 		if (get_transferring_cargoes_count() > 0)
 		{
+			buf.append("\n");
 			buf.printf("%s:\n", translator::translate("transfers"));
 		}
 		vector_tpl<ware_t> ware_transfers;
