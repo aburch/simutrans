e59d70e97a9045e73dc1c8410e5e2ef5f842c02a
Phystam <phystam.tc@gmail.com>
2017-10-09 12:33:32 +0900

    border=15

--- bauer/wegbauer.cc
+++ bauer/wegbauer.cc
@@ -1822,11 +1822,12 @@ bool way_builder_t::intern_calc_route_runways(koord3d start3d, const koord3d zie
 	}
 	const ribi_t::ribi ribi_straight = ribi_t::doubles(ribi);
 
-	// not too close to the border? 
+	// not too close to the border?
+	const uint16 border=15;
 	//	if(	 !(welt->is_within_limits(start-koord(5,5))  &&  welt->is_within_limits(start+koord(5,5)))  ||
 	//	 !(welt->is_within_limits(ziel-koord(5,5))  &&  welt->is_within_limits(ziel+koord(5,5)))  ) {
-	if(	 !(welt->is_within_limits(start-koord(15,15))  &&  welt->is_within_limits(start+koord(15,15)))  ||
-		 !(welt->is_within_limits(ziel-koord(15,15))  &&  welt->is_within_limits(ziel+koord(15,15)))  ) {
+	if(	 !(welt->is_within_limits(start-koord(border,border))  &&  welt->is_within_limits(start+koord(border,border)))  ||
+		 !(welt->is_within_limits(ziel-koord(border,border))  &&  welt->is_within_limits(ziel+koord(border,border)))  ) {
 		if(player==welt->get_active_player()) {
 			create_win( new news_img("Zu nah am Kartenrand"), w_time_delete, magic_none);
 			return false;
