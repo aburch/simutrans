e2fb6298dbe9b73afdbf2bc1445aeda4018fd3da
James E. Petts <jamespetts@yahoo.com>
2017-10-11 23:59:43 +0100

    CHANGE: De-randomise the position of the factory smoke (Vladki)

--- simfab.cc
+++ simfab.cc
@@ -1630,8 +1630,8 @@ void fabrik_t::smoke() const
 		koord ro = rada->get_pos_off(size,rot);
 		grund_t *gr = welt->lookup_kartenboden(pos_origin.get_2d()+ro);
 		// to get same random order on different compilers
-		const sint8 offsetx =  ((rada->get_xy_off(rot).x+sim_async_rand(7)-3)*OBJECT_OFFSET_STEPS)/16;
-		const sint8 offsety =  ((rada->get_xy_off(rot).y+sim_async_rand(7)-3)*OBJECT_OFFSET_STEPS)/16;
+		const sint8 offsetx = ((rada->get_xy_off(rot).x)*OBJECT_OFFSET_STEPS) / 16;
+		const sint8 offsety = ((rada->get_xy_off(rot).y)*OBJECT_OFFSET_STEPS) / 16;
 		wolke_t *smoke =  new wolke_t(gr->get_pos(), offsetx, offsety, rada->get_images() );
 		gr->obj_add(smoke);
 		welt->sync_way_eyecandy.add( smoke );
