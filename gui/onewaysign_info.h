cd5640a4ab27ac85a523d7d6fd85a94b2258741a
akimuhirai <akimuhirai@gmail.com>
2017-12-05 22:15:35 +0900

    till brueckenbauer

---
+++ gui/onewaysign_info.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 1997 - 2003 Hansjörg Malthaner
+ *
+ * This file is part of the Simutrans project under the artistic licence.
+ * (see licence.txt)
+ */
+
+#ifndef onewaysign_info_t_h
+#define onewaysign_info_t_h
+
+#include "../simconst.h"
+#include "obj_info.h"
+#include "components/action_listener.h"
+#include "components/gui_button.h"
+#include "components/gui_container.h"
+
+class roadsign_t;
+
+/**
+ * Info window for factories
+ * @author Hj. Malthaner
+ */
+class onewaysign_info_t : public obj_infowin_t, public action_listener_t
+{
+	private:
+		roadsign_t* sign;
+		button_t direction[2];
+
+	public:
+		onewaysign_info_t(roadsign_t* s, koord3d first_intersection);
+
+		/**
+		 * Set the window associated helptext
+		 * @return the filename for the helptext, or NULL
+		 * @author Hj. Malthaner
+		 */
+		const char *get_help_filename() const {return "onewaysign_info.txt";}
+
+		bool action_triggered(gui_action_creator_t*, value_t) OVERRIDE;
+
+		// called, after external change
+		void update_data();
+};
+
+#endif
--- simtypes.h
+++ simtypes.h
@@ -122,6 +122,19 @@ enum systemtype_t {
 	type_all = 255,	///< special ?
 };
 
+/**
+ * conditions for overtaking on roads
+ * @author teamhimeH
+ */
+ enum overtaking_mode_t {
+	 invalid_mode      = -1,
+	 oneway_mode       = 0,  // condition for one-way road
+	 twoway_mode       = 1,  // condition for two-way road
+	 loading_only_mode = 2,  // overtake a loading convoy only
+	 prohibited_mode   = 3,  // overtaking is completely forbidden
+	 inverted_mode = 4       // vehicles can go only on passing lane
+ };
+
 enum working_method_t { drive_by_sight, time_interval, absolute_block, token_block, track_circuit_block, cab_signalling, moving_block, one_train_staff, time_interval_with_telegraph };
 
 // makros are not very safe: thus use these macro like functions
@@ -140,7 +153,7 @@ typedef   signed int        sint32;
 #define SINT32_MAX_VALUE	INT_MAX
 #ifndef NO_UINT32_TYPES
 typedef unsigned int        uint32;
-#define UINT32_MAX_VALUE	UINT_MAX	
+#define UINT32_MAX_VALUE	UINT_MAX
 #define SINT64_MAX_VALUE	LLONG_MAX
 #endif
 #else
@@ -149,7 +162,7 @@ typedef   signed long       sint32;
 #define SINT32_MAX_VALUE	LONG_MAX
 #ifndef NO_UINT32_TYPES
 typedef unsigned long       uint32;
-#define UINT32_MAX_VALUE	ULONG_MAX	
+#define UINT32_MAX_VALUE	ULONG_MAX
 #define SINT64_MAX_VALUE	LLONG_MAX
 #endif
 #endif
@@ -171,7 +184,7 @@ typedef unsigned long long  uint64;
 #ifndef  MULTI_THREAD
 #if defined _MSC_VER
 #include <xkeycheck.h>
-#define thread_local  
+#define thread_local
 #endif
 #endif // ! MULTI_THREAD
 
@@ -233,15 +246,15 @@ public:
 	{
 		if((reduce_at % 2) != 0)
 		{
-			// This *must* be an even number, or else the 
+			// This *must* be an even number, or else the
 			// average will drift too high as "count"
-			// is truncated at each reduction. 
+			// is truncated at each reduction.
 			reduce_at++;
 		}
 
 		sint64 new_total = (sint64)total + (sint64)value;
 		count++;
-		
+
 		if(count >= reduce_at)
 		{
 			new_total /= 2;
@@ -273,7 +286,7 @@ public:
 	{
 		total = 0;
 		count = 0;
-	}		
+	}
 
 	/**
 	* This compresses all values to give new numbers added
@@ -285,7 +298,7 @@ public:
 	{
 		total = get_average();
 		count = 1;
-		return total; 
+		return total;
 	}
 };
 
