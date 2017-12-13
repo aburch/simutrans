77865b95a213415a91d7895724fcd6b5020b19d8
James E. Petts <jamespetts@yahoo.com>
2017-12-13 00:57:58 +0000

    FIX: Crash when deleting tiles of a stop in some cases

--- simplan.cc
+++ simplan.cc
@@ -749,7 +749,7 @@ void planquadrat_t::add_to_haltlist(halthandle_t halt)
 			
 			for(unsigned insert_pos = 0; insert_pos < halt_list_count; insert_pos++)
 			{			
-				if(koord_distance(halt_list[insert_pos].halt->get_next_pos(pos), pos) > distance) 
+				if(halt_list[insert_pos].halt.is_bound() && koord_distance(halt_list[insert_pos].halt->get_next_pos(pos), pos) > distance)
 				{
 					halt_list_insert_at(halt, insert_pos, distance);
 					return;
