114d4b8574d191328b61eba11d93c687fbaa4ec2
Dwachs <dwachs@gmx.net>
2018-01-21 10:12:33 +0000

    FIX clipping problems with high way images, e.g., pak192.comic city walls
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8373 8aca7d54-2c30-db11-9de9-000461428c89

--- display/simgraph16.cc
+++ display/simgraph16.cc
@@ -114,7 +114,7 @@ public:
 				inc = 1 << 16;
 			}
 			else {
-				inc = (dx << 16) / dy;
+				inc = (dx << 16) / dy -  (1 << 16);
 			}
 		}
 		else if(  dy < 0  ) {
@@ -122,7 +122,7 @@ public:
 				inc = 0; // (+1)-1 << 16;
 			}
 			else {
-				inc = (1 << 16) - (dx << 16) / dy;
+				inc = 0;
 			}
 		}
 	}
@@ -140,8 +140,9 @@ public:
 		r.non_convex_active = false;
 		if(  non_convex  &&  use_non_convex  &&  y < y0  &&  y < (y0 + dy)  ) {
 			r.non_convex_active = true;
+			y = min(y0, y0+dy) - 1;
 		}
-		else if(  dy != 0  ) {
+		if(  dy != 0  ) {
 			// init Bresenham algorithm
 			const int t = ((y - y0) << 16) / sdy;
 			// sx >> 16 = x
@@ -158,26 +159,6 @@ public:
 		if(  r.non_convex_active  ) {
 			if(  r.y == min( y0, y0 + dy )  ) {
 				r.non_convex_active = false;
-				if(  dy != 0  ) {
-					// init Bresenham algorithm
-					const int t = ((r.y - y0) << 16) / sdy;
-					// sx >> 16 = x
-					// sy >> 16 = y
-					r.sx = t * sdx + inc + (x0 << 16);
-					r.sy = t * sdy + (y0 << 16);
-					if(  dy > 0  ) {
-						const int r_xmin = r.sx >> 16;
-						if(  xmin < r_xmin  ) {
-							xmin = r_xmin;
-						}
-					}
-					else {
-						const int r_xmax = r.sx >> 16;
-						if(  xmax > r_xmax  ) {
-							xmax = r_xmax;
-						}
-					}
-				}
 			}
 			else {
 				if(  dy < 0  ) {
@@ -192,10 +173,11 @@ public:
 						xmin = r_xmin;
 					}
 				}
+				return;
 			}
 		}
 		// go along the ray, Bresenham
-		else if(  dy != 0  ) {
+		if(  dy != 0  ) {
 			if(  dy > 0  ) {
 				do {
 					r.sx += sdx;
