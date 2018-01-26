52c37b9a34e0e4b643af02855d04496ec57383b0
James E. Petts <jamespetts@yahoo.com>
2018-01-26 23:53:49 +0000

    ADD: Setting in simuconf.tab (rural_industries_no_staff_shortage), which is enabled by default, which, when enabled, allows rural industries (i.e., those not situated in towns) to continue to produce/consume even if they are short of staff. NOTE: The saved game version is incremented with this change. VERSION: Increment the minor version number

--- gui/settings_stats.cc
+++ gui/settings_stats.cc
@@ -503,6 +503,7 @@ void settings_extended_revenue_stats_t::init( settings_t *sets )
 	}
 	SEPERATOR;
 	INIT_NUM("max_comfort_preference_percentage", sets->get_max_comfort_preference_percentage(), 100, 65535, gui_numberinput_t::AUTOLINEAR, false);
+	INIT_BOOL("rural_industries_no_staff_shortage", sets->rural_industries_no_staff_shortage); 
 	clear_dirty();
 	height = ypos;
 	set_size(settings_stats_t::get_size());
@@ -564,6 +565,7 @@ void settings_extended_revenue_stats_t::read(settings_t *sets)
 	READ_NUM_VALUE( sets->catering_level5_max_revenue );
 
 	READ_NUM_VALUE(sets->max_comfort_preference_percentage);
+	READ_BOOL_VALUE(sets->rural_industries_no_staff_shortage); 
 
 	// And convert to the form used in-game...
 	sets->cache_catering_revenues();
