4d194d3fe230689fd3146a0291d4d8a3d6af0f98
Markus Pristovsek <prissi@physik.tu-berlin.de>
2018-06-12 13:50:19 +0000

    Further update paksets for installer
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8496 8aca7d54-2c30-db11-9de9-000461428c89

--- display/simgraph16.cc
+++ display/simgraph16.cc
@@ -4342,7 +4342,7 @@ uint16 display_load_font(const char* fname)
 		return 0;
 	}
 	// skip reloading if already in memory, if bdf font
-	if(  strcmp( large_font.fname, fname ) == 0  &&  strstr(fname,".bdf")  ) {
+	if(  large_font.num_chars>0  &&  strcmp( large_font.fname, fname ) == 0  ) {
 		return large_font.num_chars;
 	}
 
