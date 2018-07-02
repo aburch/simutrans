42a6717910cc6825cc1446a4357d28285145dd32
Markus Pristovsek <prissi@physik.tu-berlin.de>
2018-07-02 14:32:03 +0000

    ADD: new simuconf.tab string server_altdns, to maunallzy set a sceond name/address (espec. IP6) for servers
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8532 8aca7d54-2c30-db11-9de9-000461428c89

--- simmain.cc
+++ simmain.cc
@@ -1216,6 +1216,11 @@ DBG_MESSAGE("simmain","loadgame file found at %s",buffer);
 		DBG_DEBUG( "simmain()", "Server IP set to '%s'.", ref_str );
 	}
 
+	if(  const char *ref_str = gimme_arg(argc, argv, "-server_altdns", 1)  ) {
+		env_t::server_alt_dns = ref_str;
+		DBG_DEBUG( "simmain()", "Server IP set to '%s'.", ref_str );
+	}
+
 	if(  const char *ref_str = gimme_arg(argc, argv, "-server_name", 1)  ) {
 		env_t::server_name = ref_str;
 		DBG_DEBUG( "simmain()", "Server name set to '%s'.", ref_str );
@@ -1223,6 +1228,12 @@ DBG_MESSAGE("simmain","loadgame file found at %s",buffer);
 
 	if(  const char *ref_str = gimme_arg(argc, argv, "-server_admin_pw", 1)  ) {
 		env_t::server_admin_pw = ref_str;
+	}
+
+	if(  env_t::server_dns.empty()  &&  !env_t::server_alt_dns.empty()  ) {
+		dbg->warning( "simmain", "server_altdns but not server_dns set. Please use server_dns first!" );
+		env_t::server_dns = env_t::server_alt_dns;
+		env_t::server_alt_dns.clear();
 	}
 
 	dr_chdir(env_t::user_dir);
