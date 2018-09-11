b59756aa2ca82980656efb46c54cdb12315d14fb
Markus Pristovsek <prissi@physik.tu-berlin.de>
2018-09-11 01:26:17 +0000

    ADD: (THLeaderH) configurable height setting for extremely heigh maps (use at own risk)
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8574 8aca7d54-2c30-db11-9de9-000461428c89

--- simworld.cc
+++ simworld.cc
@@ -1250,6 +1250,8 @@ void karte_t::init(settings_t* const sets, sint8 const* const h_field)
 	recalc_average_speed();	// resets timeline
 	koord::locality_factor = settings.get_locality_factor( last_year );
 
+	world_maximum_height = sets->get_maximumheight();
+	world_minimum_height = sets->get_minimumheight();
 	groundwater = (sint8)sets->get_groundwater();      //29-Nov-01     Markus Weber    Changed
 
 	init_height_to_climate();
@@ -5226,6 +5228,9 @@ void karte_t::load(loadsave_t *file)
 	else {
 		goods_manager_t::set_multiplier( 1000 );
 	}
+
+	world_maximum_height = settings.get_maximumheight();
+	world_minimum_height = settings.get_minimumheight();
 
 	groundwater = (sint8)(settings.get_groundwater());
 	min_height = max_height = groundwater;
