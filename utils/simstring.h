b740982ce388a012b92c7de7dc9e904def41712d
Markus Pristovsek <prissi@physik.tu-berlin.de>
2019-02-13 14:08:46 +0000

    ADD (shingoushori) show coordinates in tooltip for stopped convois due to singals etc.
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8685 8aca7d54-2c30-db11-9de9-000461428c89

--- utils/simstring.h
+++ utils/simstring.h
@@ -18,6 +18,9 @@
 #endif
 #endif
 
+#if defined(_MSC_VER) && _MSC_VER < 1900
+#define snprintf(buf,len, format,...) _snprintf_s(buf, len,len, format, __VA_ARGS__)
+#endif
 
 /**
  * Set thousand separator, used in money_to_string and
