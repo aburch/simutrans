342753435a2e196104e5049a2cef7b36cc251eb4
James E. Petts <jamespetts@yahoo.com>
2019-02-24 13:00:18 +0000

    CODE: Fix errors in the description of some debugging text.

--- simconvoi.cc
+++ simconvoi.cc
@@ -1074,7 +1074,7 @@ void convoi_t::calc_acceleration(uint32 delta_t)
 				break;
 		}
 #ifdef DEBUG_ACCELERATION
-		static const char *debug_fmt1 = "%d) at tile% 4u next limit of% 4d km/h, current speed% 4d km/h,% 6d steps til brake,% 6d steps til stop";
+		static const char *debug_fmt1 = "at route index: %d; next stop index: % 4u next limit of% 4d km/h, current speed% 4d km/h,% 6d steps til brake,% 6d steps til stop";
 		dbg->warning("convoi_t::calc_acceleration 1", debug_fmt1, current_route_index - 1, next_stop_index, speed_to_kmh(next_speed_limit), speed_to_kmh(akt_speed), steps_til_brake, steps_til_limit);
 #endif
 		// Brake for upcoming speed limit?
