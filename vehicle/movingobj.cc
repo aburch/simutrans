50992230a84404f831397b56b2dec65aca9323de
Andrew Carlotti <andrew@carlotti.me.uk>
2019-04-16 18:16:05 +0100

    Minor corrections to changes from Standard
    Drawn from:
    CODE: hop_check returns pointer to next tile on success, save calls to karte_t::lookup
    
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7479 8aca7d54-2c30-db11-9de9-000461428c89

--- vehicle/movingobj.cc
+++ vehicle/movingobj.cc
@@ -361,7 +361,6 @@ grund_t* movingobj_t::hop_check()
 	 * Else pos_next_next is a single step from pos_next.
 	 * otherwise objects would jump left/right on some diagonals
 	 */
-	koord k(direction);
 	if (timetochange != 0) {
 		koord k(direction);
 		if(k.x&k.y) {
