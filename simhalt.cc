a85b7024840e7dfaba3a3cc94d63857d60d20c20
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2019-04-19 18:03:13 +0900

    FIX: Station mail records are not written in save data

--- simhalt.cc
+++ simhalt.cc
@@ -3970,17 +3970,32 @@ void haltestelle_t::rdwr(loadsave_t *file)
 		}
 	}
 
-	if (file->get_extended_version() >= 5)
+	if ((file->get_extended_version() == 14 && file->get_extended_revision() > 8) || file->get_extended_version() > 15)
 	{
-		for (int j = 0; j < 9 /*MAX_HALT_COST*/; j++)
+		for (int j = 0; j < 11  /*MAX_HALT_COST*/; j++)
 		{
-			if (((file->get_extended_version() == 14 && file->get_extended_revision() < 5) || file->get_extended_version() < 14) && j==8)
-			{
-				break;
-			}
 			for (int k = MAX_MONTHS - 1; k >= 0; k--)
 			{
 				file->rdwr_longlong(financial_history[k][j]);
+			}
+		}
+	}
+	else if (file->get_extended_version() >= 5)
+	{
+		for (int j = 0; j < 8 /*MAX_HALT_COST*/; j++)
+		{
+			for (int k = MAX_MONTHS - 1; k >= 0; k--)
+			{
+				file->rdwr_longlong(financial_history[k][j]);
+			}
+		}
+		for (int k = MAX_MONTHS - 1; k >= 0; k--)
+		{
+			if (file->is_loading())
+			{
+				financial_history[k][HALT_TOO_WAITING] = 0;
+				financial_history[k][HALT_MAIL_DELIVERED] = 0;
+				financial_history[k][HALT_MAIL_NOROUTE] = 0;
 			}
 		}
 	}
