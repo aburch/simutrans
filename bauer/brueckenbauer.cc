9a18ee2fcc40e63bdfe568674e1b554728761822
Dwachs <dwachs@gmx.net>
2019-04-28 08:05:32 +0000

    FIX: AI bridge building needs to check starting tile for correct ribis
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8751 8aca7d54-2c30-db11-9de9-000461428c89

--- bauer/brueckenbauer.cc
+++ bauer/brueckenbauer.cc
@@ -214,8 +214,8 @@ const char *check_tile( const grund_t *gr, const player_t *player, waytype_t wt,
 			return "A bridge must start on a way!";
 		}
 
-		// same waytype, any direction, no stop or depot or any other stuff */
-		if(  w->get_waytype() == wt  ) {
+		// same waytype, check direction
+		if(  w->get_waytype() == wt   &&  ribi_check(ribi, check_ribi ) ) {
 			// ok too
 			return NULL;
 		}
