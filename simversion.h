2f3b5818eabafd43dea12ebfd26e7a388b53888b
teamhimeh <thleaderh@gmail.com>
2019-08-10 20:02:50 +0900

    increment OTRP_VERSION_MINOR

--- simversion.h
+++ simversion.h
@@ -22,7 +22,7 @@
 // NOTE: increment before next release to enable save/load of new features
 
 #define OTRP_VERSION_MAJOR 22
-#define OTRP_VERSION_MINOR 2
+#define OTRP_VERSION_MINOR 3
 // NOTE: increment OTRP_VERSION_MAJOR when the save data structure changes.
 
 #define MAKEOBJ_VERSION "60.2"
