18e1207ed4d0d7b67cff98dcff6a192cbcf9ae82
Markus Pristovsek <prissi@physik.tu-berlin.de>
2019-10-02 02:55:05 +0000

    FIX: IP4 query for easyserver
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@8830 8aca7d54-2c30-db11-9de9-000461428c89

--- network/network.cc
+++ network/network.cc
@@ -833,7 +833,7 @@ bool get_external_IP( cbuffer_t &myIPaddr, cbuffer_t &altIPaddr )
 	// query "simutrans-forum.de/get_IP.php" for IP (faster than asking router and we can get IP6 too)
 	const char *err = network_http_get( QUERY_ADDR_IP, QUERY_ADDR_URL, altIPaddr );
 	// if we have a dual stack system, IP6 should be preferred, i.e. we have now the IP6
-	if(  err==NULL  &&  strstr(myIPaddr,":")  ) {
+	if(  err==NULL  &&  strstr(altIPaddr,":")  ) {
 		// try to get and IPv4 address too
 		if(  !network_http_get( QUERY_ADDR_IPv4_ONLY, QUERY_ADDR_URL, myIPaddr )  ) {
 			if(  strcmp( myIPaddr, altIPaddr ) == 0   ) {
@@ -841,6 +841,9 @@ bool get_external_IP( cbuffer_t &myIPaddr, cbuffer_t &altIPaddr )
 				altIPaddr.clear();
 			}
 		}
+	}
+	else {
+		myIPaddr = altIPaddr;
 	}
 
 #if 0
