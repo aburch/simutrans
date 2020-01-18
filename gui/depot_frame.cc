77c23eb2093e57a7df340c52d98ff29ff0c218f7
jamespetts <jamespetts@yahoo.com>
2020-01-18 13:02:20 +0000

    FIX: Loss of synchronisation with server when attempting to copy a convoy whose schedule window is open

--- gui/depot_frame.cc
+++ gui/depot_frame.cc
@@ -602,12 +602,20 @@ bool depot_frame_t::action_triggered( gui_action_creator_t *comp, value_t p)
 		else if(  comp == &bt_sell  ) {
 			depot->call_depot_tool('v', cnv, NULL);
 		}
-		else if(  comp == &bt_copy_convoi  ) {
-			if(  cnv.is_bound() && cnv->all_vehicles_are_buildable() ) {
-				if(  !welt->use_timeline()  ||  welt->get_settings().get_allow_buying_obsolete_vehicles()  ||  depot->check_obsolete_inventory( cnv )  ) {
+		else if(  comp == &bt_copy_convoi  )
+		{
+			if(  cnv.is_bound() && cnv->all_vehicles_are_buildable()) 
+			{
+				if (cnv->get_schedule() && (!cnv->get_schedule()->is_editing_finished()))
+				{
+					create_win(new news_img("Schedule is incomplete/not finished"), w_time_delete, magic_none);
+				}
+				else if(  !welt->use_timeline()  ||  welt->get_settings().get_allow_buying_obsolete_vehicles()  ||  depot->check_obsolete_inventory( cnv )  )
+				{
 					depot->call_depot_tool('c', cnv, NULL, gui_convoy_assembler_t::get_livery_scheme_index());
 				}
-				else {
+				else 
+				{
 					create_win( new news_img("Can't buy obsolete vehicles!"), w_time_delete, magic_none );
 				}
 			}
