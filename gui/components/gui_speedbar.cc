d8be847ec7c7d035550f6532876a17043d176878
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-01-22 22:08:10 +0900

    ADD: (GUI) tile occupancy bar for convoy

--- gui/components/gui_speedbar.cc
+++ gui/components/gui_speedbar.cc
@@ -11,6 +11,7 @@
 #include "../../display/simgraph.h"
 #include "../../simcolor.h"
 #include "../../simtypes.h"
+#include "../../simunits.h"
 
 
 void gui_speedbar_t::set_base(sint32 base)
@@ -57,3 +58,85 @@ void gui_speedbar_t::draw(scr_coord offset)
 		}
 	}
 }
+
+
+#define COL_ADDITIONAL COL_LIGHT_TURQUOISE
+#define COL_REDUCED    COL_LIGHT_ORANGE
+
+#define LENGTH_TO_PIXEL 1      // One tile is represented by 16 times longer (1tile = 16length) pixels
+#define OCCUPANCY_BAR_HEIGHT 5 // bar height. Do not exceed set_size.h value
+
+void gui_tile_occupancybar_t::set_base_convoy_length(uint32 convoy_length, uint8 last_veh_length)
+{
+	this->convoy_length = convoy_length;
+	this->last_veh_length = last_veh_length;
+}
+
+void gui_tile_occupancybar_t::set_new_veh_length(sint8 new_veh_length, bool insert, uint8 new_last_veh_length)
+{
+	this->new_veh_length = new_veh_length;
+	this->insert_mode = insert;
+	if (new_last_veh_length != -1) {
+		this->switched_last_veh_length = new_last_veh_length;
+	}
+}
+
+inline uint32 gui_tile_occupancybar_t::adjust_convoy_length(uint32 total_len, uint8 last_veh_len)
+{
+	if (!total_len) {
+		return 0;
+	}
+	return total_len + (max(CARUNITS_PER_TILE / 2, last_veh_len) - last_veh_len);
+}
+
+inline void gui_tile_occupancybar_t::fill_with_color(scr_coord offset, uint8 tile_index, uint8 from, uint8 to, COLOR_VAL color)
+{
+	display_fillbox_wh_clip(offset.x + (CARUNITS_PER_TILE * LENGTH_TO_PIXEL + 4)*tile_index + 1 + from * LENGTH_TO_PIXEL, offset.y + 1, (to - from) * LENGTH_TO_PIXEL, OCCUPANCY_BAR_HEIGHT - 2, color, true);
+}
+
+
+void gui_tile_occupancybar_t::draw(scr_coord offset)
+{
+	offset += pos;
+	const uint8 tilebar_width = CARUNITS_PER_TILE * LENGTH_TO_PIXEL;
+
+	// calculate internal convoy length (include margin)
+	const uint32 current_length = adjust_convoy_length(convoy_length, last_veh_length);
+	uint32 new_length = 0;
+
+	if (new_veh_length != 0) {
+		if (insert_mode) {
+			new_length = adjust_convoy_length(convoy_length + new_veh_length, last_veh_length);
+		}
+		else if (new_veh_length < 0) {
+			new_length = adjust_convoy_length(convoy_length + new_veh_length, switched_last_veh_length);
+		}
+		else {
+			new_length = adjust_convoy_length(convoy_length + new_veh_length, new_veh_length);
+		}
+	}
+
+	const sint8 len_diff = new_veh_length == 0 ? 0 : new_length - current_length;
+	COLOR_VAL col = len_diff < 0 ? COL_REDUCED : COL_ADDITIONAL;
+	uint8 last_tile_occupancy = max(current_length, new_length) % CARUNITS_PER_TILE ? max(current_length, new_length) % CARUNITS_PER_TILE : CARUNITS_PER_TILE;
+	const uint8 tiles = (max(current_length, new_length) + CARUNITS_PER_TILE - 1) / CARUNITS_PER_TILE;
+
+	for (int i = 0; i < tiles; i++)
+	{
+		// draw frame and base color
+		display_ddd_box_clip(offset.x + (tilebar_width + 4) * i, offset.y, tilebar_width + 2, OCCUPANCY_BAR_HEIGHT, 8, 8);
+		fill_with_color(offset, i, 0, i == tiles - 1 ? last_tile_occupancy : CARUNITS_PER_TILE, COL_GREY4);
+	}
+
+	if (insert_mode && len_diff > 0) {
+		// insert mode, paint the front tile
+		fill_with_color(offset, 0, 0, len_diff, col);
+	}
+	else if (len_diff != 0) {
+		fill_with_color(offset, tiles - 1, last_tile_occupancy - min(abs(len_diff), last_tile_occupancy), last_tile_occupancy, col);
+		if (abs(len_diff) - last_tile_occupancy > 0 && tiles > 1) {
+			fill_with_color(offset, tiles - 2, CARUNITS_PER_TILE - abs(len_diff) + last_tile_occupancy, CARUNITS_PER_TILE, col);
+		}
+	}
+}
+
