6b8859088171bf7d67ca68d43c749ad7dc223c09
Phystam <phystam.tc@gmail.com>
2020-01-29 18:18:58 +0900

    FIX crash when opening keyboard help window (Dwachs, from standard)
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@7579 8aca7d54-2c30-db11-9de9-000461428c89

--- simtool.cc
+++ simtool.cc
@@ -2422,10 +2422,11 @@ const way_desc_t *tool_build_way_t::get_desc( uint16 timeline_year_month, bool r
 		desc = defaults[wt & (sint8)63];
 		if(desc == NULL || !desc->is_available(timeline_year_month)) {
 			// Search for default way
-			if(  wt == tram_wt  ) {
+			if(  wt == tram_wt  ||  wt == powerline_wt  ) {
 				desc = way_builder_t::weg_search(wt, 0xffffffff, timeline_year_month, type_flat);
 			}
 			else {
+				// this triggers an assertion if wt == powerline_wt
 				weg_t *w = weg_t::alloc(wt);
 				desc = w->get_desc();
 				delete w;
