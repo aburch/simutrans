adc75f6d35254995be0ebaea2a32bfda2776686e
Dwachs <dwachs@gmx.net>
2020-03-30 16:34:03 +0000

    CODE: make condition readable
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9004 8aca7d54-2c30-db11-9de9-000461428c89

--- bauer/wegbauer.cc
+++ bauer/wegbauer.cc
@@ -2581,9 +2581,12 @@ void way_builder_t::build_road()
 			weg_t * weg = gr->get_weg(road_wt);
 
 			// keep faster ways or if it is the same way
-			if(weg->get_desc()==desc  ||  keep_existing_ways  ||  (keep_existing_city_roads  &&  weg->hat_gehweg())  ||  (keep_existing_faster_ways  &&  weg->get_desc()->get_topspeed()>desc->get_topspeed())  ||  (player_builder!=NULL  &&  weg->is_deletable(player_builder)!=NULL) || (gr->get_typ()==grund_t::monorailboden && (bautyp&elevated_flag)==0)) {
+			if(weg->get_desc()==desc  ||  keep_existing_ways
+				||  (keep_existing_city_roads  &&  weg->hat_gehweg())
+				||  (keep_existing_faster_ways  &&  weg->get_desc()->get_topspeed()>desc->get_topspeed())
+				||  (player_builder!=NULL  &&  weg->is_deletable(player_builder)!=NULL)
+				||  (gr->get_typ()==grund_t::monorailboden && (bautyp&elevated_flag)==0)) {
 				//nothing to be done
-//DBG_MESSAGE("way_builder_t::build_road()","nothing to do at (%i,%i)",k.x,k.y);
 			}
 			else {
 				// we take ownership => we take care to maintain the roads completely ...
