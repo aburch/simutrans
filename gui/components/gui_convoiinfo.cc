9c9e049f3715c1c65e35e3777e37174344977924
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-04-03 01:37:36 +0900

    CHG: Line name can be displayed even in the payload display mode

--- gui/components/gui_convoiinfo.cc
+++ gui/components/gui_convoiinfo.cc
@@ -121,15 +121,27 @@ void gui_convoiinfo_t::draw(scr_coord offset)
 		// name, use the convoi status color for redraw: Possible colors are YELLOW (not moving) BLUE: obsolete in convoi, RED: minus income, BLACK: ok
 		int max_x = display_proportional_clip(pos.x+offset.x+2+w, pos.y+offset.y+6, cnv->get_name(), ALIGN_LEFT, cnv->get_status_color(), true)+2;
 
+		// 2nd row
 		if (display_mode == cnvlist_normal) {
-			// 2nd row
 			w = display_proportional_clip(pos.x + offset.x + 2, pos.y + offset.y + 6 + LINESPACE, translator::translate("Gewinn"), ALIGN_LEFT, SYSCOL_TEXT, true) + 2;
 			char buf[256];
 			money_to_string(buf, cnv->get_jahresgewinn() / 100.0);
 			w += display_proportional_clip(pos.x + offset.x + 2 + w + 5, pos.y + offset.y + 6 + LINESPACE, buf, ALIGN_LEFT, cnv->get_jahresgewinn() > 0 ? MONEY_PLUS : MONEY_MINUS, true);
 			max_x = max(max_x, w + 5);
 
-			// 3rd row
+		}
+		else if (display_mode == cnvlist_payload) {
+			payload.set_cnv(cnv);
+			payload.draw(pos + offset + scr_coord(0, LINESPACE + 4));
+		}
+		else if (display_mode == cnvlist_formation) {
+			formation.set_cnv(cnv);
+			formation.draw(pos + offset + scr_coord(0, LINESPACE + 6));
+		}
+
+		// 3rd row
+		if (display_mode == cnvlist_normal || display_mode == cnvlist_payload)
+		{
 			// only show assigned line, if there is one!
 			if (cnv->in_depot()) {
 				const char *txt = translator::translate("(in depot)");
@@ -142,30 +154,22 @@ void gui_convoiinfo_t::draw(scr_coord offset)
 				max_x = max(max_x, w + 5);
 			}
 
-			// we will use their images offsets and width to shift them to their correct position
-			// this should work with any vehicle size ...
-			const int xoff = max(190, max_x);
-			int left = pos.x + offset.x + xoff + 4;
-			for (unsigned i = 0; i < cnv->get_vehicle_count();i++) {
-				scr_coord_val x, y, w, h;
-				const image_id image = cnv->get_vehicle(i)->get_loaded_image();
-				display_get_base_image_offset(image, &x, &y, &w, &h);
-				display_base_img(image, left - x, pos.y + offset.y + 13 - y - h / 2, cnv->get_owner()->get_player_nr(), false, true);
-				left += (w * 2) / 3;
-			}
+			if (display_mode == cnvlist_normal) {
+				// we will use their images offsets and width to shift them to their correct position
+				// this should work with any vehicle size ...
+				const int xoff = max(190, max_x);
+				int left = pos.x + offset.x + xoff + 4;
+				for (unsigned i = 0; i < cnv->get_vehicle_count();i++) {
+					scr_coord_val x, y, w, h;
+					const image_id image = cnv->get_vehicle(i)->get_loaded_image();
+					display_get_base_image_offset(image, &x, &y, &w, &h);
+					display_base_img(image, left - x, pos.y + offset.y + 13 - y - h / 2, cnv->get_owner()->get_player_nr(), false, true);
+					left += (w * 2) / 3;
+				}
 
-			// since the only remaining object is the loading bar, we can alter its position this way ...
-			filled_bar.draw(pos + offset + scr_coord(xoff, 0));
-		}
-		else if (display_mode == cnvlist_payload) {
-			// 2nd row
-			payload.set_cnv(cnv);
-			payload.draw(pos + offset + scr_coord(0, LINESPACE + 6));
-		}
-		else if (display_mode == cnvlist_formation) {
-			// 2nd row
-			formation.set_cnv(cnv);
-			formation.draw(pos + offset + scr_coord(0, LINESPACE + 6));
+				// since the only remaining object is the loading bar, we can alter its position this way ...
+				filled_bar.draw(pos + offset + scr_coord(xoff, 0));
+			}
 		}
 	}
 }
