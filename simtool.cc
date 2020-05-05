02ee4d050137a8033ce085f609ed23105e085f44
Markus Pristovsek <prissi@physik.tu-berlin.de>
2020-05-05 13:38:28 +0000

    CHG: record messages and some status messages are now not saved, while stuck and full stations expire after a month on server chat to keep overview
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9068 8aca7d54-2c30-db11-9de9-000461428c89

--- simtool.cc
+++ simtool.cc
@@ -7703,11 +7703,11 @@ bool tool_add_message_t::init(player_t *player)
 	if (  *default_param  ) {
 		uint32 type = atoi(default_param);
 		const char* text = strchr(default_param, ',');
-		if ((type & ~message_t::local_flag) >= message_t::MAX_MESSAGE_TYPE  ||  text == NULL) {
+		if ((type & ~message_t::playermsg_flag) >= message_t::MAX_MESSAGE_TYPE  ||  text == NULL) {
 			return false;
 		}
 		welt->get_message()->add_message( text+1, koord::invalid, type,
-							    type & message_t::local_flag ? color_idx_to_rgb(COL_BLACK) : PLAYER_FLAG|player->get_player_nr(), IMG_EMPTY );
+							    type & message_t::playermsg_flag ? color_idx_to_rgb(COL_BLACK) : PLAYER_FLAG|player->get_player_nr(), IMG_EMPTY );
 	}
 	return false;
 }
