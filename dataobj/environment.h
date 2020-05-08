191074bab9eb42bea2af9619cd9799d2e5aa2098
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-05-09 08:57:23 +0900

    CHG: Show convoy/line name as independent overlay

--- dataobj/environment.h
+++ dataobj/environment.h
@@ -294,6 +294,9 @@ public:
 	 */
 	static sint32 show_names;
 
+	/// Show convoy nameplates (line or convoy name). since EX-14.9 rev21
+	static uint8 show_cnv_nameplates;
+
 	/// if a schedule is open, show tiles which are used by it
 	static bool visualize_schedule;
 
