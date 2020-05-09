d37dc65344f33661994fada864b079830965e15b
Markus Pristovsek <prissi@physik.tu-berlin.de>
2020-05-09 15:45:29 +0000

    Disable line management buttons for other players
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9080 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/schedule_list.h
+++ gui/schedule_list.h
@@ -27,7 +27,7 @@ class player_t;
 class schedule_list_gui_t : public gui_frame_t, public action_listener_t
 {
 private:
-	player_t *player;
+	player_t *player, *old_player;
 
 	button_t bt_new_line, bt_edit_line, bt_delete_line, bt_withdraw_line;
 	gui_scrolled_list_t scl, scrolly_convois, scrolly_haltestellen;
