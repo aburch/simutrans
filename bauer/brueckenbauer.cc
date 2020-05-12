aburch/simutrans

commit 2f58c0d7362778bf622796cf4aaa9967e3147b52

CODE: move bridge-building checks from tool_build_bridge_t::is_valid_pos to bridge_builder_t::can_place_ramp then these checks are available for scripts, too
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9085 8aca7d54-2c30-db11-9de9-000461428c89

bauer/brueckenbauer.cc
@@ -598,6 +598,61 @@ bool bridge_builder_t::can_place_ramp(player_t *player, const grund_t *gr, wayty
 	     gr->get_typ()!=grund_t::tunnelboden  ) {
 		return false;
 	}
+	// wrong slope
+	if(!slope_t::is_way(gr->get_grund_hang())) {
+		return false;
+	}
+	// blocked from above
+	koord3d pos = gr->get_pos();
+	if(  welt->lookup( pos + koord3d(0, 0, 1))  ||  (welt->get_settings().get_way_height_clearance()==2  &&  welt->lookup( pos + koord3d(0, 0, 2) ))  ) {
+		return false;
+	}
+	// check for ribis and crossings
+	bool test_ribi = r != ribi_t::none;
+	if (wt==powerline_wt) {
+		if (gr->hat_wege()) {
+			return false;
+		}
+		if (test_ribi  &&  gr->find<leitung_t>()  &&  (gr->find<leitung_t>()->get_ribi() & ~r) != 0) {
+			return false;
+		}
+	}
+	else {
+		if (gr->find<leitung_t>()) {
+			return false;
+		}
+
+		if(wt!=road_wt) {
+			// only road bridges can have other ways on it (ie trams)
+			if(gr->has_two_ways()  ||  gr->get_weg(wt) == NULL) {
+				return false;
+			}
+			if (test_ribi  &&  (gr->get_weg_ribi_unmasked(wt) & ~r) != 0) {
+				return false;
+			}
+		}
+		else {
+			// If road and tram, we have to check both ribis.
+			ribi_t::ribi ribi = ribi_t::none;
+			for(int i=0;i<2;i++) {
+				if (const weg_t *w = gr->get_weg_nr(i)) {
+					if (w->get_waytype()!=road_wt  &&  !w->get_desc()->is_tram()) {
+						return false;
+					}
+					ribi |= w->get_ribi_unmasked();
+				}
+				else break;
+			}
+			if (test_ribi  &&  (ribi & ~r) != 0) {
+				return false;
+			}
+		}
+	}
+	// ribi from slope
+	if (test_ribi  &&  gr->get_grund_hang()  &&  (ribi_type(gr->get_grund_hang()) & ~r) != 0) {
+		return false;
+	}
+	// check everything else
 	const char *error_msg = check_tile( gr, player, wt, r );
 	return (error_msg == NULL  ||  *error_msg == 0  );
 }

simtool.cc
@@ -2730,51 +2730,27 @@ uint8 tool_build_bridge_t::is_valid_pos(  player_t *player, const koord3d &pos, 
 
 	error = NULL;
 	grund_t *gr = welt->lookup(pos);
-	if(  gr==NULL  ||  !slope_t::is_way(gr->get_grund_hang())  ||  !bridge_builder_t::can_place_ramp( player, gr, wt, (is_first_click() ? 0 : ribi_type(pos-start)) )  ) {
-		return 0;
-	}
-
-	if(  welt->lookup( pos + koord3d(0, 0, 1))  ||  (welt->get_settings().get_way_height_clearance()==2  &&  welt->lookup( pos + koord3d(0, 0, 2) ))  ) {
+	if(  gr==NULL  ||  !bridge_builder_t::can_place_ramp( player, gr, wt, (is_first_click() ? 0 : ribi_type(pos-start)) )  ) {
 		return 0;
 	}
 
 	if(  is_first_click()  ) {
 		if(  gr->ist_karten_boden()  ) {
 			// first click
+			// check ribis, all other checks already done
 			ribi_t::ribi rw = ribi_t::none;
 			if (wt==powerline_wt) {
-				if (gr->hat_wege()) {
-					return 0;
-				}
 				if (gr->find<leitung_t>()) {
 					rw |= gr->find<leitung_t>()->get_ribi();
 				}
 			}
 			else {
-				if (gr->find<leitung_t>()) {
-					return 0;
-				}
-				if(wt!=road_wt) {
-					// only road bridges can have other ways on it (ie trams)
-					if(gr->has_two_ways()  ||  (gr->hat_wege() && gr->get_weg_nr(0)->get_waytype()!=wt) ) {
-						return 0;
+				// way types are checked, take all ribis
+				for(int i=0;i<2;i++) {
+					if (const weg_t *w = gr->get_weg_nr(i)) {
+						rw |= w->get_ribi_unmasked();
 					}
-					if(gr->hat_wege()){
-						rw |= gr->get_weg_nr(0)->get_ribi_unmasked();
-					}
-				}
-				else {
-					// If road and tram, we have to check both ribis.
-					for(int i=0;i<2;i++) {
-						const weg_t *w = gr->get_weg_nr(i);
-						if (w) {
-							if (w->get_waytype()!=road_wt  &&  !w->get_desc()->is_tram()) {
-								return 0;
-							}
-							rw |= w->get_ribi_unmasked();
-						}
-						else break;
-					}
+					else break;
 				}
 			}
 			// ribi from slope

commit 227ec32e12da7e9ed7768b3d911a5d68d22af394

FIX bridge building for non-road bridges (broken by r9085)
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9089 8aca7d54-2c30-db11-9de9-000461428c89

bauer/brueckenbauer.cc
@@ -624,7 +624,7 @@ bool bridge_builder_t::can_place_ramp(player_t *player, const grund_t *gr, wayty
 
 		if(wt!=road_wt) {
 			// only road bridges can have other ways on it (ie trams)
-			if(gr->has_two_ways()  ||  gr->get_weg(wt) == NULL) {
+			if(gr->has_two_ways()  ||  (gr->hat_wege()  && gr->get_weg(wt) == NULL) ) {
 				return false;
 			}
 			if (test_ribi  &&  (gr->get_weg_ribi_unmasked(wt) & ~r) != 0) {