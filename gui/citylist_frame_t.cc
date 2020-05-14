c4bb49b4b969998157fedd8defa565db099fff61
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-05-14 19:21:10 +0900

    CHG: Replace sort button with combo box - citylist

--- gui/citylist_frame_t.cc
+++ gui/citylist_frame_t.cc
@@ -126,15 +126,23 @@ citylist_frame_t::citylist_frame_t() :
 	sort_label.set_pos(scr_coord(BUTTON1_X, 40-D_BUTTON_HEIGHT-(LINESPACE+1)));
 	add_component(&sort_label);
 
-	sortedby.init(button_t::roundbox, "", scr_coord(BUTTON1_X, 40-D_BUTTON_HEIGHT), scr_size(D_BUTTON_WIDTH,D_BUTTON_HEIGHT));
+	sortedby.set_pos(scr_coord(BUTTON1_X,40 - D_BUTTON_HEIGHT));
+	sortedby.set_size(scr_size(D_BUTTON_WIDTH*1.5, D_BUTTON_HEIGHT));
+	sortedby.set_max_size(scr_size(LINESPACE*8, D_BUTTON_HEIGHT));
+
+	for (int i = 0; i < citylist::SORT_MODES; i++) {
+		sortedby.append_element(new gui_scrolled_list_t::const_text_scrollitem_t(translator::translate(sort_text[i]), SYSCOL_TEXT));
+	}
+	sortedby.set_selection(get_sortierung());
+
 	sortedby.add_listener(this);
 	add_component(&sortedby);
 
-	sorteddir.init(button_t::roundbox, "", scr_coord(BUTTON2_X, 40-D_BUTTON_HEIGHT), scr_size(D_BUTTON_WIDTH,D_BUTTON_HEIGHT));
+	sorteddir.init(button_t::roundbox, "", scr_coord(BUTTON1_X+ D_BUTTON_WIDTH*1.5, 40-D_BUTTON_HEIGHT), scr_size(D_BUTTON_WIDTH,D_BUTTON_HEIGHT));
 	sorteddir.add_listener(this);
 	add_component(&sorteddir);
 
-	filter_within_network.init(button_t::square_state, "Within own network", scr_coord(BUTTON3_X + D_H_SPACE, 40-D_BUTTON_HEIGHT));
+	filter_within_network.init(button_t::square_state, "Within own network", scr_coord(D_BUTTON_WIDTH*1.5 + BUTTON2_X + D_H_SPACE, 40-D_BUTTON_HEIGHT));
 	filter_within_network.set_tooltip("Show only cities within the active player's transportation network");
 	filter_within_network.add_listener(this);
 	filter_within_network.pressed = filter_own_network;
@@ -146,7 +154,6 @@ citylist_frame_t::citylist_frame_t() :
 	add_component(&show_stats);
 
 	// name buttons
-	sortedby.set_text(sort_text[get_sortierung()]);
 	sorteddir.set_text(get_reverse() ? "hl_btn_sort_desc" : "hl_btn_sort_asc");
 
 	year_month_tabs.add_tab(&chart, translator::translate("Years"));
@@ -204,7 +211,6 @@ bool citylist_frame_t::action_triggered( gui_action_creator_t *comp,value_t /* *
 {
 	if(comp == &sortedby) {
 		set_sortierung((citylist::sort_mode_t)((get_sortierung() + 1) % citylist::SORT_MODES));
-		sortedby.set_text(sort_text[get_sortierung()]);
 		stats.sort(get_sortierung(),get_reverse(), get_filter_own_network());
 		stats.recalc_size();
 	}
