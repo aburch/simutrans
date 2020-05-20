ca1f2d7fa4799dea65ad4b4b1ba038fc01b2ac20
jamespetts <jamespetts@yahoo.com>
2020-05-20 12:20:55 +0100

    Revert "Remove using namespace std"
    This reverts commit 65192df78a7969d7cd4bce04aac601f63a497213.

--- simworld.cc
+++ simworld.cc
@@ -8786,7 +8786,7 @@ void karte_t::load(loadsave_t *file)
 		bool read_userdir_simuconf = env_t::default_settings.get_userdir_overrides_savegame_settings();
 		tabfile_t simuconf;
 		sint16 idummy;
-		std::string dummy;
+		string dummy;
 
 		if (read_progdir_simuconf) {
 			chdir( env_t::program_dir );
