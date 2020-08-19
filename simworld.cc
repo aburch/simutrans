a244faf7e5a063cc3428033850e59294f89dea47
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-08-19 22:54:05 +0900

    FIX: marge errors

--- simworld.cc
+++ simworld.cc
@@ -9991,13 +9991,6 @@ void karte_t::prepare_tiles(rect_t const &new_area, rect_t const &old_area) {
 	}
 }
 
-void karte_t::update_underground()
-{
-	DBG_MESSAGE( "karte_t::update_underground_map()", "" );
-	world_xy_loop(&karte_t::update_underground_intern, SYNCX_FLAG);
-	set_dirty();
-}
-
 void karte_t::update_underground_intern( sint16 x_min, sint16 x_max, sint16 y_min, sint16 y_max )
 {	 
 	for(  sint16 y = y_min;  y < y_max;  y++  ) {
