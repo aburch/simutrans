6171d9f73746bdffef66fb1a1127b2adec1bce2e
ceeac <ceeac@users.noreply.github.com>
2020-08-23 14:10:11 +0200

    Correct include in simsys_s2.cc

--- sys/simsys_s2.cc
+++ sys/simsys_s2.cc
@@ -3,14 +3,10 @@
  * (see LICENSE.txt)
  */
 
-#if (defined(__MINGW64__) || defined(__MINGW32__))
-#  include <windows.h>
-#  include <SDL2/SDL.h>
-#elif defined(_WIN32)
-#  include <windows.h>
-#  include "SDL.h"
-#else
-#  include <SDL2/SDL.h>
+#include <SDL2/SDL.h>
+
+#if defined(_WIN32)
+#include <windows.h>
 #endif
 
 #include <stdio.h>
