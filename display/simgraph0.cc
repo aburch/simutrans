61c00f07bafdce3b279547065b0aed956700652e
ceeac <ceeac@users.noreply.github.com>
2020-08-27 15:13:35 +0200

    FIX: Missing return value in display_fluctuation_triangle_rgb

--- display/simgraph0.cc
+++ display/simgraph0.cc
@@ -427,6 +427,7 @@ void display_filled_circle_rgb(KOORD_VAL, KOORD_VAL, int, const PLAYER_COLOR_VAL
 
 int display_fluctuation_triangle_rgb(KOORD_VAL, KOORD_VAL, uint8, const bool, sint64)
 {
+	return 0;
 }
 
 void draw_bezier_rgb(KOORD_VAL, KOORD_VAL, KOORD_VAL, KOORD_VAL, KOORD_VAL, KOORD_VAL, KOORD_VAL, KOORD_VAL, const PLAYER_COLOR_VAL, KOORD_VAL, KOORD_VAL)
