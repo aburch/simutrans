b5d379f249e8f7970b814d73fe6068e987f12ae1
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-08-28 18:26:00 +0900

    FIX: Missing override

--- gui/money_frame.h
+++ gui/money_frame.h
@@ -146,7 +146,7 @@ public:
 
 	bool action_triggered(gui_action_creator_t*, value_t) OVERRIDE;
 
-	bool infowin_event(const event_t *ev);
+	bool infowin_event(const event_t *ev) OVERRIDE;
 
 	// saving/restore stuff
 	uint32 get_rdwr_id() OVERRIDE;
