3b01df3f52470b1ce93fc9c526bfb45d7a4ed5b2
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-08-30 02:20:11 +0900

    FIX: Factory status color conversion error

--- gui/halt_detail.cc
+++ gui/halt_detail.cc
@@ -1077,9 +1077,8 @@ void gui_halt_nearby_factory_info_t::draw(scr_coord offset)
 	FORX(const slist_tpl<fabrik_t*>, const fab, fab_list, yoff += LINESPACE + 1) {
 		xoff = D_POS_BUTTON_WIDTH + D_H_SPACE;
 		// [status color bar]
-		PIXVAL col = fabrik_t::status_to_color[fab->get_status() % fabrik_t::staff_shortage];
-		uint indikatorfarbe = fabrik_t::status_to_color[fab->get_status() % fabrik_t::staff_shortage];
-		display_fillbox_wh_clip_rgb(offset.x + xoff + 1, offset.y + yoff + 3, D_INDICATOR_WIDTH / 2 - 1, D_INDICATOR_HEIGHT, col, true);
+		const PIXVAL col_val = color_idx_to_rgb(fabrik_t::status_to_color[fab->get_status() % fabrik_t::staff_shortage]);
+		display_fillbox_wh_clip_rgb(offset.x + xoff + 1, offset.y + yoff + 3, D_INDICATOR_WIDTH / 2 - 1, D_INDICATOR_HEIGHT, col_val, true);
 		xoff += D_INDICATOR_WIDTH / 2 + 3;
 
 		// [name]
