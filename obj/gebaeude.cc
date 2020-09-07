238e9d73a13f210c755e67d0428c6debaf031ab2
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-09-07 16:51:44 +0900

    (r8653) Link to headquarter info

--- obj/gebaeude.cc
+++ obj/gebaeude.cc
@@ -14,7 +14,7 @@ static pthread_mutex_t add_to_city_mutex = PTHREAD_MUTEX_INITIALIZER;
 #endif
 
 #include "../bauer/hausbauer.h"
-#include "../gui/money_frame.h"
+#include "../gui/headquarter_info.h"
 #include "../simworld.h"
 #include "../simobj.h"
 #include "../simfab.h"
@@ -818,7 +818,7 @@ void gebaeude_t::show_info()
 	bool special = is_headquarter() || is_townhall();
 
 	if (is_headquarter()) {
-		create_win(new money_frame_t(get_owner()), w_info, magic_finances_t + get_owner()->get_player_nr());
+		create_win( new headquarter_info_t(get_owner()), w_info, magic_headquarter+get_owner()->get_player_nr() );
 	}
 	else if (is_townhall()) {
 		get_stadt()->show_info();
