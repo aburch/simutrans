7fb5ff3427734e56e87837bbbe8e5e20128e8ed0
Markus Pristovsek <prissi@physik.tu-berlin.de>
2020-09-10 13:34:09 +0000

    FIX: climate map was not ratoated
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9232 8aca7d54-2c30-db11-9de9-000461428c89

--- simworld.cc
+++ simworld.cc
@@ -3339,6 +3339,8 @@ DBG_MESSAGE( "karte_t::rotate90()", "called" );
 	delete [] water_hgts;
 	water_hgts = rotate90_new_water;
 
+	climate_map.rotate90();
+
 	// rotate heightmap
 	sint8 *new_hgts = new sint8[(cached_grid_size.x+1)*(cached_grid_size.y+1)];
 	const int LOOP_BLOCK = 64;
