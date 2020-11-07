f8a5047942c0f0635ffef2f767bad56211d3065f
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-11-07 22:13:52 +0900

    FIX: The size of the link button to the signalbox is not initialized

--- gui/signal_info.cc
+++ gui/signal_info.cc
@@ -156,6 +156,7 @@ signal_info_t::signal_info_t(signal_t* const s) :
 		add_component(&lb_sb_name);
 
 		bt_info_signalbox.init(button_t::roundbox, "Details");
+		bt_info_signalbox.set_size(scr_size(D_BUTTON_WIDTH*2/3, D_BUTTON_HEIGHT));
 		bt_info_signalbox.set_visible(false);
 		bt_info_signalbox.set_tooltip(translator::translate("open_signalbox_info"));
 		bt_info_signalbox.add_listener(this);
