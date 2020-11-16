3c0d1c74cd9cad788bb3553ec8fc89114007eb18
ceeac <git.ceeac@gmail.com>
2020-11-16 23:19:06 +0000

    CODE: Add rdwr_stream_t to read or write data
    ADD: classify_file() to determine file format
    FIX: Data race potentially causing hang when reading zstd saves multi-threaded
    
    Cherry-pick of commit 1f61ea16e from Standard.

--- simworld.cc
+++ simworld.cc
@@ -3131,9 +3131,6 @@ karte_t::karte_t() :
 	// for new world just set load version to current savegame version
 	load_version = loadsave_t::int_version( env_t::savegame_version_str, NULL );
 
-	load_version.extended_version = EX_VERSION_MAJOR;
-	load_version.extended_revision = EX_VERSION_MINOR;
-
 	// standard prices
 	goods_manager_t::set_multiplier( 1000, settings.get_meters_per_tile() );
 
@@ -6046,7 +6043,7 @@ void karte_t::step()
 	// routings for goods/passengers.
 	// Default: 8192 ~ 1h (game time) at 125m/tile.
 
-	// This is not the computationally intensive bit of the path explorer. 
+	// This is not the computationally intensive bit of the path explorer.
 	if((steps % get_settings().get_reroute_check_interval_steps()) == 0)
 	{
 		path_explorer_t::refresh_all_categories(false);
@@ -8449,7 +8446,12 @@ DBG_MESSAGE("karte_t::save()", "saving game to '%s'", filename);
 	}
 
 	display_show_load_pointer( true );
-	if(!file.wr_open( savename.c_str(), autosave ? loadsave_t::autosave_mode : loadsave_t::save_mode, autosave ? loadsave_t::autosave_level : loadsave_t::save_level, env_t::objfilename.c_str(), version_str, ex_version_str, ex_revision_str )) {
+
+	const loadsave_t::mode_t mode = autosave ? loadsave_t::autosave_mode : loadsave_t::save_mode;
+	const int level = autosave ? loadsave_t::autosave_level : loadsave_t::save_level;
+	loadsave_t::file_status_t status = file.wr_open( savename.c_str(), mode, level, env_t::objfilename.c_str(), version_str, ex_version_str, ex_revision_str );
+
+	if(status != loadsave_t::FILE_STATUS_OK) {
 		create_win(new news_img("Kann Spielstand\nnicht speichern.\n"), w_info, magic_none);
 		dbg->error("karte_t::save()","cannot open file for writing! check permissions!");
 	}
@@ -8896,7 +8898,7 @@ DBG_MESSAGE("karte_t::save(loadsave_t *file)", "motd filename %s", env_t::server
 	{
 		path_explorer_t::rdwr(file);
 	}
-	
+
 	if (file->get_extended_version() >= 15 || (file->get_extended_version() == 14 && file->get_extended_revision() >= 35))
 	{
 		uint32 count = cities_awaiting_private_car_route_check.get_count();
@@ -8905,7 +8907,7 @@ DBG_MESSAGE("karte_t::save(loadsave_t *file)", "motd filename %s", env_t::server
 		for (auto city : cities_awaiting_private_car_route_check)
 		{
 			koord location = city->get_center();
-			location.rdwr(file); 
+			location.rdwr(file);
 		}
 	}
 
@@ -9085,7 +9087,7 @@ bool karte_t::load(const char *filename)
 		name.append(filename);
 	}
 
-	if(!file.rd_open(name)) {
+	if(file.rd_open(name) != loadsave_t::FILE_STATUS_OK) {
 
 		if(file.get_version_int() == 0 || file.get_version_int() > loadsave_t::int_version(env_t::savegame_version_str, NULL).version) {
 			dbg->warning("karte_t::load()", translator::translate("WRONGSAVE") );
@@ -9124,7 +9126,7 @@ DBG_MESSAGE("karte_t::load()","Savegame version is %u", file.get_version_int());
 				char fn[256];
 				sprintf( fn, "server%d-pwdhash.sve", env_t::server );
 				loadsave_t pwdfile;
-				if(  pwdfile.rd_open(fn)  ) {
+				if(  pwdfile.rd_open(fn) == loadsave_t::FILE_STATUS_OK  ) {
 					rdwr_player_password_hashes( &pwdfile );
 					// correct locking info
 					nwc_auth_player_t::init_player_lock_server(this);
@@ -10187,7 +10189,7 @@ DBG_MESSAGE("karte_t::load()", "%d factories loaded", fab_list.get_count());
 			koord location;
 			location.rdwr(file);
 			stadt_t* city = get_city(location);
-			cities_awaiting_private_car_route_check.append(city); 
+			cities_awaiting_private_car_route_check.append(city);
 		}
 	}
 
