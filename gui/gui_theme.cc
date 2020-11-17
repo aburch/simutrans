caf066e845d5ed7cdd32ba60e996c553d6e0cda6
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-11-18 00:56:56 +0900

    FIX: Default obsolete color was incorrect

--- gui/gui_theme.cc
+++ gui/gui_theme.cc
@@ -188,7 +188,7 @@ void gui_theme_t::init_gui_defaults()
 	gui_color_loadingbar_inner             = color_idx_to_rgb(COL_GREY5);
 	gui_color_loadingbar_progress          = color_idx_to_rgb(COL_SOFT_BLUE);
 
-	gui_color_obsolete                     = color_idx_to_rgb(COL_BLUE);
+	gui_color_obsolete                     = color_idx_to_rgb(COL_DARK_BLUE);
 	gui_color_out_of_production            = color_idx_to_rgb(COL_SOFT_BLUE);
 	gui_color_empty                        = color_idx_to_rgb(COL_WHITE);
 
