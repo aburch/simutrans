91d302c4db95bf4bf9c02cdc42dcd9ae59d9732b
(´・ω・｀) <Ranran-the-JuicyPork@users.noreply.github.com>
2020-12-09 22:11:04 +0900

    FIX: The transparency flag is set even when hiding with the construction site

--- obj/gebaeude.cc
+++ obj/gebaeude.cc
@@ -675,7 +675,7 @@ FLAGGED_PIXVAL gebaeude_t::get_outline_colour() const
 {
 	uint8 colours[] = { COL_BLACK, COL_YELLOW, COL_YELLOW, COL_PURPLE, COL_RED, COL_GREEN };
 	FLAGGED_PIXVAL disp_colour = 0;
-	if (env_t::hide_buildings != env_t::NOT_HIDE) {
+	if(env_t::hide_buildings!=env_t::NOT_HIDE && env_t::hide_with_transparency) {
 		if(is_city_building()) {
 			disp_colour = color_idx_to_rgb(colours[0]) | TRANSPARENT50_FLAG | OUTLINE_FLAG;
 		}
