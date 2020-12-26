6774f9b68e100de89998ed0a718899c8d22f8994
James E. Petts <jamespetts@yahoo.com>
2020-12-26 11:20:06 +0000

    CODE: Revert the attempted fix to the power line graphics as this does not work

--- obj/leitung2.cc
+++ obj/leitung2.cc
@@ -341,15 +341,35 @@ void leitung_t::calc_image()
 		mark_image_dirty(old_image,0);
 	}
 }
-
+/*
 image_id leitung_t::get_image() const
 {
+	if (is_crossing)
+	{
+		return IMG_EMPTY;
+	}
+	
 	grund_t *gr = welt->lookup(get_pos());
-	if (gr == NULL || gr->ist_bruecke() || is_crossing) {
+	 if(gr == NULL || gr->ist_bruecke()) {
 		return IMG_EMPTY;
 	}
 	return image;
 }
+
+image_id leitung_t::get_front_image() const
+{
+	if (is_crossing)
+	{
+		return IMG_EMPTY;
+	}
+	
+	grund_t *gr = welt->lookup(get_pos());
+	 if(gr == NULL || gr->ist_bruecke()) {
+		return IMG_EMPTY;
+	}
+	return image;
+}
+*/
 
 /**
  * Recalculates the images of all neighbouring
