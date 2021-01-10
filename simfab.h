7ba279ad3de059fa03e4231c834f497fddd46272
Markus Pristovsek <prissi@physik.tu-berlin.de>
2021-01-10 12:13:42 +0000

    FIX: rotate also smoke on factory rotation with rotation tool
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9541 8aca7d54-2c30-db11-9de9-000461428c89

--- simfab.h
+++ simfab.h
@@ -620,6 +620,7 @@ public:
 	void build(sint32 rotate, bool build_fields, bool force_initial_prodbase, bool from_saved = false);
 
 	sint16 get_rotate() const { return rotate; }
+	void set_rotate( uint8 r ) { rotate = r; }
 
 	/* field generation code
 	 * spawns a field for sure if probability>=1000
