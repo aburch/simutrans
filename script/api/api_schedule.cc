2305cc04c1955f0ce58c4ee498ee5341c703ffa7
Dwachs <dwachs@gmx.net>
2021-05-19 18:16:00 +0000

    sqapi ADD schedule_entry_x.waiting_time_to_string
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9790 8aca7d54-2c30-db11-9de9-000461428c89

--- script/api/api_schedule.cc
+++ script/api/api_schedule.cc
@@ -11,6 +11,7 @@
 #include "../api_function.h"
 #include "../../dataobj/schedule.h"
 #include "../../simhalt.h"
+#include "../../simintr.h"
 #include "../../simworld.h"
 
 using namespace script_api;
@@ -22,6 +23,14 @@ halthandle_t get_halt_from_koord3d(koord3d pos, const player_t *player )
 		return halthandle_t();
 	}
 	return haltestelle_t::get_halt(pos, player);
+}
+
+SQInteger waiting_time_to_string(HSQUIRRELVM vm)
+{
+	schedule_entry_t entry(koord3d::invalid, 0, 0);
+	get_slot(vm, "wait", entry.waiting_time, -1);
+	plainstring str = difftick_to_string(entry.get_waiting_ticks(), false);
+	return param<plainstring>::push(vm, str);
 }
 
 SQInteger schedule_constructor(HSQUIRRELVM vm) // instance, wt, entries
@@ -130,6 +139,11 @@ void export_schedule(HSQUIRRELVM vm)
 	 * @typemask halt_x(player_x)
 	 */
 	register_method(vm, &get_halt_from_koord3d, "get_halt", true);
+	/**
+	 * Returns waiting time formatted as string.
+	 * @typemask string()
+	 */
+	register_function(vm, waiting_time_to_string, "waiting_time_to_string", 1, "x" );
 
 	end_class(vm);
 
