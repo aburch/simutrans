3f8a709e89629e4aff051e0faf0d5458387f3c82
ceeac <git.ceeac@gmail.com>
2021-07-31 04:53:18 +0000

    ADD: [CMake] Use fallback revision string for git commits not submitted to svn
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@9979 8aca7d54-2c30-db11-9de9-000461428c89

--- cmake/SimutransRevision.cmake
+++ cmake/SimutransRevision.cmake
@@ -3,20 +3,23 @@
 # (see licence.txt)
 #
 
-include(FindSubversion)
-include(FindGit)
+find_package(Subversion QUIET)
+find_package(Git QUIET)
 
-
-# We have to try Git first because Subversion_WC_INFO does not fail silently if
-# this repository is not a Subversion repository
 # assume this is from aburch/simutrans!
-if (GIT_FOUND)
-	execute_process(WORKING_DIRECTORY ${SOURCE_DIR} COMMAND ${GIT_EXECUTABLE} log -1 RESULT_VARIABLE res_var OUTPUT_VARIABLE SIMUTRANS_RAW_WC_REVISION)
-	if ( ${res_var} EQUAL 0 )
+if (Git_FOUND)
+	execute_process(WORKING_DIRECTORY "${SOURCE_DIR}"
+		COMMAND "${GIT_EXECUTABLE}" log -1
+		RESULT_VARIABLE res_var
+		OUTPUT_VARIABLE SIMUTRANS_RAW_WC_REVISION
+	)
+
+	if (res_var EQUAL 0)
 		message( "git log -1 ok:" ${SIMUTRANS_RAW_WC_REVISION})
 		string( REGEX REPLACE "[\t\r\n]" " " TEMP1 ${SIMUTRANS_RAW_WC_REVISION})
 		string( REGEX REPLACE "^.*trunk\@" "" TEMP2 ${TEMP1})
 		string( REGEX REPLACE " .*$" "" SIMUTRANS_WC_REVISION ${TEMP2})
+
 		if(SIMUTRANS_WC_REVISION MATCHES "commit")
 			unset(SIMUTRANS_WC_REVISION)
 		endif()
@@ -24,9 +27,30 @@ if (GIT_FOUND)
 endif ()
 
 if ( NOT SIMUTRANS_WC_REVISION AND Subversion_FOUND )
-	execute_process(WORKING_DIRECTORY ${SOURCE_DIR} COMMAND svn info --show-item revision RESULT_VARIABLE res_var OUTPUT_VARIABLE SIMUTRANS_WC_REVISION)
-	if ( NOT ${res_var} EQUAL 0 )
+	execute_process(WORKING_DIRECTORY "${SOURCE_DIR}"
+		COMMAND svn info --show-item revision
+		RESULT_VARIABLE res_var
+		OUTPUT_VARIABLE SIMUTRANS_WC_REVISION
+		ERROR_VARIABLE dummy
+	)
+
+	if (res_var)
 		# SVN not found => no result
+		unset(SIMUTRANS_WC_REVISION)
+	endif ()
+endif ()
+
+# Fallback for git commits not submitted to svn (e.g. when using git-svn)
+if (NOT SIMUTRANS_WC_REVISION AND Git_FOUND)
+	execute_process(WORKING_DIRECTORY "${SOURCE_DIR}"
+		COMMAND "${GIT_EXECUTABLE}" describe --tags --always
+		RESULT_VARIABLE res_var
+		OUTPUT_VARIABLE SIMUTRANS_WC_REVISION
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+
+	if (res_var)
+		# not a git repository
 		unset(SIMUTRANS_WC_REVISION)
 	endif ()
 endif ()
@@ -36,15 +60,16 @@ if (SIMUTRANS_WC_REVISION)
 	file(WRITE revision.h.txt "#define REVISION ${SIMUTRANS_WC_REVISION}\n")
 	# copy the file to the final header only if the version changes
 	# reduces needless rebuilds
-	execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different revision.h.txt ${SOURCE_DIR}/revision.h)
+	execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different revision.h.txt "${SOURCE_DIR}/revision.h")
 
 	message(STATUS "Compiling Simutrans revision ${SIMUTRANS_WC_REVISION} ...")
 else ()
 	message(WARNING "Could not find revision information because this repository "
 		"is neither a Subversion nor a Git repository. Revision information "
 		"will be unavailable.")
-	if (NOT EXISTS ${SOURCE_DIR}/revision.h)
-		file(WRITE ${SOURCE_DIR}/revision.h "#define REVISION \n")
+
+	if (NOT EXISTS "${SOURCE_DIR}/revision.h")
+		file(WRITE "${SOURCE_DIR}/revision.h" "#define REVISION \n")
 	endif ()
 endif ()
 
