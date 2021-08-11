b8b5cd20a7b26e9d1ba3eae0c4146df73c990d2b
ceeac <git.ceeac@gmail.com>
2021-08-11 20:03:07 +0000

    FIX: Typo in gui_schedule_t::action_triggered
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10005 8aca7d54-2c30-db11-9de9-000461428c89

--- gui/components/gui_schedule.cc
+++ gui/components/gui_schedule.cc
@@ -590,7 +590,7 @@ bool gui_schedule_t::action_triggered( gui_action_creator_t *comp, value_t p)
 				schedule->entries[schedule->get_current_stop()].minimum_loading = max( 101, min(255,(uint8)p.i+100) );
 				// clip waiting time to 1/nth of the month
 				if( schedule->entries[schedule->get_current_stop()].waiting_time>65535/(schedule->entries[schedule->get_current_stop()].minimum_loading-100) ) {
-					schedule->entries[schedule->get_current_stop()].waiting_time>65535/(schedule->entries[schedule->get_current_stop()].minimum_loading-100);
+					schedule->entries[schedule->get_current_stop()].waiting_time=65535/(schedule->entries[schedule->get_current_stop()].minimum_loading-100);
 				}
 			}
 			update_selection();
