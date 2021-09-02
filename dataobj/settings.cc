04fd11ed7e72db961c5fce85be6d335029e4488c
Markus Pristovsek <prissi@physik.tu-berlin.de>
2021-09-02 05:34:36 +0000

    FIX: to define fonts in simuconf.tab, one needs to remove spaces
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10064 8aca7d54-2c30-db11-9de9-000461428c89

--- dataobj/settings.cc
+++ dataobj/settings.cc
@@ -953,11 +953,12 @@ void settings_t::parse_simuconf( tabfile_t& simuconf, sint16& disp_width, sint16
 #endif
 
 	//check for fontname, must be a valid name!
-	const char *fname = contents.get_string( "fontname", env_t::fontname.c_str() );
-	if( FILE *f = fopen( fname, "r" ) ) {
+	std::string fname = trim( contents.get_string( "fontname", env_t::fontname.c_str() ) );
+	if( FILE* f = fopen( fname.c_str(), "r" ) ) {
 		fclose( f );
 		env_t::fontname = fname;
 	}
+	env_t::fontsize  = contents.get_int( "fontsize", env_t::fontsize );
 
 	env_t::water_animation           = contents.get_int_clamped( "water_animation_ms",          env_t::water_animation,           0, INT_MAX);
 	env_t::ground_object_probability = contents.get_int_clamped( "random_grounds_probability",  env_t::ground_object_probability, 0, INT_MAX);
