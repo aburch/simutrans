aac00264af156bd2332cef7266b7bcb5717d4182
Markus Pristovsek <prissi@physik.tu-berlin.de>
2021-09-11 14:55:05 +0000

    (Krosk) move asset download for android into the android folder
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10089 8aca7d54-2c30-db11-9de9-000461428c89

---
+++ android/add_assets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script adds assets to embed with the Android build
+# It is called by AndroidPreBuild.sh
+# Working folder is expected to be this file location (<repo root>/simutrans/android/
+# Assets are downloaded to this folder before being copied to the target location
+
+echo "Adding assets"
+
+[ -e ../simutrans/pak ] || (wget --show-progress -nc https://downloads.sourceforge.net/project/simutrans/pak64/122-0/simupak64-122-0.zip && unzip -n simupak64-122-0.zip -d ..)
+[ -e ../simutrans/pak64.german ] || (wget --show-progress -nc http://simutrans-germany.com/pak.german/pak64.german_0-122-0-0-2_full.zip && unzip -n pak64.german_0-122-0-0-2_full.zip -d ..)
+[ -e ../simutrans/music/TimGM6mb.sf2 ] || wget -nc --show-progress https://sourceforge.net/p/mscore/code/HEAD/tree/trunk/mscore/share/sound/TimGM6mb.sf2?format=raw -O ../simutrans/music/TimGM6mb.sf2
+[ -e ../simutrans/font/RobotoCondensed-Regular.ttf ] || (wget -nc --show-progress https://fonts.google.com/download?family=Roboto%20Condensed -O Roboto_Condensed.zip && unzip -n Roboto_Condensed.zip -d ../simutrans/font)
+[ -e ../simutrans/font/Roboto-Regular.ttf ] || (wget -nc --show-progress https://fonts.google.com/download?family=Roboto -O Roboto.zip && unzip -n Roboto.zip -d ../simutrans/font)
+[ -e ../simutrans/cacert.pem ] || cp ./cacert.pem ../simutrans/cacert.pem
