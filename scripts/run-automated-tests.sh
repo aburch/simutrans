6254ae875f9a2394a43c962e0fe793b4fac3d93c
ceeac <git.ceeac@gmail.com>
2021-10-18 19:12:27 +0000

    FIX: Tests run forever when script compilation fails
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10177 8aca7d54-2c30-db11-9de9-000461428c89

--- scripts/run-automated-tests.sh
+++ scripts/run-automated-tests.sh
@@ -29,7 +29,8 @@ do
 		break
 	fi
 
-	if [[ -n "$(grep 'error \[Call function failed\] calling' output.log)" ]]
+	if [[ -n "$(grep 'error \[Call function failed\] calling' output.log)" ||
+	      -n "$(grep 'error \[Reading / compiling script failed] calling' output.log)" ]]
 	then
 		# kill
 		echo "Killing process (test failed)"
