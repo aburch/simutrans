bef01db196a178023f5f3533fd223cd3794f77ba
Markus Pristovsek <prissi@physik.tu-berlin.de>
2021-10-31 00:39:45 +0000

    Forgot revision script
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10204 8aca7d54-2c30-db11-9de9-000461428c89

---
+++ get_revision.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#
+# This file is part of the Simutrans project under the Artistic License.
+# (see LICENSE.txt)
+#
+
+#
+# script to the revision number and put it into the file revision.h
+#
+
+svn info 2>/dev/null 1>/dev/null
+if [[ $? -eq 0 ]]; then
+  # using svn for verything
+  REV=$(svnversion)
+  HASH=$REV
+else
+  # try using git
+  git describe --always 2>/dev/null 1>/dev/null
+  if [[ $? -eq 0 ]]; then
+    REV=$(git log -1 | grep "git-svn-id" | sed "s/^.*trunk\@//" | sed "s/ .*$//")
+    if [[ -z "$REV" ]]; then
+      # make preudo rev from git
+      REV=$(git rev-list --count --first-parent HEAD)
+      REV=`expr $REV + 328`
+    fi
+  else
+    REV=""
+  fi
+fi
+
+echo $REV
