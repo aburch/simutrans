cda4dfa85067406cee0f60e793b98cdd2d103008
Markus Pristovsek <prissi@physik.tu-berlin.de>
2022-05-01 10:28:36 +0000

    Dragging with left mouse also on large zoom out
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10616 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/siminteraction.cc
+++ src/simutrans/siminteraction.cc
@@ -330,7 +330,7 @@ bool interaction_t::process_event( event_t &ev )
 	else if(  IS_LEFTDRAG(&ev)  &&  IS_LEFT_BUTTON_PRESSED(&ev)  &&  (is_world_dragging  ||  (!world->get_tool(world->get_active_player_nr())->move_has_effects()  &&  !IS_CONTROL_PRESSED(&ev))  )  ) {
 		/* ok, we have a general tool selected, and we have a left drag or left release event with an actual difference
 		 * => move the map, if we are beyond a threshold */
-		if(  is_world_dragging  ||  abs(ev.cx-ev.mx)+abs(ev.cy-ev.my)>=env_t::scroll_threshold  ) {
+		if(  is_world_dragging  ||  abs(ev.cx-ev.mx)+abs(ev.cy-ev.my)>=max(1,(env_t::scroll_threshold* get_tile_raster_width())/get_base_tile_raster_width())  ) {
 			if (!is_world_dragging) {
 				display_show_pointer(false);
 				is_world_dragging = true;
