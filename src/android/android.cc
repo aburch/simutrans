876cf1dc926b0aaf423ca4c4e7a9d48b26d1bed9
Roboron3042 <Roboron3042@users.noreply.github.com>
2023-06-05 19:26:07 +0000

    fix android package path
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10913 8aca7d54-2c30-db11-9de9-000461428c89

--- src/android/android.cc
+++ src/android/android.cc
@@ -2,8 +2,8 @@
 #include "../simutrans/sys/simsys.h"
 
 extern "C" {
-JNIEXPORT jstring JNICALL Java_com_simutrans_simutrans_Simutrans_getVersion(JNIEnv* env, jobject thisObject) { 
-    return env->NewStringUTF(get_version()); 
-} 
+JNIEXPORT jstring JNICALL Java_com_simutrans_Simutrans_getVersion(JNIEnv* env, jobject thisObject) {
+    return env->NewStringUTF(get_version());
+}
 
 }
