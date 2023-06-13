9172d18c92196c9cf3f7d20d722d08705ea83672
ceeac <git.ceeac@gmail.com>
2023-06-13 08:42:47 +0000

    FIX: Broken compat.tab parsing
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10930 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/dataobj/translator.cc
+++ src/simutrans/dataobj/translator.cc
@@ -300,17 +300,19 @@ void translator::init_custom_names(int lang)
 /* now on to the translate stuff */
 
 
-static bool is_not_format_string(const char* str)
+static bool is_format_string(const char* str)
 {
 	// %._CITY_SYLL
 	if (*str == '%'  &&  *(str+1)  &&  strcmp(str+2, "_CITY_SYLL")==0) {
-		return true;
+		return false;
 	}
+
 	// .center, .suburb, .extern
 	if (*str  &&  (strcmp(str+1, "center")==0  ||  strcmp(str+1, "suburb")==0  ||  strcmp(str+1, "extern")==0) ) {
-		return true;
+		return false;
 	}
-	return false;
+
+	return true;
 }
 
 static void load_language_file_body(FILE* file, stringhashtable_tpl<const char*>* table, bool language_is_utf, bool file_is_utf, bool language_is_latin2 )
@@ -335,17 +337,17 @@ static void load_language_file_body(FILE* file, stringhashtable_tpl<const char*>
 				char *repaired = NULL;
 
 				// check format strings (only for unicode, ignore special strings)
-				if(language_is_utf  &&  (is_not_format_string(raw)  ||  cbuffer_t::check_and_repair_format_strings(raw, translated, &repaired) ) ) {
-					if (repaired) {
-						free(translated);
-						translated = repaired;
-					}
-					table->set( raw, translated );
-				}
-				else {
+				if(language_is_utf  &&  (is_format_string(raw)  &&  !cbuffer_t::check_and_repair_format_strings(raw, translated, &repaired) ) ) {
 					free(raw);
 					free(translated);
+					continue;
 				}
+				else if (repaired) {
+					free(translated);
+					translated = repaired;
+				}
+
+				table->set( raw, translated );
 			}
 		}
 	} while (!feof(file));
