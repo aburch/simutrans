27261cec58b38c88b9b2207fccb7889d8c3f6ff0
ceeac <git.ceeac@gmail.com>
2023-06-27 12:29:20 +0000

    CODE: Fix uneven indentation
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@10936 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/gui/welt.h
+++ src/simutrans/gui/welt.h
@@ -26,53 +26,52 @@ class welt_gui_t  :
 	public  gui_frame_t,
 	private action_listener_t
 {
-	private:
-		settings_t* sets;
+	settings_t* sets;
 
-		/**
-		* Mini Map-Preview
-		*/
-		array2d_tpl<PIXVAL> map;
-		scr_size            map_size;
+	/**
+	* Mini Map-Preview
+	*/
+	array2d_tpl<PIXVAL> map;
+	scr_size            map_size;
 
-		bool load_heightfield;
-		bool loaded_heightfield;
+	bool load_heightfield;
+	bool loaded_heightfield;
 
-		double city_density;
-		double industry_density;
-		double attraction_density;
-		double river_density;
+	double city_density;
+	double industry_density;
+	double attraction_density;
+	double river_density;
 
-		gui_map_preview_t
-			map_preview;
+	gui_map_preview_t
+		map_preview;
 
-		gui_numberinput_t
-			inp_map_number,
-			inp_x_size, inp_y_size;
+	gui_numberinput_t
+		inp_map_number,
+		inp_x_size, inp_y_size;
 
-		button_t
-			random_map,
-			load_map;
+	button_t
+		random_map,
+		load_map;
 
-		gui_numberinput_t
-			inp_number_of_towns,
-			inp_town_size,
-			inp_intercity_road_len,
-			inp_other_industries,
-			inp_tourist_attractions,
-			inp_intro_date;
+	gui_numberinput_t
+		inp_number_of_towns,
+		inp_town_size,
+		inp_intercity_road_len,
+		inp_other_industries,
+		inp_tourist_attractions,
+		inp_intro_date;
 
-		gui_label_buf_t
-			size_label;
-		button_t
-			use_intro_dates,
-			use_beginner_mode,
-			open_climate_gui,
-			open_setting_gui,
-			load_game,
-			load_scenario,
-			start_game,
-			quit_game;
+	gui_label_buf_t
+		size_label;
+	button_t
+		use_intro_dates,
+		use_beginner_mode,
+		open_climate_gui,
+		open_setting_gui,
+		load_game,
+		load_scenario,
+		start_game,
+		quit_game;
 
 	/**
 	* Calculates preview from height map
@@ -86,37 +85,38 @@ class welt_gui_t  :
 
 
 public:
-		welt_gui_t(settings_t*);
+	welt_gui_t(settings_t*);
 
-		/**
-		* Berechnet Preview-Karte neu. Inititialisiert RNG neu!
-		* public, because also the climate dialog need it
+public:
+	/**
+	* Berechnet Preview-Karte neu. Inititialisiert RNG neu!
+	* public, because also the climate dialog need it
+	*/
+	void update_preview(bool load_heightfield = false);
+	void clear_loaded_heightfield() { loaded_heightfield =0; }
+	bool get_loaded_heightfield() const { return loaded_heightfield; }
+
+	/**
+	* Set the window associated helptext
+		* @return the filename for the helptext, or NULL
 		*/
-		void update_preview(bool load_heightfield = false);
-		void clear_loaded_heightfield() { loaded_heightfield =0; }
-		bool get_loaded_heightfield() const { return loaded_heightfield; }
+	const char * get_help_filename() const OVERRIDE {return "new_world.txt";}
 
-		/**
-		* Set the window associated helptext
-		 * @return the filename for the helptext, or NULL
-		 */
-		const char * get_help_filename() const OVERRIDE {return "new_world.txt";}
+	settings_t* get_sets() const { return sets; }
 
-		settings_t* get_sets() const { return sets; }
+	// does not work during new world dialog
+	bool has_sticky() const OVERRIDE { return false; }
 
-		// does not work during new world dialog
-		bool has_sticky() const OVERRIDE { return false; }
+	/**
+		* Draw new component. The values to be passed refer to the window
+		* i.e. It's the screen coordinates of the window where the
+		* component is displayed.
+		*/
+	void draw(scr_coord pos, scr_size size) OVERRIDE;
 
-		/**
-		 * Draw new component. The values to be passed refer to the window
-		 * i.e. It's the screen coordinates of the window where the
-		 * component is displayed.
-		 */
-		void draw(scr_coord pos, scr_size size) OVERRIDE;
+	bool action_triggered(gui_action_creator_t*, value_t) OVERRIDE;
 
-		bool action_triggered(gui_action_creator_t*, value_t) OVERRIDE;
-
-		static void update_memory(gui_label_buf_t *label, const settings_t* sets);
+	static void update_memory(gui_label_buf_t *label, const settings_t* sets);
 };
 
 #endif
