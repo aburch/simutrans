f65ed12e05dbf4d7b420d185c380d491d3c42e31
ceeac <git.ceeac@gmail.com>
2024-02-10 16:49:34 +0000

    Fix miniz update script
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11062 8aca7d54-2c30-db11-9de9-000461428c89

--- src/external/refresh.sh
+++ src/external/refresh.sh
@@ -1,3 +1,5 @@
-wget "https://github.com/kuba--/zip/archive/refs/heads/master.zip"
-unzip -j master.zip "zip-master/src/*"
+#!/bin/sh
+wget "https://github.com/kuba--/zip/zipball/master/" -O master.zip
+subdir="$(unzip -lqq master.zip | grep -o -e "kuba---zip-[^/]*" | uniq)"
+unzip -j master.zip "$subdir/src/*"
 rm -f master.zip
