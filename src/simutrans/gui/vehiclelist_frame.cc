f87dd9787c55b41d12071b75972a026997a7442f
Markus Pristovsek <prissi@physik.tu-berlin.de>
2024-04-26 13:34:47 +0000

    FIX: vehiclelist ignored name filter for waytypes selection
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11155 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/gui/vehiclelist_frame.cc
+++ src/simutrans/gui/vehiclelist_frame.cc
@@ -176,72 +176,68 @@ vehiclelist_frame_t::vehiclelist_frame_t() :
 	scrolly.set_cmp( vehiclelist_stats_t::compare );
 	scrolly.set_checkered(true);
 
-	set_table_layout(1,0);
+	set_table_layout(3,0);
 
-	add_table(3,0);
-	{
-		new_component<gui_label_t>("Filter:");
-		name_filter_input.set_text(name_filter, lengthof(name_filter));
-		name_filter_input.set_notify_all_changes_delay(0);
-		add_component(&name_filter_input,2);
-		name_filter_input.add_listener(this);
+	new_component<gui_label_t>("Filter:");
+	name_filter_input.set_text(name_filter, lengthof(name_filter));
+	name_filter_input.set_notify_all_changes_delay(0);
+	add_component(&name_filter_input,2);
+	name_filter_input.add_listener(this);
 
-		// next rows
-		bt_obsolete.init(button_t::square_state, "Show obsolete");
-		bt_obsolete.add_listener(this);
-		add_component(&bt_obsolete);
+	// next rows
+	bt_obsolete.init(button_t::square_state, "Show obsolete");
+	bt_obsolete.add_listener(this);
+	add_component(&bt_obsolete);
 
-		bt_future.init(button_t::square_state, "Show future");
-		bt_future.add_listener(this);
-		bt_future.pressed = true;
-		add_component(&bt_future);
+	bt_future.init(button_t::square_state, "Show future");
+	bt_future.add_listener(this);
+	bt_future.pressed = true;
+	add_component(&bt_future);
 
-		ware_filter.clear_elements();
-		ware_filter.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate("All"), SYSCOL_TEXT);
-		idx_to_ware.append(NULL);
-		for (int i = 0; i < goods_manager_t::get_count(); i++) {
-			const goods_desc_t* ware = goods_manager_t::get_info(i);
-			if (ware == goods_manager_t::none) {
-				continue;
-			}
-			if (ware->get_catg() == 0) {
-				ware_filter.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate(ware->get_name()), SYSCOL_TEXT);
-				idx_to_ware.append(ware);
-			}
+	ware_filter.clear_elements();
+	ware_filter.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate("All"), SYSCOL_TEXT);
+	idx_to_ware.append(NULL);
+	for (int i = 0; i < goods_manager_t::get_count(); i++) {
+		const goods_desc_t* ware = goods_manager_t::get_info(i);
+		if (ware == goods_manager_t::none) {
+			continue;
 		}
-		// now add other good categories
-		for (int i = 1; i < goods_manager_t::get_max_catg_index(); i++) {
-			const goods_desc_t* ware = goods_manager_t::get_info_catg(i);
-			if (ware->get_catg() != 0) {
-				ware_filter.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate(ware->get_catg_name()), SYSCOL_TEXT);
-				idx_to_ware.append(ware);
-			}
+		if (ware->get_catg() == 0) {
+			ware_filter.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate(ware->get_name()), SYSCOL_TEXT);
+			idx_to_ware.append(ware);
 		}
-		ware_filter.set_selection(0);
-		ware_filter.add_listener(this);
-		add_component(&ware_filter);
-
-		// second row
-		new_component<gui_label_t>( "hl_txt_sort" );
-
-		sort_by.clear_elements();
-		for( int i = 0; i < vehicle_builder_t::sb_length; i++ ) {
-			sort_by.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate(vehicle_builder_t::vehicle_sort_by[i]), SYSCOL_TEXT);
-		}
-		sort_by.set_selection( vehiclelist_stats_t::sort_mode );
-		sort_by.add_listener( this );
-		add_component( &sort_by );
-
-		sorteddir.init( button_t::sortarrow_state, NULL );
-		sorteddir.pressed = vehiclelist_stats_t::reverse;
-		sorteddir.add_listener( this );
-		add_component( &sorteddir );
 	}
-	end_table();
+	// now add other good categories
+	for (int i = 1; i < goods_manager_t::get_max_catg_index(); i++) {
+		const goods_desc_t* ware = goods_manager_t::get_info_catg(i);
+		if (ware->get_catg() != 0) {
+			ware_filter.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate(ware->get_catg_name()), SYSCOL_TEXT);
+			idx_to_ware.append(ware);
+		}
+	}
+	ware_filter.set_selection(0);
+	ware_filter.add_listener(this);
+	add_component(&ware_filter);
+
+	// second row
+	new_component<gui_label_t>( "hl_txt_sort" );
+
+	sort_by.clear_elements();
+	for( int i = 0; i < vehicle_builder_t::sb_length; i++ ) {
+		sort_by.new_component<gui_scrolled_list_t::const_text_scrollitem_t>(translator::translate(vehicle_builder_t::vehicle_sort_by[i]), SYSCOL_TEXT);
+	}
+	sort_by.set_selection( vehiclelist_stats_t::sort_mode );
+	sort_by.add_listener( this );
+	add_component( &sort_by );
+
+	sorteddir.init( button_t::sortarrow_state, NULL );
+	sorteddir.pressed = vehiclelist_stats_t::reverse;
+	sorteddir.add_listener( this );
+	add_component( &sorteddir );
 
 	tabs.init_tabs(&scrolly);
 	tabs.add_listener(this);
-	add_component(&tabs);
+	add_component(&tabs,3);
 
 	fill_list();
 
@@ -293,40 +289,25 @@ void vehiclelist_frame_t::fill_list()
 	vehiclelist_stats_t::img_width = 32; // reset col1 width
 	uint32 month = world()->get_current_month();
 	const goods_desc_t *ware = idx_to_ware[ max( 0, ware_filter.get_selection() ) ];
-	if(  tabs.get_active_tab_waytype() == ignore_wt) {
-		// adding all vehiles, i.e. iterate over all available waytypes
-		for(  uint32 i=1;  i<tabs.get_count();  i++  ) {
-			for(vehicle_desc_t const* const veh : vehicle_builder_t::get_info(tabs.get_tab_waytype(i)) ) {
-				if(  bt_obsolete.pressed  ||  !veh->is_retired( month )  ) {
-					if(  bt_future.pressed  ||  !veh->is_future( month )  ) {
-						if(name_filter[0]==0  ||  utf8caseutf8(translator::translate(veh->get_name()),name_filter)) {
-							if( ware ) {
-								const goods_desc_t *vware = veh->get_freight_type();
-								if(  (ware->get_catg_index() > 0  &&  vware->get_catg_index() == ware->get_catg_index())  ||  vware->get_index() == ware->get_index()  ) {
-									scrolly.new_component<vehiclelist_stats_t>( veh );
-								}
-							}
-							else {
+	// adding all vehiles, i.e. iterate over all available waytypes
+	for (uint32 i = 1; i < tabs.get_count(); i++) {
+		if(  tabs.get_active_tab_index()>0  &&  i!=tabs.get_active_tab_index()  ) {
+			// wrong waytype
+			continue;
+		}
+		for(vehicle_desc_t const* const veh : vehicle_builder_t::get_info(tabs.get_tab_waytype(i)) ) {
+			if(  bt_obsolete.pressed  ||  !veh->is_retired( month )  ) {
+				if(  bt_future.pressed  ||  !veh->is_future( month )  ) {
+					if(  name_filter[0]==0  ||  utf8caseutf8(translator::translate(veh->get_name()),name_filter)  ) {
+						if(  ware  ) {
+							const goods_desc_t *vware = veh->get_freight_type();
+							if(  (ware->get_catg_index() > 0  &&  vware->get_catg_index() == ware->get_catg_index())  ||  vware->get_index() == ware->get_index()  ) {
 								scrolly.new_component<vehiclelist_stats_t>( veh );
 							}
 						}
-					}
-				}
-			}
-		}
-	}
-	else {
-		for(vehicle_desc_t const* const veh : vehicle_builder_t::get_info(tabs.get_active_tab_waytype())) {
-			if(  bt_obsolete.pressed  ||  !veh->is_retired( month )  ) {
-				if(  bt_future.pressed  ||  !veh->is_future( month )  ) {
-					if( ware ) {
-						const goods_desc_t *vware = veh->get_freight_type();
-						if(  (ware->get_catg_index() > 0  &&  vware->get_catg_index() == ware->get_catg_index())  ||  vware->get_index() == ware->get_index()  ) {
+						else {
 							scrolly.new_component<vehiclelist_stats_t>( veh );
 						}
-					}
-					else {
-						scrolly.new_component<vehiclelist_stats_t>( veh );
 					}
 				}
 			}
