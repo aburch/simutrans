aburch/simutrans

commit 30f9e5255e0ac2067496d6f5ccba45d94a153fa4

(sodiboo) only create user and install directory after the final path has been determined
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11175 8aca7d54-2c30-db11-9de9-000461428c89

src/simutrans/sys/simsys.cc
@@ -379,13 +379,15 @@ bool check_and_set_dir( const char *path, const char *info, char *result, const 
 {
 	if(  path  &&  *path  ) {
 		bool ok = !dr_chdir(path);
-		FILE * testf = NULL;
-		ok &=  ok  &&  testfile  &&  (testf = fopen(testfile,"r"));
+		if(testfile) {
+			FILE* testf = fopen(testfile,"r");
+			ok = ok && testf;
+			fclose(testf);
+		}
 		if(!ok) {
 			printf("WARNING: Objects not found in %s \"%s\"!\n",  info, path);
 		}
 		else {
-			fclose(testf);
 			dr_getcwd( result, PATH_MAX-1 );
 			strcat( result, PATH_SEPARATOR );
 			return true;
@@ -532,8 +534,6 @@ char const *dr_query_homedir()
 	}
 #endif
 
-	// create directory and subdirectories
-	dr_mkdir(buffer);
 	strcat(buffer, PATH_SEPARATOR);
 	return buffer;
 }
@@ -578,8 +578,6 @@ char const *dr_query_installdir()
 	}
 #endif
 
-	// create directory and subdirectories
-	dr_mkdir(buffer);
 	strcat(buffer, PATH_SEPARATOR);
 	return buffer;
 }

src/simutrans/simmain.cc
@@ -589,6 +589,11 @@ int simu_main(int argc, char** argv)
 			}
 		}
 	}
+	
+	// create directory and subdirectories
+	dr_mkdir(env_t::install_dir);
+	dr_mkdir(env_t::user_dir);
+    
 	// set portable, if base dir and user dir were same
 	not_portable &= strcmp( env_t::user_dir, env_t::base_dir )!=0;
 

src/simutrans/sys/simsys.h
@@ -163,7 +163,7 @@ bool check_and_set_dir(const char *path, const char *info, char *result, const c
 * @param base_dir_arg : command line argument "-set_basedir"
 * @param executable_path : Path to executable (as stored in argv[0])
 */
-bool dr_set_basedir(const char * base_dir_arg, char * executable_path);
+bool dr_set_basedir(const char *base_dir_arg, char *executable_path);
 
 /* query home directory */
 char const* dr_query_homedir();

commit 4a4e34397905f9c61afe3dc6978c1a591baca684

FIX: Segmentation fault caused by r11175
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11178 8aca7d54-2c30-db11-9de9-000461428c89

src/simutrans/sys/simsys.cc
@@ -382,7 +382,9 @@ bool check_and_set_dir( const char *path, const char *info, char *result, const 
 		if(testfile) {
 			FILE* testf = fopen(testfile,"r");
 			ok = ok && testf;
-			fclose(testf);
+			if(testf) {
+				fclose(testf);
+			}
 		}
 		if(!ok) {
 			printf("WARNING: Objects not found in %s \"%s\"!\n",  info, path);