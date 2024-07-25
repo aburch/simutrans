cdb585397c6ddf391025ece549d1053961ac047c
Markus Pristovsek <prissi@physik.tu-berlin.de>
2024-07-25 11:36:52 +0000

    Release of 142.2.1
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11366 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/simversion.h
+++ src/simutrans/simversion.h
@@ -19,7 +19,7 @@
 #define SIM_VERSION_MAJOR 124
 #define SIM_VERSION_MINOR   2
 #define SIM_VERSION_PATCH   1
-#define SIM_VERSION_BUILD SIM_BUILD_NIGHTLY
+#define SIM_VERSION_BUILD SIM_BUILD_RELEASE
 
 // Beware: SAVEGAME minor is often ahead of version minor when there were patches.
 // ==> These have no direct connection at all!
