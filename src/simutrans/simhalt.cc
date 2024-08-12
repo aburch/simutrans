a904064aabfb9966a4fba92a77b2e60a7ef7360c
Markus Pristovsek <prissi@physik.tu-berlin.de>
2024-08-12 21:25:05 +0000

    FIX: crash when removing the last element
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11382 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/simhalt.cc
+++ src/simutrans/simhalt.cc
@@ -937,8 +937,8 @@ void haltestelle_t::request_loading( convoihandle_t cnv )
 	if(  last_loading_step != welt->get_steps()  ) {
 		last_loading_step = welt->get_steps();
 		// now iterate over all convois
-		for(  vector_tpl<convoihandle_t>::iterator i = loading_here.begin(); i != loading_here.end();  ) {
-			convoihandle_t const c = *i;
+		for (uint32 i = 0;  i<loading_here.get_count();  ) {
+			convoihandle_t const c = loading_here[i];
 			if (c.is_bound() && c->get_state() == convoi_t::LOADING) {
 				// now we load into convoi
 				c->hat_gehalten(self);
@@ -947,7 +947,7 @@ void haltestelle_t::request_loading( convoihandle_t cnv )
 				++i;
 			}
 			else {
-				i = loading_here.erase( i );
+				loading_here.remove_at(i);
 			}
 		}
 	}
