e634f186cd1898f8274f2bf6969595003b24256e
Markus Pristovsek <prissi@physik.tu-berlin.de>
2025-01-13 07:51:12 +0000

    updating the source packing tool
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11597 8aca7d54-2c30-db11-9de9-000461428c89

--- tools/zipsrc.sh
+++ tools/zipsrc.sh
@@ -3,10 +3,11 @@
 packen()
 {
 	# pack all files of the current release
-	FILELISTE=`find . -type f "(" -name "*.c" -o -name "*.h" -o -name "*.cc" -o -name "*.mm" ")"`
-	FILELISTF=`find . -type f "(" -name "*.sh" -o -name "*.mk"  -o -name "*.nut" ")"`
-	FILELISTG=`find . -type f "(" -name "*.txt" -o -name "*.bdf"  -o -name "*.fnt"  -o -name "*.mid"  -o -name "*.tab" ")"`
-	FILELISTI=`find . -type f "(" -name "*.tab" -o -name "*.dat"  -o -name "*.png" -o -name "*.svg" -o -name "*.desktop" ")"`
+	FILELISTE=`find . -type f "(" -name "*.c" -o -name "*.h" -o -name "*.cc" -o -name "*.mm" ")" -not -path */.vs/* -not -path */vcpkg_installed/*`
+	FILELISTF=`find . -type f "(" -name "*.sh" -o -name "*.mk"  -o -name "*.nut" ")" -not -path */.vs/* -not -path */vcpkg_installed/*`
+	FILELISTG=`find . -type f "(" -name "*.txt" -o -name "*.bdf"  -o -name "*.fnt"  -o -name "*.mid"  -o -name "*.tab" ")" -not -path */.vs/* -not -path */vcpkg_installed/*`
+	FILELISTI=`find . -type f "(" -name "*.tab" -o -name "*.dat"  -o -name "*.png" -o -name "*.svg" -o -name "*.desktop" ")" -not -path */.vs/* -not -path */vcpkg_installed/*`
+	FILELISTI=`find . -type f "(" -name "*.vcxproj" -o -name "*.vcxproj.filters"  -o -name "*.sln" -o -name "*.vcxitems" -o -name "*.json" ")" -not -path */.vs/* -not -path */vcpkg_installed/*`
 	rm ../simutrans-src.zip
 	zip ../simutrans-src.zip $FILELISTE
 	zip ../simutrans-src.zip $FILELISTF
@@ -16,7 +17,6 @@ packen()
 	zip ../simutrans-src.zip Makefile src/makeobj/Makefile src/nettool/Makefile
 	zip ../simutrans-src.zip cmake/*.cmake
 	zip ../simutrans-src.zip configure.ac config.default.in config.template src/OSX/simutrans.icns
-	zip ../simutrans-src.zip documentation/*.pal simutrans/themes/*.pak
 	zip -r ../simutrans-src.zip src/Windows
 }
 
