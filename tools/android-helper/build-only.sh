1b6c86e5bd96b5848c0d23fe914f42c445e6e90c
Markus Pristovsek <prissi@physik.tu-berlin.de>
2025-01-30 08:07:36 +0000

    ADD: script for settung up android environment, android simutrans files and build from commandline
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11624 8aca7d54-2c30-db11-9de9-000461428c89

---
+++ tools/android-helper/build-only.sh
@@ -0,0 +1,34 @@
+cd simutrans/jni
+
+# Assets
+#./simutrans/src/android/AndroidPreBuild.sh
+cp -rf simutrans/simutrans/. ../src/main/assets
+
+# Revision
+cd simutrans/tools
+REVISION=$(./get_revision.sh)
+sed -i "s/versionCode [0-9]\+/versionCode $REVISION/" ../../../build.gradle
+
+cd ../../../..
+VERSION=`sed -n 's/#define SIM_VERSION_MAJOR *\([0-9]*\)$/\1/ p' <simutrans/jni/simutrans/src/simutrans/simversion.h`.`sed -n 's/#define SIM_VERSION_MINOR *\([0-9]*\)$/\1/ p' <simutrans/jni/simutrans/src/simutrans/simversion.h`.`sed -n 's/#define SIM_VERSION_PATCH *\([0-9]*\)$/\1/ p' <simutrans/jni/simutrans/src/simutrans/simversion.h`
+NIGHTLY=`sed -n 's/#define SIM_VERSION_BUILD SIM_BUILD_NIGHTLY/ Nightly/ p' <simutrans/jni/simutrans/src/simutrans/simversion.h``sed -n 's/#define SIM_VERSION_BUILD SIM_BUILD_RELEASE_CANDIDATE/ Release candidate/ p' <simutrans/jni/simutrans/src/simutrans/simversion.h`
+sed -i 's/versionName.*$/versionName "'"$VERSION$NIGHTLY"'"/' simutrans/build.gradle
+
+echo "Build Simutrans $VERSION$NIGHTLY r$REVISION"
+echo "$PWD"
+
+# Build Android project
+cp -r simutrans/jni/SDL/android-project/app/src/main/java simutrans/src/main
+
+cat simutrans/build.gradle
+./gradlew assembleRelease
+./gradlew bundleRelease
+
+cp simutrans/build/outputs/bundle/release/simutrans-release.aab ./test-simutrans.aab
+#jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore $SIGNING_KEYSTORE test-simutrans.aab $SIGNING_KEY_ALIAS -storepass $SIGNING_STORE_PASSWORD 
+bundletool build-apks --bundle=test-simutrans.aab --output=temp.apks --mode=universal
+unzip temp.akps
+rm -rf temp.apks toc.pb
+mv unversal.apk simutrans-nightly.apk
+
+
---
+++ tools/android-helper/setup-android.sh
@@ -0,0 +1,53 @@
+
+
+sudo apt-get update
+sudo dpkg --add-architecture i386
+sudo apt-get -yqq update
+sudo apt-get -ym install curl expect git libc6:i386 libgcc1:i386 libncurses6:i386 libstdc++6:i386 zlib1g:i386 openjdk-17-jdk wget unzip vim make subversion zip python3
+
+sudo mkdir -p /opt/android-sdk-linux
+sudo chown -R $USER /opt/android-sdk-linux
+
+echo "export ANDROID_HOME=/opt/android-sdk-linux" >> ~/.profile
+echo "export ANDROID_SDK_HOME=/opt/android-sdk-linux" >> ~/.profile
+echo "export ANDROID_SDK_ROOT=/opt/android-sdk-linux" >> ~/.profile
+echo "export ANDROID_SDK=/opt/android-sdk-linux" >> ~/.profile
+echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >> ~/.profile
+echo "export ANDROID_NDK=/opt/android-sdk-linux/ndk/25.1.8937393/" >> ~/.profile
+echo 'PATH=$PATH:/opt/android-sdk-linux/cmdline-tools/latest/bin:/opt/android-sdk-linux/platform-tools:/opt/android-sdk-linux/build-tools/34.0.0:/opt/android-sdk-linux/cmake/3.22.1/bin:/opt/android-sdk-linux/ndk/25.1.8937393:/opt/android-sdk-linux/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/bin/' >> ~/.prof
+
+export ANDROID_HOME=/opt/android-sdk-linux
+export ANDROID_SDK_HOME=/opt/android-sdk-linux
+export ANDROID_SDK_ROOT=/opt/android-sdk-linux
+export ANDROID_SDK=/opt/android-sdk-linux
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export ANDROID_NDK=/opt/android-sdk-linux/ndk/25.1.8937393/
+
+PATH=$PATH:/opt/android-sdk-linux/cmdline-tools/latest/bin:/opt/android-sdk-linux/platform-tools:/opt/android-sdk-linux/build-tools/34.0.0:/opt/android-sdk-linux/cmake/3.22.1/bin:/opt/android-sdk-linux/ndk/25.1.8937393:/opt/android-sdk-linux/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/bin/
+
+cd /opt/android-sdk-linux
+wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
+unzip commandlinetools-linux-11076708_latest.zip
+rm commandlinetools-linux-11076708_latest.zip
+mv cmdline-tools latest
+mkdir cmdline-tools
+mv latest cmdline-tools/latest
+yes | sdkmanager --licenses
+yes | sdkmanager --install "platform-tools"
+yes | sdkmanager --install "build-tools;34.0.0"
+yes | sdkmanager --install "cmake;3.22.1"
+yes | sdkmanager --install "ndk;25.1.8937393"
+ln -s llvm-objdump /opt/android-sdk-linux/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/bin/objdump
+
+
+mkdir ~/.android/
+keytool -genkey -v -keystore ~/.android/debug.keystore -alias androiddebugkey -keyalg RSA -keysize 2048 -validity 10000 -keypass android -storepass android -dname "cn=example.com,ou=exampleou,dc=example,dc=com"
+
+export SIGNING_KEYSTORE=~/.android/debug.keystore
+export SIGNING_STORE_PASSWORD=android
+export SIGNING_KEY_PASSWORD=android
+export SIGNING_KEY_ALIAS=androiddebugkey
+echo "export SIGNING_KEYSTORE=~/.android/debug.keystore" >> ~/.profile
+echo "export SIGNING_STORE_PASSWORD=android" >> ~/.profile
+echo "export SIGNING_KEY_PASSWORD=android" >> ~/.profile
+echo "export SIGNING_KEY_ALIAS=androiddebugkey" >> ~/.profile
