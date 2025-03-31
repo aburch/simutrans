a08f2aba6b1243726d3ef23e7993c71f506dc371
simu_poppo <118958719+simupoppo@users.noreply.github.com>
2025-03-31 23:43:33 +0900

    Make Convoi Stops List in convoi_info_t

---
+++ gui/convoi_stops_list_t.h
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the Simutrans project under the Artistic License.
+ * (see LICENSE.txt)
+ */
+
+#ifndef GUI_SCHEDULE_GUI_H
+#define GUI_SCHEDULE_GUI_H
+
+
+#include "gui_frame.h"
+
+#include "components/gui_label.h"
+#include "components/gui_numberinput.h"
+#include "components/gui_combobox.h"
+#include "components/gui_button.h"
+#include "components/action_listener.h"
+
+#include "components/gui_scrollpane.h"
+
+#include "../convoihandle_t.h"
+#include "simwin.h"
+#include "../tpl/vector_tpl.h"
+
+
+class schedule_t;
+class player_t;
+class cbuffer_t;
+class convoi_stops_list_t;
+class gui_convoi_stops_list_t;
+
+
+/**
+ * List of displayed schedule entries.
+ */
+class convoi_stops_list_t : public gui_aligned_container_t, action_listener_t, public gui_action_creator_t
+{
+	convoihandle_t cnv;
+	static cbuffer_t buf;
+
+	vector_tpl<gui_convoi_stops_list_t*> entries;
+	schedule_t *gui_schedule; // displaied schedule
+public:
+	player_t*  player;
+	void init(convoihandle_t cnv);
+
+	convoi_stops_list_t(convoihandle_t cnv_ = convoihandle_t());
+
+	// shows/deletes highlighting of tiles
+	void update_schedule();
+	void draw(scr_coord offset) OVERRIDE;
+	bool action_triggered(gui_action_creator_t *comp, value_t p) OVERRIDE;
+};
+
+#endif
