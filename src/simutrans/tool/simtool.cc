0c592f8a3787b019d14bb9296b641306b0e6f8c8
ceeac <git.ceeac@gmail.com>
2025-06-03 12:15:40 +0000

    FIX: Graphical glitch when dragging the forest tool across slopes if the pakset does not have double grounds
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11718 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/tool/simtool.cc
+++ src/simutrans/tool/simtool.cc
@@ -6457,22 +6457,20 @@ void tool_forest_t::mark_tiles(  player_t *, const koord3d &start, const koord3d
 	k1.y = start.y < end.y ? start.y : end.y;
 	k2.x = start.x + end.x - k1.x;
 	k2.y = start.y + end.y - k1.y;
+
 	koord k;
 	for(  k.x = k1.x;  k.x <= k2.x;  k.x++  ) {
 		for(  k.y = k1.y;  k.y <= k2.y;  k.y++  ) {
-			grund_t *gr = welt->lookup_kartenboden( k );
+			grund_t *gr = welt->lookup_kartenboden_nocheck( k );
+
+			// use grund_hang only and not weg_hang since the tool only operates on kartenboden.
+			// In particular, dragging the tool across a hill in sliced underground mode
+			// affects the invisible tiles at the top of the hill and not any tunnel tiles.
+			const slope_t::type grund_hang = gr->get_grund_hang();
 
 			zeiger_t *marker = new zeiger_t(gr->get_pos(), NULL );
-
-			const uint8 grund_hang = gr->get_grund_hang();
-			const uint8 weg_hang = gr->get_weg_hang();
-			const uint8 hang = max( corner_sw(grund_hang), corner_sw(weg_hang)) +
-					3 * max( corner_se(grund_hang), corner_se(weg_hang)) +
-					9 * max( corner_ne(grund_hang), corner_ne(weg_hang)) +
-					27 * max( corner_nw(grund_hang), corner_nw(weg_hang));
-			uint8 back_hang = (hang % 3) + 3 * ((uint8)(hang / 9)) + 27;
-			marker->set_foreground_image( ground_desc_t::marker->get_image( grund_hang % 27 ) );
-			marker->set_image( ground_desc_t::marker->get_image( back_hang ) );
+			marker->set_foreground_image(ground_desc_t::get_marker_image(grund_hang, false));
+			marker->set_image(           ground_desc_t::get_marker_image(grund_hang, true ));
 
 			marker->mark_image_dirty( marker->get_image(), 0 );
 			gr->obj_add( marker );
