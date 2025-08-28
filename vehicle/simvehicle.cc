16f40352165531cbb4d8a130c3dd356412c64d5e
simupoppo <simupoppo@gmail.com>
2025-08-28 21:00:19 +0900

    change reverse point

--- vehicle/simvehicle.cc
+++ vehicle/simvehicle.cc
@@ -5156,9 +5156,12 @@ bool air_vehicle_t::can_enter_tile(const grund_t *gr, sint32 &restart_speed, uin
 		return false;
 	}
 
-	if(  cnv->is_reversed() && route_index > min(2, takeoff)  ) {
-		cnv->reverse_vehicles_on_user_request();
-		return false;
+	if(  cnv->is_reversed()  ) {
+		ribi_t::ribi next_dir = ribi_type(cnv->get_route()->at(min(route_index+1,cnv->get_route()->get_count()-1)) - cnv->get_route()->at(route_index));
+		if( (next_dir & get_direction())==0 || route_index>=takeoff ) {
+			cnv->reverse_vehicles_on_user_request();
+			return true;
+		}
 	}
 
 	if(  route_index < takeoff  &&  route_index > 1  &&  takeoff<cnv->get_route()->get_count()-1  ) {
