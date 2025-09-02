426bc246812bdf9db179daeec2d3457cb61b6ded
simu_poppo <simupoppo@gmail.com>
2025-09-02 17:11:45 +0900

    update is_load_before_departure
    発車直前に乗車する機能を改善し、子編成でも機能するようにしました
    また、積むまで待機が発車事項設定より優位になったことを受け、処理の順番を変更しました。

--- simconvoi.cc
+++ simconvoi.cc
@@ -3791,11 +3791,14 @@ void convoi_t::hat_gehalten(halthandle_t halt, uint32 halt_length_in_vehicle_ste
 	const goods_desc_t* cargo_type_prev = NULL;
 	bool loading_needed = !no_load  &&  !next_depot;
 	// When load_before_departure is enabled, load cargos only when the departure time condition is satisfied.
-	if(  schedule->get_current_entry().get_wait_for_time()  &&  schedule->get_current_entry().is_load_before_departure()  ) {
-		loading_needed &= (scheduled_departure_time!=0  &&  is_first_ticks_bigger(welt->get_ticks(), scheduled_departure_time - time));
-	}
-	else if(schedule->get_current_entry().waiting_time_shift > 0  &&  schedule->get_current_entry().is_load_before_departure() ){
+	// first we check the waiting_time_shift, then check the departure slot time
+	// only parents convoy has usable scheduled_departure_time
+	convoihandle_t leading_convoy = find_most_parent_convoi();
+	if(schedule->get_current_entry().waiting_time_shift > 0  &&  schedule->get_current_entry().is_load_before_departure() ){
 		loading_needed &= (schedule->get_current_entry().waiting_time_shift > 0  &&  welt->get_ticks() - arrived_time >= welt->ticks_per_world_month / schedule->get_current_entry().waiting_time_shift);
+	}
+	else if(  leading_convoy->schedule->get_current_entry().get_wait_for_time()  &&  schedule->get_current_entry().is_load_before_departure()  ) {
+		loading_needed &= (leading_convoy->scheduled_departure_time!=0  &&  is_first_ticks_bigger(welt->get_ticks(), leading_convoy->scheduled_departure_time - time));
 	}
 
 	vector_tpl<haltestelle_t::reachable_halt_t> reachable_halts;
