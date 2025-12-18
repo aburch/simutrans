11ff719a99bde092a769461cb62d0ba6510d1909
simu_poppo <simupoppo@gmail.com>
2025-12-18 23:43:53 +0900

    self_destruct update

--- simconvoi.cc
+++ simconvoi.cc
@@ -4256,12 +4256,7 @@ void convoi_t::self_destruct()
 		return;
 	}
 	else if(  is_coupled()  ) {
-		// find parent convoy and try uncoupling.
-		FOR(vector_tpl<convoihandle_t>, const& c, world()->convoys()) {
-			if(  c->get_coupling_convoi()==self  ) {
-				c->uncouple_convoi();
-			}
-		}
+		parent_convoi->uncouple_convoi();
 	}
 	state = SELF_DESTRUCT;
 	wait_lock = 0;
