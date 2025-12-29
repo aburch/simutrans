185a43dc00f4d7383d52bd4c1e5415f53e6c744d
Markus Pristovsek <prissi@physik.tu-berlin.de>
2025-12-29 03:20:07 +0000

    Try to force our NDK
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11852 8aca7d54-2c30-db11-9de9-000461428c89

--- tools/android-helper/setup-simutrans-android.sh
+++ tools/android-helper/setup-simutrans-android.sh
@@ -12,7 +12,7 @@ git revert 53dbe18  --no-commit
 cd ..
 svn checkout svn://servers.simutrans.org/simutrans/trunk simutrans
 
-./build_libraries.sh
+#./build_libraries.sh
 
 # Fluidsynth is a PITA to build; using the prebuilt release instead
 wget https://github.com/FluidSynth/fluidsynth/releases/download/v2.5.1/fluidsynth-2.5.1-android24.zip
