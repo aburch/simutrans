4410bf740bc9bd9270efcfef542cd961b2a05fdb
simu_poppo <simupoppo@gmail.com>
2026-01-10 20:38:17 +0900

    do not clip obj in ground except for tree

--- simplan.cc
+++ simplan.cc
@@ -478,6 +478,10 @@ void planquadrat_t::display_obj(const sint16 xpos, const sint16 ypos, const sint
 				// not too low?
 				if(  htop >= hmin  ) {
 					// something on top: clip horizontally to prevent trees etc shining trough bridges
+					if(  !gr0->obj_bei(0) || gr0->obj_bei(0)->get_typ()==obj_t::way || gr0->obj_bei(0)->get_typ()==obj_t::gebaeude || gr0->get_leitung()  ) {
+						// no tree
+						break;
+					}
 					const sint16 yh = ypos - tile_raster_scale_y( (h + corner_nw(data.some[j]->get_grund_hang()) - h0) * TILE_HEIGHT_STEP, raster_tile_width ) + ((3 * raster_tile_width) >> 2);
 					if(  yh >= p_cr.y  ) {
 						display_push_clip_wh(p_cr.x, yh, p_cr.w, p_cr.h + p_cr.y - yh  CLIP_NUM_PAR  );
