b0a1c4a0c7f2d6af0c814dfc30170cd35563adec
simu_poppo <simupoppo@gmail.com>
2026-02-10 01:34:04 +0900

    add comments

--- simhalt.cc
+++ simhalt.cc
@@ -2080,6 +2080,7 @@ int haltestelle_t::search_route( const halthandle_t *const start_halts, const ui
 	}
 	else {
 		// we already set getoff stop (because this goods is dummy!)
+		// e.g. called by route_search_frame_t
 		halthandle_t halt = ware.get_ziel();
 				end_halts.append(halt);
 
