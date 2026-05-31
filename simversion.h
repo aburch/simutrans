287f010ea356305988af10c24364f1d57dfb03c2
simu_poppo <simupoppo@gmail.com>
2026-05-31 18:35:37 +0900

    increment #define OTRP_VERSION_MINOR
    v55_4

--- simversion.h
+++ simversion.h
@@ -31,7 +31,7 @@
 // NOTE: increment before next release to enable save/load of new features
 
 #define OTRP_VERSION_MAJOR 55
-#define OTRP_VERSION_MINOR 3
+#define OTRP_VERSION_MINOR 4
 #define OTRP_VERSION_PATCH 0
 // NOTE: increment OTRP_VERSION_MAJOR when the save data structure changes.
 
