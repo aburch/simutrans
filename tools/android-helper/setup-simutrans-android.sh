36d32b83488b33d81a2bc9845f84c87eefdf1e10
Roboron3042 <Roboron3042@users.noreply.github.com>
2026-07-10 17:37:16 +0000

    Update FluidSynth version to 2.5.6
    git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@12077 8aca7d54-2c30-db11-9de9-000461428c89

--- tools/android-helper/setup-simutrans-android.sh
+++ tools/android-helper/setup-simutrans-android.sh
@@ -15,7 +15,7 @@ svn checkout svn://servers.simutrans.org/simutrans/trunk simutrans
 #./build_libraries.sh
 
 # Fluidsynth is a PITA to build; using the prebuilt release instead
-wget https://github.com/FluidSynth/fluidsynth/releases/download/v2.5.1/fluidsynth-2.5.1-android24.zip
+wget https://github.com/FluidSynth/fluidsynth/releases/download/v2.5.6/fluidsynth-v2.5.6-android24.zip
 unzip fluidsynth-*.zip -d fluidsynth
 
 ./simutrans/src/android/AndroidPreBuild.sh
